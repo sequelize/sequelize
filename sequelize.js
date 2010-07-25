68a9341089c90cbd5cd7add894e3868e59e0fe73
Sascha Depold <sascha@depold.com>
2010-07-25 08:32:05 +0200

    moved all sql helpers to Helper.SQL.X

--- sequelize.js
+++ sequelize.js
@@ -81,53 +81,107 @@ exports.Sequelize.prototype = {
 var TableWrapper = function(sequelize, tableName, attributes) {
   var table = function(values) {
     var self = this
-    Helper.Hash.keys(values).forEach(function(key) {
-      if(attributes[key]) {
-        self[key] = values[key]
-      }
+    Helper.Hash.forEach(values, function(value, key) {
+      if(attributes[key])
+        self[key] = value
     })
     this.id = null // specify id as null to declare this object as unsaved and as not present in the database
     this.tableName = tableName
     this.attributes = attributes
   }
   
-  table.sync = function(callback) {
-    var fields = ["id INT"]
-    Helper.Hash.keys(attributes).forEach(function(name) { fields.push(name + " " + attributes[name]) })
-    var query = "CREATE TABLE IF NOT EXISTS " + tableName + " (" + fields.join(', ') + ")"
+  // class methods
+  var classMethods = {
+    sync: function(callback) {
+      var fields = ["id INT NOT NULL auto_increment PRIMARY KEY"]
+      Helper.Hash.keys(attributes).forEach(function(name) { fields.push(name + " " + attributes[name]) })
+      var query = "CREATE TABLE IF NOT EXISTS " + tableName + " (" + fields.join(', ') + ")"
 
-    sequelize.query(query, callback)
+      sequelize.query(query, function() {
+        if(callback) callback(table)
+      })
+    },
+
+    drop: function(callback) {
+      var query = "DROP TABLE IF EXISTS " + tableName
+      sequelize.query(query, function() {
+        if(callback) callback(table)
+      })
+    },
+
+    findAll: function(callback) {
+      var query = "SELECT * FROM " + tableName
+      sequelize.query(query, function(result) {
+        var objects = []
+        result.forEach(function(resultSet) {
+          objects.push(table.sqlResultToObject(resultSet))
+        })
+        
+        if(callback) callback(objects)
+      })
+    },
+
+    find: function(conditions, callback) {
+      var query = Helper.evaluateTemplate(
+        "SELECT * FROM %{table} WHERE %{conditions} ORDER BY id DESC LIMIT 1",
+        { table: tableName, conditions: Helper.SQL.hashToWhereConditions(conditions) }
+      )
+      
+      sequelize.query(query, function(result) {
+        if (callback) callback(table.sqlResultToObject(result[0]))
+      })
+    },
+    
+    sqlResultToObject: function(result) {
+      var object = new table(result)
+      object.id = result.id
+      return object
+    }
   }
   
-  table.drop = function(callback) {
-    var query = "DROP TABLE IF EXISTS " + tableName
-    sequelize.query(query, callback)
-  }
-  
-  table.findAll = function(callback) {
-    var query = "SELECT * FROM " + tableName
-    sequelize.query(query, callback)
-  }
-  
+  // instance methods
   table.prototype = {
+    get values() {
+      var result = {}
+      var self = this
+      Helper.Hash.keys(attributes).forEach(function(attribute) {
+        result[attribute] = self[attribute]
+      })
+      return result
+    },
+    
     save: function(callback) {
       var query = null
-
+      var self = this
       if(this.id == null) {
         query = Helper.evaluateTemplate(
           "INSERT INTO %{table} (%{fields}) VALUES (%{values})",
-          { table: this.tableName, fields: Helper.fieldsForInsertQuery(this), values: Helper.valuesForInsertQuery(this) }
+          { table: this.tableName, fields: Helper.SQL.fieldsForInsertQuery(this), values: Helper.SQL.valuesForInsertQuery(this) }
         )
       } else {
         query = evaluateTemplate(
           "UPDATE %{table} SET %{values} WHERE id = %{id}",
-          { table: this.tableName, values: Helper.valuesForUpdate(this), id: this.id }
+          { table: this.tableName, values: Helper.SQL.valuesForUpdate(this), id: this.id }
         )
       }
       
-      sequelize.query(query, callback)
+      sequelize.query(query, function() {
+        if(self.id == null) {
+          table.find(self.values, function(result) {
+            Helper.log(result)
+            self.id = result.id
+            if(callback) callback(self)
+          })
+        } else {
+          if(callback) callback(self)
+        }
+      })
     }
   }
+  
+  Helper.Hash.forEach(classMethods, function(func, funcName) {
+    table[funcName] = func
+  })
   
   return table
 }
@@ -140,55 +194,63 @@ var Helper = {
     sys.puts(sys.inspect(obj))
   },
   
-  values: function(object) {
-    var result = {}
-    Helper.Hash.keys(object.attributes).forEach(function(key) {
-      result[key] = object[key]
-    })
-    return result
-  },
-  
-  valuesForInsertQuery: function(object) {
-    var actualValues = Helper.values(object),
-        result  = []
-        
-    Helper.Hash.keys(actualValues).forEach(function(key) {
-      var value     = actualValues[key],
-          dataType  = object.attributes[key]
-          
-      result.push(Helper.transformValueByDataType(value, dataType))
-    })
+  SQL: {
+    valuesForInsertQuery: function(object) {
+      var actualValues = object.values,
+          result  = []
+
+      Helper.Hash.keys(actualValues).forEach(function(key) {
+        var value     = actualValues[key],
+            dataType  = object.attributes[key]
+
+        result.push(Helper.SQL.transformValueByDataType(value, dataType))
+      })
+
+      return result
+    },
+
+    fieldsForInsertQuery: function(object) {
+      return Helper.Hash.keys(object.values).join(", ")
+    },
+
+    transformValueByDataType: function(value, dataType) {
+      var result = null
+      switch(dataType) {
+        case exports.Sequelize.INTEGER:
+          result = value; break;
+        default:
+          result = "'" + value + "'"; break;
+      }
+      return result
+    },
+
+    valuesForUpdate: function(object, options) {
+      var actualValues = object.values,
+          result  = []
+
+      options = options || {}
+
+      Helper.Hash.keys(actualValues).forEach(function(key) {
+        var value     = actualValues[key],
+            dataType  = object.attributes[key]
+
+        result.push([key, Helper.SQL.transformValueByDataType(value, dataType)].join(" = "))
+      })
+
+      return result.join(options.seperator || ", ")
+    },
     
-    return result
-  },
-  
-  fieldsForInsertQuery: function(object) {
-    return Helper.Hash.keys(Helper.values(object)).join(", ")
-  },
-  
-  transformValueByDataType: function(value, dataType) {
-    var result = null
-    switch(dataType) {
-      case exports.Sequelize.INTEGER:
-        result = value; break;
-      default:
-        result = "'" + value + "'"; break;
+    hashToWhereConditions: function(conditions) {
+      if(typeof conditions == 'number')
+        return ('id = ' + conditions)
+      else {
+        var result = []
+        Helper.Hash.forEach(conditions, function(value, key) {
+          result.push(key + "=" + Helper.SQL.transformValueByDataType(value))
+        })
+        return result.join(" AND ")
+      }
     }
-    return result
-  },
-  
-  valuesForUpdate: function(object) {
-    var actualValues = Helper.values(object),
-        result  = []
-        
-    Helper.Hash.keys(actualValues).forEach(function(key) {
-      var value     = actualValues[key],
-          dataType  = object.attributes[key]
-          
-      result.push([key, Helper.transformValueByDataType(value, dataType)].join(" = "))
-    })
-    
-    return result.join(", ")
   },
   
   evaluateTemplate: function(template, replacements) {
@@ -200,6 +262,12 @@ var Helper = {
   },
   
   Hash: {
+    forEach: function(object, func) {
+      Helper.Hash.keys(object).forEach(function(key) {
+        func(object[key], key, object)
+      })
+    },
+    
     keys: function(object) {
       var results = []
       for (var property in object)
