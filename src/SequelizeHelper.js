4fda57ac29a0cc6a148097a6449f8547a6fdcb20
Sascha Depold <sascha@depold.com>
2010-08-04 10:36:22 +0200

    some helpers

--- src/SequelizeHelper.js
+++ src/SequelizeHelper.js
@@ -142,11 +142,31 @@ SequelizeHelper = {
       })
       return result
     },
-    without: function(array, func) {
+    reject: function(array, func) {
       var result = []
       array.forEach(function(element) {
         if(!func(element)) result.push(element)
       })
+      return result
+    },
+    select: function(array, func) {
+      var result = []
+      array.forEach(function(element) {
+        if(func(element)) result.push(element)
+      })
+      return result
+    },
+    without: function(array, withouts) {
+      var result = []
+      array.forEach(function(e) {
+        if(withouts.indexOf(e) == -1) result.push(e)
+      })
+      return result
+    },
+    join: function(arr1, arr2) {
+      var result = []
+      arr1.forEach(function(e) { result.push(e) })
+      arr2.forEach(function(e) { result.push(e) })
       return result
     }
   }
