2da1680fa9d79696313df4de204d1a8d017d65cb
Sascha Depold <sascha@depold.com>
2010-09-01 20:54:04 +0200

    use no password + some output

--- examples/SequelizeWithOptions/app.js
+++ examples/SequelizeWithOptions/app.js
@@ -1,9 +1,15 @@
 var Sequelize = require(__dirname + "/../../lib/sequelize/Sequelize").Sequelize,
-    sequelize = new Sequelize("sequelize_test", "test", "test", {
+    sequelize = new Sequelize("sequelize_test", "root", null, {
       // use other database server or port
       host: 'my.srv.tld',
       port: 12345,
       
       // disable logging
       disableLogging: true
-    })
+    }),
+    Smth      = sequelize.define('Smth', {foo: Sequelize.STRING})
+
+Smth.sync(function(_, err) {
+  if(err) Sequelize.Helper.log(err)
+  else Sequelize.Helper.log('Hey we established the connection successfully! Woot!')
+})
