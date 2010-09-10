fff633bb48e6fc888b5961206692a72ff00d0c71
Sascha Depold <sascha@depold.com>
2010-09-11 00:11:06 +0200

    new example for associations

--- examples/Associations/app.js
+++ examples/Associations/app.js
@@ -24,13 +24,15 @@ Sequelize.chainQueries([{drop: sequelize}, {sync: sequelize}], function() {
   var person  = new Person({ name: 'Luke' }),
       mother  = new Person({ name: 'Jane' }),
       father  = new Person({ name: 'John' }),
-      brother = new Person({ name: 'brother' }),
-      sister  = new Person({ name: 'sister' })
+      brother = new Person({ name: 'Brother' }),
+      sister  = new Person({ name: 'Sister' }),
+      pet     = new Pet({ name: 'Bob' })
    
-   Sequelize.chainQueries([{save: person}, {save: mother}, {save: father}, {save: brother}, {save: sister}], function() {
+   Sequelize.chainQueries([{save: person}, {save: mother}, {save: father}, {save: brother}, {save: sister}, {save: pet}], function() {
      person.setMother(mother, function(mom) { Sequelize.Helper.log('my mom: ' + mom.name) })
      person.setFather(father, function(dad) { Sequelize.Helper.log('my dad: ' + dad.name) })
      /*person.setBrothers([brother], function(bros) { Sequelize.Helper.log('ma bro: ' + bros[0].name)})
           person.setSisters([sister], function(sis) { Sequelize.Helper.log('ma sis\': ' + sis[0].name)})*/
+    person.setPets([pet], function(pets) { Sequelize.Helper.log('my pet: ' + pets[0].name )})
    })
 })
