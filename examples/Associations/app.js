4d39a3bd983833c2cc101696ce5bbad4bf7eb35f
Sascha Depold <sascha@depold.com>
2010-09-11 20:53:58 +0200

    hasAndBelongsToMany association!

--- examples/Associations/app.js
+++ examples/Associations/app.js
@@ -1,5 +1,5 @@
 var Sequelize = require(__dirname + "/../../lib/sequelize/Sequelize").Sequelize,
-    sequelize = new Sequelize("sequelize_test", "root", null)
+    sequelize = new Sequelize("sequelize_test", "root", null, {disableLogging: true})
 
 var Person = sequelize.define('person', {
   name: Sequelize.STRING
@@ -9,8 +9,8 @@ var Pet = sequelize.define('pet', {
   name: Sequelize.STRING
 })
 
-/*Person.hasMany('brothers', Person)
-Person.hasMany('sisters', Person)*/
+Person.hasMany('brothers')
+Person.hasMany('sisters')
 
 var fatherAssoc = Person.hasOne('father', Person)
 var motherAssoc = Person.hasOne('mother', Person)
@@ -28,11 +28,11 @@ Sequelize.chainQueries([{drop: sequelize}, {sync: sequelize}], function() {
       sister  = new Person({ name: 'Sister' }),
       pet     = new Pet({ name: 'Bob' })
    
-   Sequelize.chainQueries([{save: person}, {save: mother}, {save: father}, {save: brother}, {save: sister}, {save: pet}], function() {
-     person.setMother(mother, function(mom) { Sequelize.Helper.log('my mom: ' + mom.name) })
-     person.setFather(father, function(dad) { Sequelize.Helper.log('my dad: ' + dad.name) })
-     /*person.setBrothers([brother], function(bros) { Sequelize.Helper.log('ma bro: ' + bros[0].name)})
-          person.setSisters([sister], function(sis) { Sequelize.Helper.log('ma sis\': ' + sis[0].name)})*/
+  Sequelize.chainQueries([{save: person}, {save: mother}, {save: father}, {save: brother}, {save: sister}, {save: pet}], function() {
+    person.setMother(mother, function(mom) { Sequelize.Helper.log('my mom: ' + mom.name) })
+    person.setFather(father, function(dad) { Sequelize.Helper.log('my dad: ' + dad.name) })
+    person.setBrothers([brother], function(bros) { Sequelize.Helper.log("ma bro: " + bros[0].name)})
+    person.setSisters([sister], function(sis) { Sequelize.Helper.log("ma sis: " + sis[0].name)})
     person.setPets([pet], function(pets) { Sequelize.Helper.log('my pet: ' + pets[0].name )})
-   })
+  })
 })
