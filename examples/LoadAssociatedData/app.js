5c1ea45c12bbbfd5e33a25c248fe63eefc74961c
Sascha Depold <sascha@depold.com>
2010-09-17 10:16:28 +0200

    example for loading associations

---
+++ examples/LoadAssociatedData/app.js
@@ -0,0 +1,28 @@
+var Sequelize = require(__dirname + "/../../lib/sequelize/Sequelize").Sequelize,
+    sequelize = new Sequelize("sequelize_test", "root", null, {disableLogging: false})
+
+var Person = sequelize.define('person', {
+  name: Sequelize.STRING
+})
+
+var Pet = sequelize.define('pet', {
+  name: Sequelize.STRING
+})
+
+Person.hasManyAndBelongsTo('pets', Pet, 'owner')
+
+Sequelize.chainQueries([{drop: sequelize}, {sync: sequelize}], function() {
+  var person  = new Person({ name: 'Luke' }),
+      pet     = new Pet({ name: 'Bob' })
+
+  Sequelize.chainQueries([{save: person}, {save: pet}], function() {
+    person.setPets([pet], function(pets) {
+      console.log('my pet: ' + pets[0].name )
+      console.log("Now let's get the same data with loadAssociatedData!")
+      person.loadAssociatedData(function(data) {
+        Sequelize.Helper.log("And here we are: " + data.pets[0].name)
+        Sequelize.Helper.log("The object should now also contain the data: " + person.associatedData.pets[0].name)
+      })
+    })
+  })
+})
