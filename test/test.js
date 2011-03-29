fa190e8ad603e57b6e4067777fea4ce510a771ba
Sascha Depold <sascha@depold.com>
2011-03-29 22:29:53 +0200

    adjusted example

--- test/test.js
+++ test/test.js
@@ -3,18 +3,16 @@ var config = require("./config")
   , sequelize = new Sequelize(config.database, config.username, config.password)
   
 var User = sequelize.define('User', {
-  title: Sequelize.STRING,
+  name: Sequelize.STRING,
   bio: Sequelize.TEXT
 })
 
 User.sync({force: true}).on('success', function() {
-  var user = User.build({title: 'barfooz', bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'})
-  user.save.on('success', function(u) {
-    console.log(u)
-    console.log('just saved the user :)')
-    
-    u.destroy.on('success', function() {
-      console.log('just destroyed the user')
+  User
+    .create({name: 'barfooz', bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'})
+    .on('success', function(user) {
+      User.all.on('success', function(users) {
+        console.log(users.map(function(u) { return u.values }))
+      })
     })
-  })
 })
