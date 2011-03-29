7fc980f8f831f7caae6802a9fcf7320408612843
Sascha Depold <sascha@depold.com>
2011-03-29 20:10:21 +0200

    pass the callee to sequelize.query will return it on success and failure

--- test/test.js
+++ test/test.js
@@ -9,8 +9,8 @@ var User = sequelize.define('User', {
 
 User.sync({force: true}).on('success', function() {
   var user = User.build({title: 'barfooz', bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'})
-  console.log(user)
-  user.save.on('success', function() {
+  user.save.on('success', function(u) {
+    console.log(u)
     console.log('just saved the user :)')
   })
 })
