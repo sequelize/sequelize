f88c5951ea39750ab6a65756a8f156248994b0a4
Sascha Depold <sascha@depold.com>
2011-03-30 18:39:39 +0200

    define user globally

--- test/Model/find-findAll-all.js
+++ test/Model/find-findAll-all.js
@@ -2,13 +2,10 @@ var assert = require("assert")
   , config = require("./../config")
   , Sequelize = require("./../../index")
   , sequelize = new Sequelize(config.database, config.username, config.password, {logging: false})
+  , User = sequelize.define('User', { name: Sequelize.STRING, bio: Sequelize.TEXT })
 
 module.exports = {
   'all should return all created models': function(beforeExit) {
-    var User = sequelize.define('User', {
-      name: Sequelize.STRING,
-      bio: Sequelize.TEXT
-    })
     User.sync({force: true}).on('success', function() {
       User.create({name: 'foo', bio: 'foobar'}).on('success', function() {
         User.create({name: 'bar', bio: 'foobar'}).on('success', function() {
@@ -19,5 +16,8 @@ module.exports = {
         })
       })
     })
+  },
+  'find should return a single model': function(beforeExit) {
+    beforeExit()
   }
 }
--- test/Model/sync-drop.js
+++ test/Model/sync-drop.js
@@ -2,28 +2,18 @@ var assert = require("assert")
   , config = require("./../config")
   , Sequelize = require("./../../index")
   , sequelize = new Sequelize(config.database, config.username, config.password, {logging: false})
+  , User = sequelize.define('User', { name: Sequelize.STRING, bio: Sequelize.TEXT })
 
 module.exports = {
   'sync should work with correct database config': function(beforeExit) {
-    var User = sequelize.define('User', {
-      name: Sequelize.STRING,
-      bio: Sequelize.TEXT
-    })
     User.sync().on('success', beforeExit)
   },
   'sync should fail with incorrect database config': function(beforeExit) {
     var s = new Sequelize('foo', 'bar', null, {logging: false})
-    var User = s.define('User', {
-      name: Sequelize.STRING,
-      bio: Sequelize.TEXT
-    })
-    User.sync().on('failure', beforeExit)
+    var User2 = s.define('User', { name: Sequelize.STRING, bio: Sequelize.TEXT })
+    User2.sync().on('failure', beforeExit)
   },
   'drop should work': function(beforeExit) {
-    var User = sequelize.define('User', {
-      name: Sequelize.STRING,
-      bio: Sequelize.TEXT
-    })
     User.drop().on('success', beforeExit)
   }
 }
