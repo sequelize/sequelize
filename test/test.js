17fd3447a8f2efaa97b7965b89f98c606e0bf145
Sascha Depold <sascha@depold.com>
2011-03-30 07:33:04 +0200

    find will return a single record not an array

--- test/test.js
+++ test/test.js
@@ -11,8 +11,11 @@ User.sync({force: true}).on('success', function() {
   User
     .create({name: 'barfooz', bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'})
     .on('success', function(user) {
-      User.all.on('success', function(users) {
-        console.log(users.map(function(u) { return u.values }))
+      // User.findAll().on('success', function(users) {
+      //   console.log('Found the following users', users.map(function(u) { return u.name }))
+      // })
+      User.find(user.id).on('success', function(_user) {
+        console.log('Found the following user', _user)
       })
     })
 })
