78d1f617b315f96aef2a564a7f3aca4ef86255e3
Sascha Depold <sascha@depold.com>
2011-03-30 18:19:20 +0200

    test for on('failure', ...)

--- test/Model/sync.js
+++ test/Model/sync.js
@@ -4,11 +4,19 @@ var assert = require("assert")
   , sequelize = new Sequelize(config.database, config.username, config.password)
 
 module.exports = {
-  'sync should work': function(beforeExit) {
+  'sync should work with correct database config': function(beforeExit) {
     var User = sequelize.define('User', {
       name: Sequelize.STRING,
       bio: Sequelize.TEXT
     })
     User.sync().on('success', beforeExit)
+  },
+  'sync should fail with incorrect database config': function(beforeExit) {
+    var s = new Sequelize('foo', 'bar', null)
+    var User = s.define('User', {
+      name: Sequelize.STRING,
+      bio: Sequelize.TEXT
+    })
+    User.sync().on('failure', beforeExit)
   }
 }
