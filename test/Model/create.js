7720bc1505034eb6f81dc9ec2d4e9755c70d29fb
Sascha Depold <sascha@depold.com>
2011-04-01 18:47:20 +0200

    tests for unique:true

---
+++ test/Model/create.js
@@ -0,0 +1,24 @@
+var assert = require("assert")
+  , config = require("./../config")
+  , Sequelize = require("./../../index")
+  , sequelize = new Sequelize(config.database, config.username, config.password, {logging: false})
+
+var initUsers = function(num, callback) {
+  return sequelize.define('User' + parseInt(Math.random() * 99999999), { name: Sequelize.STRING, bio: Sequelize.TEXT })
+}
+
+module.exports = {
+  'do not allow duplicated records with unique:true': function(exit) {
+    var User = sequelize.define('User' + parseInt(Math.random() * 99999999), {
+       username: {type: Sequelize.STRING, unique: true}
+     })
+     User.sync({force:true}).on('success', function() {
+       User.create({username:'foo'}).on('success', function() {
+          User.create({username: 'foo'}).on('failure', function(err) {
+            assert.eql(err.message, "Duplicate entry 'foo' for key 'username'")
+            exit()
+          })
+        })
+     })
+  }
+}
