607a3b3011d4d66a32d3fdaae06d1d6a7a22954f
Sascha Depold <sascha@depold.com>
2011-04-01 07:27:29 +0200

    test for build

---
+++ test/Model/build.js
@@ -0,0 +1,26 @@
+var assert = require("assert")
+  , config = require("./../config")
+  , Sequelize = require("./../../index")
+  , sequelize = new Sequelize(config.database, config.username, config.password, {logging: false})
+
+var initUsers = function(num, callback) {
+  var User  = sequelize.define('User' + parseInt(Math.random() * 99999999), { name: Sequelize.STRING, bio: Sequelize.TEXT })
+    , users = []
+    
+  User.sync({force: true}).on('success', function() {
+    while(num--) users.push(User.build({name: 'user' + num, bio: 'foobar'}))
+    callback(users, User)
+  })
+}
+
+module.exports = {
+  'build should not create database entries': function(exit) {
+    initUsers(10, function(users, User) {
+      assert.eql(users.length, 10)
+      User.all.on('success', function(users) {
+        assert.eql(users.length, 0)
+        exit()
+      })
+    })
+  }
+}
