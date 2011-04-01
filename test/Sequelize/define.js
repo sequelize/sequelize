ff7794d320b8ac4c82917fee6746c55bc9a45299
Sascha Depold <sascha@depold.com>
2011-04-01 18:30:02 +0200

    added helper in utils for client.escape + added extended data types

--- test/Sequelize/define.js
+++ test/Sequelize/define.js
@@ -7,5 +7,19 @@ module.exports = {
     assert.eql(s.modelManager.all.length, 0)
     s.define('foo', { title: Sequelize.STRING })
     assert.eql(s.modelManager.all.length, 1)
+  },
+  'it should handle extended attributes': function() {
+    var s = new Sequelize('database', 'username', 'password')
+    var User = s.define('User', {
+      name: Sequelize.STRING,
+      username: {type: Sequelize.STRING, unique: true},
+      password: {type: Sequelize.STRING, default: 'password'}
+    })
+    assert.eql(User.attributes, {
+      name:"VARCHAR(255)",
+      username:"VARCHAR(255) UNIQUE",
+      password:"VARCHAR(255) DEFAULT 'password'",
+      id:"INT NOT NULL auto_increment PRIMARY KEY"
+    })
   }
 }
