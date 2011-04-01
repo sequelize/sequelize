f0eb03f2e0615ba594552f35ea08b9e711edfcd1
Sascha Depold <sascha@depold.com>
2011-04-01 18:43:34 +0200

    added allowNull, defaultValue, primaryKey for extended datatypes

--- test/Sequelize/define.js
+++ test/Sequelize/define.js
@@ -1,5 +1,6 @@
 var assert    = require("assert")
   , Sequelize = require("./../../index")
+  , sequelize = new Sequelize('database', 'username', 'password')
 
 module.exports = {
   'it should add a new model to the model-manager': function() {
@@ -8,18 +9,28 @@ module.exports = {
     s.define('foo', { title: Sequelize.STRING })
     assert.eql(s.modelManager.all.length, 1)
   },
-  'it should handle extended attributes': function() {
-    var s = new Sequelize('database', 'username', 'password')
-    var User = s.define('User', {
-      name: Sequelize.STRING,
-      username: {type: Sequelize.STRING, unique: true},
-      password: {type: Sequelize.STRING, default: 'password'}
+  'it should handle extended attributes correctly - unique': function() {
+    var User = sequelize.define('User' + parseInt(Math.random() * 999999999), {
+      username: {type: Sequelize.STRING, unique: true}
     })
-    assert.eql(User.attributes, {
-      name:"VARCHAR(255)",
-      username:"VARCHAR(255) UNIQUE",
-      password:"VARCHAR(255) DEFAULT 'password'",
-      id:"INT NOT NULL auto_increment PRIMARY KEY"
+    assert.eql(User.attributes, {username:"VARCHAR(255) UNIQUE",id:"INT NOT NULL auto_increment PRIMARY KEY"})
+  },
+  'it should handle extended attributes correctly - default': function() {
+    var User = sequelize.define('User' + parseInt(Math.random() * 999999999), {
+      username: {type: Sequelize.STRING, defaultValue: 'foo'}
     })
+    assert.eql(User.attributes, {username:"VARCHAR(255) DEFAULT 'foo'",id:"INT NOT NULL auto_increment PRIMARY KEY"})
+  },
+  'it should handle extended attributes correctly - null': function() {
+    var User = sequelize.define('User' + parseInt(Math.random() * 999999999), {
+      username: {type: Sequelize.STRING, allowNull: false}
+    })
+    assert.eql(User.attributes, {username:"VARCHAR(255) NOT NULL",id:"INT NOT NULL auto_increment PRIMARY KEY"})
+  },
+  'it should handle extended attributes correctly - primary key': function() {
+    var User = sequelize.define('User' + parseInt(Math.random() * 999999999), {
+      username: {type: Sequelize.STRING, primaryKey: true}
+    })
+    assert.eql(User.attributes, {username:"VARCHAR(255) PRIMARY KEY",id:"INT NOT NULL auto_increment PRIMARY KEY"})
   }
 }
