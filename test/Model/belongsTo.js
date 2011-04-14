af7b75bbe13105a2601aa7881a34fc93b66151a6
Sascha Depold <sascha@depold.com>
2011-04-14 07:34:15 +0200

    belongs_to association

---
+++ test/Model/belongsTo.js
@@ -0,0 +1,51 @@
+var assert = require("assert")
+  , config = require("./../config")
+  , Sequelize = require("./../../index")
+  , sequelize = new Sequelize(config.database, config.username, config.password, {logging: false})
+
+module.exports = {
+  'it should correctly add the foreign id': function() {
+    var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING })
+    var Task = sequelize.define('Task' + parseInt(Math.random() * 99999999), { title: Sequelize.STRING })
+    
+    User.belongsTo('task', Task)
+    assert.eql(User.attributes.taskId, "INT")
+  },
+  'it should correctly add the foreign id with underscored': function() {
+    var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING }, {underscored: true})
+    var Task = sequelize.define('Task' + parseInt(Math.random() * 99999999), { title: Sequelize.STRING })
+    
+    User.belongsTo('task', Task)
+    assert.eql(User.attributes.task_id, "INT")
+  },
+  'it should define getter and setter': function() {
+    var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING })
+    var Task = sequelize.define('Task' + parseInt(Math.random() * 99999999), { title: Sequelize.STRING })
+    
+    User.belongsTo('task', Task)
+    
+    var u = User.build({username: 'asd'})
+    console.log(u)
+    assert.isDefined(u.setTask)
+    assert.isDefined(u.getTask)
+  },
+  'it should set and get the correct object': function(exit) {
+    var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING })
+    var Task = sequelize.define('Task' + parseInt(Math.random() * 99999999), { title: Sequelize.STRING })
+    
+    User.belongsTo('task', Task)
+    
+    User.sync({force: true}).on('success', function() {
+      User.create({username: 'asd'}).on('success', function(u) {
+        Task.create({title: 'a task'}).on('success', function(t) {
+          u.setTask(t).on('success', function() {
+            u.getTask().on('success', function(task) {
+              assert.eql(task.title, 'a task')
+              exit()
+            })
+          })
+        })
+      })
+    })
+  }
+}
