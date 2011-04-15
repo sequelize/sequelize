ebcf8ab66625339589bddbebeb87e36c45ac9d55
Sascha Depold <sascha@depold.com>
2011-04-15 07:41:30 +0200

    some first steps for hasOne

--- test/Model/has-one.js
+++ test/Model/has-one.js
@@ -2,27 +2,29 @@ var assert = require("assert")
   , config = require("./../config")
   , Sequelize = require("./../../index")
   , sequelize = new Sequelize(config.database, config.username, config.password, {logging: false})
-return module.exports = {'asd': function(){}}
+
 module.exports = {
   'it should correctly add the foreign id': function() {
-    var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING })
+    var num = parseInt(Math.random() * 99999999)
+    var User = sequelize.define('User' + num, { username: Sequelize.STRING })
     var Task = sequelize.define('Task' + parseInt(Math.random() * 99999999), { title: Sequelize.STRING })
     
-    Task.hasOne('user', User)
-    assert.eql(User.attributes.taskId, "INT")
+    User.hasOne('task', Task)
+    assert.eql(Task.attributes['User'+num+'Id'], "INT")
   },
   'it should correctly add the foreign id with underscore': function() {
-    var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING }, {underscored: true})
+    var num = parseInt(Math.random() * 99999999)
+    var User = sequelize.define('User' + num, { username: Sequelize.STRING }, {underscored: true})
     var Task = sequelize.define('Task' + parseInt(Math.random() * 99999999), { title: Sequelize.STRING })
     
-    User.belongsTo('task', Task)
-    assert.eql(User.attributes.task_id, "INT")
+    User.hasOne('task', Task)
+    assert.eql(Task.attributes['user'+num+'_id'], "INT")
   },
   'it should define getter and setter': function() {
     var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING })
     var Task = sequelize.define('Task' + parseInt(Math.random() * 99999999), { title: Sequelize.STRING })
     
-    User.belongsTo('task', Task)
+    User.hasOne('task', Task)
     
     var u = User.build({username: 'asd'})
     assert.isDefined(u.setTask)
