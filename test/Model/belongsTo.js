4309d20399ede05bea5f27ddaa0ce5fe588f1d65
Sascha Depold <sascha@depold.com>
2011-04-16 19:06:05 +0200

    unstable commit

--- test/Model/belongsTo.js
+++ test/Model/belongsTo.js
@@ -5,17 +5,19 @@ var assert = require("assert")
 
 module.exports = {
   'it should correctly add the foreign id': function() {
-    var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING })
+    var num  = parseInt(Math.random() * 99999999)
+    var User = sequelize.define('User' + num, { username: Sequelize.STRING })
     var Task = sequelize.define('Task' + parseInt(Math.random() * 99999999), { title: Sequelize.STRING })
     
-    Task.belongsTo('user', User)
-    assert.eql(Task.attributes.userId, "INT")
+    Task.belongsTo(User)
+    assert.eql(Task.attributes['User'+num+'Id'], "INT")
   },
   'it should correctly add the foreign id with underscore': function() {
     var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING })
     var Task = sequelize.define('Task' + parseInt(Math.random() * 99999999), { title: Sequelize.STRING }, {underscored: true})
     
     Task.belongsTo('user', User)
+console.log(Task.attributes)
     assert.eql(Task.attributes.user_id, "INT")
   },
   'it should define getter and setter': function() {
@@ -27,6 +29,15 @@ module.exports = {
     var t = Task.build({title: 'asd'})
     assert.isDefined(t.setUser)
     assert.isDefined(t.getUser)
+  },
+  'it should set the foreign id to null': function() {
+    var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING })
+    var Task = sequelize.define('Task' + parseInt(Math.random() * 99999999), { title: Sequelize.STRING })
+    
+    Task.belongsTo('user', User)
+    
+    var t = Task.build({title: 'asd'})
+    assert.isNull(t.userId)
   },
   'it should set and get the correct object': function(exit) {
     var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING })
--- test/Model/has-one.js
+++ test/Model/has-one.js
@@ -29,5 +29,26 @@ module.exports = {
     var u = User.build({username: 'asd'})
     assert.isDefined(u.setTask)
     assert.isDefined(u.getTask)
+  },
+  'it should set and get the correct objects': function(exit) {
+    var User = sequelize.define('User' + parseInt(Math.random() * 99999999), { username: Sequelize.STRING })
+    var Task = sequelize.define('Task' + parseInt(Math.random() * 99999999), { title: Sequelize.STRING })
+    
+    User.hasOne('task', Task)
+    
+    User.sync({force: true}).on('success', function() {
+      Task.sync({force: true}).on('success', function() {
+        User.create({username: 'name'}).on('success', function(user) {
+          Task.create({title: 'snafu'}).on('success', function(task) {
+            user.setTask(task).on('success', function() {
+              user.getTask().on('success', function(task2) {
+                assert.eql(task.title, task2.title)
+                exit(function(){})
+              })
+            })
+          })
+        })
+      })
+    })
   }
 }
