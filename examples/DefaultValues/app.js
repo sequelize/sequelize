9b7282d31508bf75c4b06e71833bd0a727e41620
Sascha Depold <sascha@depold.com>
2011-05-04 07:17:12 +0200

    fixed example

--- examples/DefaultValues/app.js
+++ examples/DefaultValues/app.js
@@ -5,20 +5,24 @@
   you have to pass a hash with a type and a default. You also might want to specify either an attribute can be null or not!
 */
 
-var Sequelize = require(__dirname + "/../../lib/sequelize/Sequelize").Sequelize,
-    sequelize = new Sequelize("sequelize_test", "root", null),
-    User      = sequelize.define('User', {
+var Sequelize = require(__dirname + "/../../index")
+  , config    = require(__dirname + "/../../test/config")
+  , sequelize = new Sequelize(config.database, config.username, config.password, {logging: false})
+
+var User = sequelize.define('User', {
       name: { type: Sequelize.STRING, allowNull: false},
-      isAdmin: { type: Sequelize.BOOLEAN, allowNull: false, default: false }
-    }),
-    user      = new User({ name: 'Someone' })
+      isAdmin: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false }
+    })
+  , user = User.build({ name: 'Someone' })
+
+sequelize.sync({force: true}).on('success', function() {
+  user.save().on('success', function(user) {
+    console.log("user.isAdmin should be the default value (false): ", user.isAdmin)
     
-Sequelize.chainQueries([{drop: User}, {sync: User}], function() {
-  user.save(function(user) {
-    Sequelize.Helper.log("user.isAdmin should be the default value (false): " + user.isAdmin)
-    
-    user.updateAttributes({ isAdmin: true }, function(user) {
-      Sequelize.Helper.log("user.isAdmin was overwritten to true: " + user.isAdmin)
+    user.updateAttributes({ isAdmin: true }).on('success', function(user) {
+      console.log("user.isAdmin was overwritten to true: " + user.isAdmin)
     })
   })
+}).on('failure', function(err) {
+  console.log(err)
 })
