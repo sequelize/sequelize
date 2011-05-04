1d563cffb6219d665bac6d728762e0ee7ae94600
Sascha Depold <sascha@depold.com>
2011-05-04 07:36:46 +0200

    fixed example

--- examples/SequelizeWithOptions/app.js
+++ examples/SequelizeWithOptions/app.js
@@ -1,15 +1,17 @@
-var Sequelize = require(__dirname + "/../../lib/sequelize/Sequelize").Sequelize,
-    sequelize = new Sequelize("sequelize_test", "root", null, {
+var Sequelize = require(__dirname + "/../../index")
+  , config    = require(__dirname + "/../../test/config")
+  , sequelize = new Sequelize(config.database, config.username, config.password, {
       // use other database server or port
       host: 'my.srv.tld',
       port: 12345,
       
       // disable logging
-      disableLogging: true
-    }),
-    Smth      = sequelize.define('Smth', {foo: Sequelize.STRING})
+      logging: false
+    })
+  , Smth      = sequelize.define('Smth', {foo: Sequelize.STRING})
 
-Smth.sync(function(_, err) {
-  if(err) Sequelize.Helper.log(err)
-  else Sequelize.Helper.log('Hey we established the connection successfully! Woot!')
+sequelize.sync({force: true}).on('success', function() {
+  console.log('Hey we established the connection successfully! Woot!')
+}).on('failure', function(err) {
+  console.log(err)
 })
