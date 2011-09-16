d75829c136add80715fa7ea468c63915ece49be2
Sascha Depold <sascha@depold.com>
2011-09-16 21:32:11 +0200

    refactored example

--- examples/Performance/app.js
+++ examples/Performance/app.js
@@ -2,44 +2,43 @@ var Sequelize    = require(__dirname + "/../../index")
   , config       = require("../../test/config")
   , sequelize    = new Sequelize(config.database, config.username, config.password, {logging: false, host: config.host})
   , QueryChainer = Sequelize.Utils.QueryChainer
+  , sys          = require("sys")
 
 var Person = sequelize.define('person', { name: Sequelize.STRING })
 
 Person.sync({force: true}).on('success', function() {
-  var start   = Date.now()
-    , count   = 10000
-    , offset  = 0
-    , stepWidth = 50
-    , queries = []
-    , chainer = new QueryChainer
+  var start = Date.now()
+    , count = 10000
+    , done  = 0
   
-  var perform = function(cb) {
-    console.log("Begin to create " + offset + " - " + (offset+stepWidth) + " items!")
+  var createPeople = function(callback) {
+    Person.create({name: 'someone'}).on('success', callback).on('failure', function(err) { console.log(err) })
+  }
+  var createPeopleBatch = function(batchSize, callback) {
+    var done = 0
     
-    for(var i = offset; i < (offset + stepWidth); i++)
-      chainer.add(Person.create({name: 'someone'}))
+    for(var i = 0; i < batchSize; i++) 
+      createPeople(function() { (++done == batchSize) && callback() })
+  }
+  var batchCallback = function() {
+    sys.print(".");
     
-    chainer.run().on('success', function() {
-      if(count - offset > 0) {
-        offset += stepWidth
-        perform(cb)
-      } else {
-        console.log("Saving " + count + " items took: " + (Date.now() - start) + "ms")
-        cb && cb.call()
-      }
-    }).on('failure', function(err) {
-      console.log(err)
-    })
+    if((done += 50) != count)
+      createPeopleBatch(50, batchCallback)
+    else {
+      console.log("\nFinished creation of " + count + " people. Took: " + (Date.now() - start) + "ms")
+      
+      start = Date.now()
+      console.log("Will now read them from the database:")
+    
+      Person.findAll().on('success', function(people) {
+        console.log("Reading " + people.length + " items took: " + (Date.now() - start) + "ms")
+      })
+    }
   }
   
-  perform(function() {
-    start = Date.now()
-    console.log("Will now read them from the database:")
-
-    Person.findAll().on('success', function(people) {
-      console.log("Reading " + people.length + " items took: " + (Date.now() - start) + "ms")
-    })
-  })
+  console.log('Creating people :)')
+  createPeopleBatch(50, batchCallback)
 }).on('failure', function(err) {
   console.log(err)
 })
