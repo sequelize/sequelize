dc1c1820467ba7106f7cfca4f5ba54e450758aa9
Sascha Depold <sascha@depold.com>
2011-10-24 16:36:11 +0200

    test for options attribute

--- spec/model-definition.spec.js
+++ spec/model-definition.spec.js
@@ -7,21 +7,27 @@ describe('ModelDefinition', function() {
   var sequelize = new Sequelize(config.database, config.username, config.password, { logging: false })
     , User      = sequelize.define('User', { name: Sequelize.STRING, bio: Sequelize.TEXT })
 
-  beforeEach(function() {
-    var createUser  = function(num, cb) {
-    console.log('create user')
-      User.create({name: 'user' + num, bio: 'foobar'}).on('success', function(user){
-        --num ? createUser(num, cb) : cb()
-      })
-    }
-
+  afterEach(function() {
     Helpers.async(function(done) {
-      User.sync({force: true}).on('success', function() { done() })
+      sequelize.drop().on('success', done)
     })
-    Helpers.async(function(done) { createUser(2, done) })
   })
 
   describe('.all', function() {
+    beforeEach(function() {
+      var createUser  = function(num, cb) {
+      console.log('create user')
+        User.create({name: 'user' + num, bio: 'foobar'}).on('success', function(user){
+          --num ? createUser(num, cb) : cb()
+        })
+      }
+
+      Helpers.async(function(done) {
+        User.sync({force: true}).on('success', function() { done() })
+      })
+      Helpers.async(function(done) { createUser(2, done) })
+    })
+
     it("should return all users", function() {
 
       Helpers.async(function(done) {
@@ -31,6 +37,26 @@ describe('ModelDefinition', function() {
         })
       })
 
+    })
+  })
+
+  describe('.create with options', function() {
+    var Person = sequelize.define('Person', { name: Sequelize.STRING, options: Sequelize.TEXT })
+
+    beforeEach(function() {
+      Helpers.async(function(done) {
+        Person.sync({force: true}).on('success', function() { done() })
+      })
+    })
+
+    it('should allow the creation of an object with options as attribute', function() {
+      Helpers.async(function(done) {
+        var options = JSON.stringify({ foo: 'bar', bar: 'foo' })
+        Person.create({name: 'John Doe', options: options}).on('success', function(person) {
+          expect(person.options).toEqual(options)
+          done()
+        })
+      })
     })
   })
 })
