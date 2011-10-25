482b32341c72902d0b91d75d9d2cdb26a4998880
Sascha Depold <sascha@depold.com>
2011-10-25 19:18:00 +0200

    added correct database config for travis

--- spec/config/config.js
+++ spec/config/config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  username: 'root',
+  username: null,
   password: null,
   database: 'sequelize_test',
   host: '127.0.0.1',
-  rand: function() { return parseInt(Math.random() * 99999999999999) }
+  rand: function() {
+    return parseInt(Math.random() * 99999999999999)
+  }
 }
--- test/config.js
+++ test/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    username: 'root'
-  , password: null
-  , database: 'sequelize_test'
-  , host: '127.0.0.1'
-  , rand: function() {
-      return parseInt(Math.random() * 99999999999999)
-    }
+  username: null,
+  password: null,
+  database: 'sequelize_test',
+  host: '127.0.0.1',
+  rand: function() {
+    return parseInt(Math.random() * 99999999999999)
+  }
 }
