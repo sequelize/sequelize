1e1ef9a5177cea493b920d59c67df2b3dce64f1e
Sascha Depold <sascha@depold.com>
2011-10-26 19:59:26 +0200

    fixed has_many association for tables with identical prefix

--- spec/model-definition.spec.js
+++ spec/model-definition.spec.js
@@ -1,5 +1,4 @@
-var should    = require("should")
-  , config    = require("./config/config")
+var config    = require("./config/config")
   , Helpers   = require("./config/helpers")
   , Sequelize = require("../index")
 
@@ -7,51 +6,48 @@ describe('ModelDefinition', function() {
   var sequelize = new Sequelize(config.database, config.username, config.password, { logging: false })
     , User      = sequelize.define('User', { name: Sequelize.STRING, bio: Sequelize.TEXT })
 
+  beforeEach(function() {
+    Helpers.async(function(done) {
+      sequelize.sync({force: true}).on('success', done).on('failure', function(err) { console.log(err) })
+    })
+  })
+
   afterEach(function() {
     Helpers.async(function(done) {
       sequelize.drop().on('success', done).on('failure', function(err) { console.log(err) })
     })
   })
 
+  //////////// all //////////////
+
   describe('.all', function() {
     beforeEach(function() {
-      var createUser  = function(num, cb) {
-      console.log('create user')
-        User.create({name: 'user' + num, bio: 'foobar'}).on('success', function(user){
-          --num ? createUser(num, cb) : cb()
+      var UserFactory = function(options, callback, count) {
+        count = count || 1
+
+        User.create(options).on('success', function(user){
+          --count ? UserFactory(options, callback, count) : callback(user)
+        }).on('failure', function(err) {
+          console.log(err)
         })
       }
-
-      Helpers.async(function(done) {
-        User.sync({force: true})
-            .on('success', function() { done() })
-            .on('failure', function(err) { console.log(err) })
-      })
-      Helpers.async(function(done) { createUser(2, done) })
+      Helpers.async(function(done) { UserFactory({name: 'user', bio: 'foobar'}, done, 2) })
     })
 
     it("should return all users", function() {
-
       Helpers.async(function(done) {
         User.all.on('success', function(users) {
           done()
           expect(users.length).toEqual(2)
         }).on('failure', function(err) { console.log(err) })
       })
-
     })
   })
 
+  /////////// create ////////////
+
   describe('.create with options', function() {
     var Person = sequelize.define('Person', { name: Sequelize.STRING, options: Sequelize.TEXT })
-
-    beforeEach(function() {
-      Helpers.async(function(done) {
-        Person.sync({force: true})
-              .on('success', function() { done() })
-              .on('failure', function(err) { console.log(err) })
-      })
-    })
 
     it('should allow the creation of an object with options as attribute', function() {
       Helpers.async(function(done) {
@@ -60,6 +56,25 @@ describe('ModelDefinition', function() {
           expect(person.options).toEqual(options)
           done()
         }).on('failure', function(err) { console.log(err) })
+      })
+    })
+  })
+
+  /////////// many-to-many with same prefix ////////////
+
+  describe('many-to-many', function() {
+    describe('where tables have the same prefix', function() {
+      var Table2 = sequelize.define('wp_table2', {foo: Sequelize.STRING})
+        , Table1 = sequelize.define('wp_table1', {foo: Sequelize.STRING})
+
+      Table1.hasMany(Table2)
+      Table2.hasMany(Table1)
+
+      it("should create a table wp_table1wp_table2s", function() {
+        var models = sequelize.modelManager.models.filter(function(model) {
+          return model.tableName.indexOf('wp_table1swp_table2s') > -1
+        })
+        expect(models.length).toBe(1)
       })
     })
   })
