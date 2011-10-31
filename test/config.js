b35685ceaf87e1dc0c4b9403e626d9e5b7b19ca5
Sascha Depold <sascha@depold.com>
2011-10-31 20:04:15 +0100

    fixed travis

--- test/config.js
+++ test/config.js
@@ -4,6 +4,6 @@ module.exports = {
   database: 'sequelize_test',
   host: '127.0.0.1',
   rand: function() {
-    return parseInt(Math.random() * 99999999999999)
+    return parseInt(Math.random() * 9999999)
   }
 }
