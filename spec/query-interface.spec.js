86b36804a82d5750e40e01b0469713ff6e6557d8
sdepold <sascha@depold.com>
2011-11-28 06:26:05 +0100

    spec for dropAllTables

---
+++ spec/query-interface.spec.js
@@ -0,0 +1,53 @@
+var config         = require("./config/config")
+  , Sequelize      = require("../index")
+  , sequelize      = new Sequelize(config.database, config.username, config.password, { logging: false })
+  , Helpers        = new (require("./config/helpers"))(sequelize)
+  , QueryInterface = require("../lib/query-interface")
+
+describe('QueryInterface', function() {
+  describe('dropAllTables', function() {
+    var interface = null
+
+    beforeEach(function() {
+      interface = sequelize.getQueryInterface()
+      Helpers.async(function(done) {
+        interface.dropAllTables().success(done).error(function(err) { console.log(err) })
+      })
+    })
+
+    afterEach(function() { interface = null })
+
+    it("should drop all tables", function() {
+      Helpers.async(function(done) {
+        interface.showAllTables().success(function(tableNames) {
+          expect(tableNames.length).toEqual(0)
+          done()
+        })
+      })
+
+      Helpers.async(function(done) {
+        interface.createTable('table', { name: Sequelize.STRING })
+          .success(done)
+          .error(function(err){ console.log(err)})
+      })
+
+      Helpers.async(function(done) {
+        interface.showAllTables().success(function(tableNames) {
+          expect(tableNames.length).toEqual(1)
+          done()
+        })
+      })
+
+      Helpers.async(function(done) {
+        interface.dropAllTables().success(done).error(function(err) { console.log(err) })
+      })
+
+      Helpers.async(function(done) {
+        interface.showAllTables().success(function(tableNames) {
+          expect(tableNames.length).toEqual(0)
+          done()
+        })
+      })
+    })
+  })
+})
