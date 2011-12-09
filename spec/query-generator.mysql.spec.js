905f7a55928e4543a25fa1122982da4f9aadf89a
sdepold <sascha@depold.com>
2011-12-09 06:25:31 +0100

    refactored specs

--- spec/query-generator.mysql.spec.js
+++ spec/query-generator.mysql.spec.js
@@ -1,30 +1,48 @@
-var config    = require("./config/config")
-  , Sequelize = require("../index")
-  , sequelize = new Sequelize(config.database, config.username, config.password, { logging: false })
-  , Helpers   = new (require("./config/helpers"))(sequelize)
+var config         = require("./config/config")
+  , Sequelize      = require("../index")
+  , sequelize      = new Sequelize(config.database, config.username, config.password, { logging: false })
+  , Helpers        = new (require("./config/helpers"))(sequelize)
   , QueryGenerator = require("../lib/connectors/mysql/query-generator")
+  , util          = require("util")
 
 describe('QueryGenerator', function() {
   beforeEach(function() { Helpers.sync() })
   afterEach(function() { Helpers.drop() })
 
-  describe('hashToWhereConditions', function() {
-    it("should correctly transform array into IN", function() {
-      expect(
-        QueryGenerator.hashToWhereConditions({ id: [1,2,3] })
-      ).toEqual(
-        "`id` IN (1,2,3)"
-      )
+  var suites = {
+    'hashToWhereConditions': [
+      {
+        arguments: [{ id: [1,2,3] }],
+        expectation: "`id` IN (1,2,3)"
+      }
+    ],
+    'selectQuery': [
+      {
+        arguments: ['foo', { attributes: [['count(*)', 'count']] }],
+        expectation: 'SELECT count(*) as `count` FROM `foo`;'
+      }
+    ]
+  }
+
+  Sequelize.Utils._.each(suites, function(tests, suiteTitle) {
+    describe(suiteTitle, function() {
+      tests.forEach(function(test) {
+        var title = 'correctly returns ' + test.expectation + ' for ' + test.arguments
+        it(title, function() {
+          var conditions = QueryGenerator[suiteTitle].apply(null, test.arguments)
+          expect(conditions).toEqual(test.expectation)
+        })
+      })
     })
   })
 
-  describe('selectQuery', function() {
-    it("should correctly convert arrays into aliases", function() {
-      expect(
-        QueryGenerator.selectQuery('foo', { attributes: [['count(*)', 'count']] })
-      ).toEqual(
-        'SELECT count(*) as `count` FROM `foo`;'
-      )
-    })
-  })
+  // //describe('addIndexQuery', function() {
+  // //  it("only returns the basics if only necessary parameters are passed", function() {
+  // //    expect(
+  // //      QueryGenerator.addIndexQuery('User', ['username', 'isAdmin'])
+  // //    ).toEqual(
+  // //      'CREATE INDEX user_username_is_admin ON User username, isAdmin'
+  // //    )
+  // //  })
+  // //})
 })
