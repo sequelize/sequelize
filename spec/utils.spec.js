d407fdfc3089021eda688a8b147a16fb4b2fcbdb
sdepold <sascha@depold.com>
2011-12-14 17:08:51 +0100

    moved utils specs

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -36,4 +36,34 @@ describe('Utils', function() {
       expect(result).toMatch(/.*alert\(2\).*/)
     })
   })
+
+  describe('underscore', function() {
+    describe('underscoredIf', function() {
+      it('is defined', function() {
+        expect(Utils._.underscoredIf).toBeDefined()
+      })
+
+      it('underscores if second param is true', function() {
+        expect(Utils._.underscoredIf('fooBar', true)).toEqual('foo_bar')
+      })
+
+      it("doesn't underscore if second param is false", function() {
+        expect(Utils._.underscoredIf('fooBar', false)).toEqual('fooBar')
+      })
+    })
+
+    describe('camelizeIf', function() {
+      it('is defined', function() {
+        expect(Utils._.camelizeIf).toBeDefined()
+      })
+
+      it('camelizes if second param is true', function() {
+        expect(Utils._.camelizeIf('foo_bar', true)).toEqual('fooBar')
+      })
+
+      it("doesn't camelize if second param is false", function() {
+        expect(Utils._.underscoredIf('fooBar', true)).toEqual('foo_bar')
+      })
+    })
+  })
 })
