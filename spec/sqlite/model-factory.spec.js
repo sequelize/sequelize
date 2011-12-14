4b85088b0dfaaf106876d813e6f4052905168b2e
sdepold <sascha@depold.com>
2011-12-14 06:46:14 +0100

    more sqlite specs

--- spec/sqlite/model-factory.spec.js
+++ spec/sqlite/model-factory.spec.js
@@ -37,70 +37,88 @@ describe('ModelFactory', function() {
         }).error(function(err){ console.log(err) })
       })
     })
+
+    it('should allow the creation of an object with options as attribute', function() {
+      var Person = sequelize.define('Person', {
+        name: Sequelize.STRING,
+        options: Sequelize.TEXT
+      })
+
+      Helpers.async(function(done) {
+        Person.sync({force: true}).success(done)
+      })
+
+      Helpers.async(function(done) {
+        var options = JSON.stringify({ foo: 'bar', bar: 'foo' })
+        Helpers.Factories.Model('Person', {
+          name: 'John Doe',
+          options: options
+        }, function(people) {
+          expect(people[0].options).toEqual(options)
+          done()
+        })
+      })
+    })
   })
 
 
   ////////// find //////////////
-//
-  // describe('.find', function() {
-    // beforeEach(function() {
-      // Helpers.Factories.User({name: 'user', bio: 'foobar'}, null, 2)
-    // })
-//
-    // it("should make aliased attributes available", function() {
-      // Helpers.async(function(done) {
-        // User.find({ where: 'id = 1', attributes: ['id', ['name', 'username']] }).success(function(user) {
-          // expect(user.username).toEqual('user')
-          // done()
-        // })
-      // })
-    // })
-  // })
-//
+
+  describe('.find', function() {
+    beforeEach(function() {
+      Helpers.Factories.User({name: 'user', bio: 'foobar'}, null, 2)
+    })
+
+    it("finds normal lookups", function() {
+      Helpers.async(function(done) {
+        User.find({ where: { name:'user' } }).success(function(user) {
+          expect(user.name).toEqual('user')
+          done()
+        })
+      })
+    })
+
+    it("should make aliased attributes available", function() {
+      Helpers.async(function(done) {
+        User.find({ where: { name:'user' }, attributes: ['id', ['name', 'username']] }).success(function(user) {
+          expect(user.username).toEqual('user')
+          done()
+        })
+      })
+    })
+  })
+
   ////////// all //////////////
-//
-  // describe('.all', function() {
-    // beforeEach(function() {
-      // Helpers.Factories.User({name: 'user', bio: 'foobar'}, null, 2)
-    // })
-//
-    // it("should return all users", function() {
-      // Helpers.async(function(done) {
-        // User.all().on('success', function(users) {
-          // done()
-          // expect(users.length).toEqual(2)
-        // }).on('failure', function(err) { console.log(err) })
-      // })
-    // })
-  // })
-//
-  ///////// create ////////////
-//
-  // describe('.create with options', function() {
-    // var Person = sequelize.define('Person', { name: Sequelize.STRING, options: Sequelize.TEXT })
-//
-    // it('should allow the creation of an object with options as attribute', function() {
-      // var options = JSON.stringify({ foo: 'bar', bar: 'foo' })
-      // Helpers.Factories.Model('Person', {name: 'John Doe', options: options}, function(people) {
-        // expect(people[0].options).toEqual(options)
-      // })
-    // })
-  // })
-//
+
+  describe('.all', function() {
+    beforeEach(function() {
+      Helpers.Factories.User({name: 'user', bio: 'foobar'}, null, 2)
+    })
+
+    it("should return all users", function() {
+      Helpers.async(function(done) {
+        User.all().on('success', function(users) {
+          done()
+          expect(users.length).toEqual(2)
+        }).on('failure', function(err) { console.log(err) })
+      })
+    })
+  })
+
   ////////// min //////////////
-//
+
   // describe('.min', function() {
-    // it("should return the min value", function() {
-      // for(var i = 2; i < 5; i++) Helpers.Factories.User({ age: i })
-//
-      // Helpers.async(function(done) {
-        // User.min('age').on('success', function(min) {
-          // expect(min).toEqual(2); done()
-        // })
-      // })
-    // })
+  //   it("should return the min value", function() {
+  //     for(var i = 2; i < 5; i++) Helpers.Factories.User({ age: i })
+
+  //     Helpers.async(function(done) {
+  //       User.min('age').on('success', function(min) {
+  //         expect(min).toEqual(2); done()
+  //       })
+  //     })
+  //   })
   // })
-//
+
   ////////// max //////////////
 //
   // describe('.max', function() {
