01f3180c2866f0344cd4911729f05c06fc98e25e
sdepold <sascha@depold.com>
2011-12-14 17:08:44 +0100

    use jasmine for tests instead of expresso

---
+++ spec/mysql/connector-manager.spec.js
@@ -0,0 +1,38 @@
+var config    = require("../config/config")
+  , Sequelize = require("../../index")
+  , sequelize = new Sequelize(config.database, config.username, config.password, { logging: false })
+  , Helpers   = new (require("../config/helpers"))(sequelize)
+
+describe('ConnectorManager', function() {
+  beforeEach(function() {
+    Helpers.dropAllTables()
+  })
+
+  afterEach(function() {
+    Helpers.dropAllTables()
+  })
+
+  it('works correctly after being idle', function() {
+    var User = sequelize.define('User', { username: Sequelize.STRING })
+
+    Helpers.async(function(done) {
+      User.sync({force: true}).on('success', function() {
+        User.create({username: 'user1'}).on('success', function() {
+          User.count().on('success', function(count) {
+            expect(count).toEqual(1)
+            done()
+          })
+        })
+      })
+    })
+
+    Helpers.async(function(done) {
+      setTimeout(function() {
+        User.count().on('success', function(count) {
+          expect(count).toEqual(1)
+          done()
+        })
+      }, 1000)
+    })
+  })
+})
