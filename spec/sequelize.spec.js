5b96044b727607def9d24cb519d0e05144fe1d1d
sdepold <sascha@depold.com>
2011-12-15 06:30:57 +0100

    moved specs to jasmine

--- spec/sequelize.spec.js
+++ spec/sequelize.spec.js
@@ -1,12 +1,17 @@
-var config    = require("./config/config")
-  , Sequelize = require("../index")
+var config         = require("./config/config")
+  , Sequelize      = require("../index")
+  , QueryInterface = require("../lib/query-interface")
 
 describe('Sequelize', function() {
   var sequelize = null
+    , Helpers   = null
 
 
   var setup = function(options) {
+    options   = options || {logging: false}
     sequelize = new Sequelize(config.database, config.username, config.password, options)
+    Helpers   = new (require("./config/helpers"))(sequelize)
+
     return options
   }
 
@@ -34,6 +39,28 @@ describe('Sequelize', function() {
       expect(sequelize.modelManager.all.length).toEqual(0)
       sequelize.define('foo', { title: Sequelize.STRING })
       expect(sequelize.modelManager.all.length).toEqual(1)
+    })
+  })
+
+  describe('sync', function() {
+    it("synchronizes all models", function() {
+      var Project = sequelize.define('project' + config.rand(), { title: Sequelize.STRING })
+      var Task = sequelize.define('task' + config.rand(), { title: Sequelize.STRING })
+
+      Helpers.async(function(done) {
+        sequelize.sync().success(function() {
+          Project.create({title: 'bla'}).success(function() {
+            Task.create({title: 'bla'}).success(done)
+          })
+        })
+      })
+    })
+  })
+
+  describe('import', function() {
+    it("imports a model definition from a file", function() {
+      var Project = sequelize.import(__dirname + "/assets/project")
+      expect(Project).toBeDefined()
     })
   })
 })
