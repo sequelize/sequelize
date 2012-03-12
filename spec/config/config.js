124920a6989d56fa6d39e7f787d2f52dc8ba51eb
Meg Sharkey <meg@metamarketsgroup.com>
2012-03-12 09:39:01 -0700

    added test

--- spec/config/config.js
+++ spec/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  username: "root",
-  password: null,
-  database: 'sequelize_test',
+  username: 'meg', //"root",
+  password: 'meg', //null,
+  database: 'test', //'sequelize_test',
   host: '127.0.0.1',
   rand: function() {
     return parseInt(Math.random() * 999)
