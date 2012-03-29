ecb9eb227a20afefc2309fbf105e825485cd49a7
sdepold <sascha@depold.com>
2012-03-29 07:57:52 +0200

    added missing config o0

---
+++ spec-jasmine/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  username: "root",
+  password: null,
+  database: 'sequelize_test',
+  host: '127.0.0.1',
+  rand: function() {
+    return parseInt(Math.random() * 999)
+  }
+}
+
