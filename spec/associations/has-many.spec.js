6af88d03f973c4002ff2a24c026fc8c9209d4973
sdepold <sascha@depold.com>
2012-07-01 11:13:52 +0200

    refactored tests

--- spec/associations/has-many.spec.js
+++ spec/associations/has-many.spec.js
@@ -1,148 +1,126 @@
-if(typeof require === 'function') {
+if (typeof require === 'function') {
   const buster    = require("buster")
       , Sequelize = require("../../index")
       , config    = require("../config/config")
-      , sequelize = new Sequelize(config.database, config.username, config.password, {logging: false})
+      , sequelize = new Sequelize(config.database, config.username, config.password, {
+          logging: false
+        })
 }
 
 buster.spec.expose()
 buster.testRunner.timeout = 500
 
-describe('Testing \'has\' function', function() {
+describe('Associations', function() {
   before(function(done) {
     var self = this
 
-    sequelize
-      .getQueryInterface()
+    sequelize.getQueryInterface()
       .dropAllTables()
-      .success(function() {
-        done()
-      })
+      .success(done)
       .error(function(err) { console.log(err) })
   }),
-  
-  describe('using Article and Labels', function() {
-    before(function(done) {
-      var self = this
-      
-      this.Article = sequelize.define('Article', {
-        'title': Sequelize.STRING
+
+  describe('hasMany', function() {
+    describe('hasAssociation', function() {
+      before(function(done) {
+        var self = this
+
+        this.Article = sequelize.define('Article', { 'title': Sequelize.STRING })
+        this.Label = sequelize.define('Label', { 'text': Sequelize.STRING })
+
+        this.Article.hasMany(this.Label)
+
+        sequelize.sync({ force: true }).success(done)
       })
-      this.Label = sequelize.define('Label', {
-        'text': Sequelize.STRING
-      })
-      
-      this.Article.hasMany(this.Label);
-      
-      this.Article.sync({ force: true }).success(function() {
-        self.Label.sync({ force: true }).success(done).error(function(err) {
-          console.log(err)
+
+      describe('hasLabel', function() {
+        it('does not have any labels assigned to it initially', function(done) {
+          var self = this
+
+          var chainer = new Sequelize.Utils.QueryChainer([
+            this.Article.create({ title: 'Article' }),
+            this.Label.create({ text: 'Awesomeness' }),
+            this.Label.create({ text: 'Epicness' })
+          ])
+
+          chainer.run().success(function(results, article, label1, label2) {
+            var chainer = new Sequelize.Utils.QueryChainer([
+              article.hasLabel(label1),
+              article.hasLabel(label2)
+            ])
+
+            chainer.run().success(function(_, hasLabel1, hasLabel2) {
+              expect(hasLabel1).toBeFalse()
+              expect(hasLabel2).toBeFalse()
+              done()
+            })
+          })
         })
-      }).error(function(err) {
-        console.log(err)
+
+        it('answers true if the label has been assigned', function(done) {
+          var self = this
+
+          var chainer = new Sequelize.Utils.QueryChainer([
+            this.Article.create({ title: 'Article' }),
+            this.Label.create({ text: 'Awesomeness' }),
+            this.Label.create({ text: 'Epicness' })
+          ])
+
+          chainer.run().success(function(results, article, label1, label2) {
+            var chainer = new Sequelize.Utils.QueryChainer([
+              [ article, 'addLabel', [ label1 ]],
+              [ article, 'hasLabel', [ label1 ]],
+              [ article, 'hasLabel', [ label2 ]]
+            ])
+
+            chainer.runSerially().success(function(_, label1, hasLabel1, hasLabel2) {
+              expect(hasLabel1).toBeTrue()
+              expect(hasLabel2).toBeFalse()
+              done()
+            })
+          })
+        })
       })
-    }),
-    
-	 describe('hasSingle', function() {
-		it('does not have any labels assigned to it initially', function(done) {
-			var self = this;
-			
-			this.Article.create({
-				title: 'Article'
-			}).success(function(article) {
-				self.Label.create({
-					text: 'Awesomeness'
-				}).success(function(label1) {
-					self.Label.create({
-						text: 'Epicness'
-					}).success(function(label2) {
-						article.hasLabel(label1).success(function(result) {
-							expect(result).toBeFalse();
-							
-							article.hasLabel(label2).success(function(result) {
-								expect(result).toBeFalse();
-								done();
-							});
-						});
-					});
-				});
-			});
-		 }),
-		 
-		 it('only answers true if the label has been assigned', function(done) {
-			var self = this;
-			
-			this.Article.create({
-				title: 'Article'
-			}).success(function(article) {
-				self.Label.create({
-					text: 'Awesomeness'
-				}).success(function(label1) {
-					self.Label.create({
-						text: 'Epicness'
-					}).success(function(label2) {
-						article.addLabel(label1).success(function() {
-							article.hasLabel(label1).success(function(result) {
-								expect(result).toBeTrue();
-								
-								article.hasLabel(label2).success(function(result) {
-									expect(result).toBeFalse();
-									done();
-								});
-							});
-						})
-					});
-				});
-			});
-		 })
-	 }),
-	 
-	 describe('hasAll', function() {
-		it('answers false if only some labels have been assigned', function(done) {
-			var self = this;
-			
-			this.Article.create({
-				title: 'Article'
-			}).success(function(article) {
-				self.Label.create({
-					text: 'Awesomeness'
-				}).success(function(label1) {
-					self.Label.create({
-						text: 'Epicness'
-					}).success(function(label2) {
-						article.addLabel(label1).success(function() {
-							article.hasLabels([label1, label2]).success(function(result) {
-								expect(result).toBeFalse();
-								done();
-							});
-						})
-					});
-				});
-			});
-		 }),
-		 
-		it('answers true if all label have been assigned', function(done) {
-			var self = this;
-			
-			this.Article.create({
-				title: 'Article'
-			}).success(function(article) {
-				self.Label.create({
-					text: 'Awesomeness'
-				}).success(function(label1) {
-					self.Label.create({
-						text: 'Epicness'
-					}).success(function(label2) {
-						article.setLabels([label1, label2]).success(function() {
-							article.hasLabels([label1, label2]).success(function(result) {
-								expect(result).toBeTrue();
-								done();
-							});
-						})
-					});
-				});
-			});
-		 })
-	 });
+
+      describe('hasLabels', function() {
+        it('answers false if only some labels have been assigned', function(done) {
+          var self = this
+
+          var chainer = new Sequelize.Utils.QueryChainer([
+            this.Article.create({ title: 'Article' }),
+            this.Label.create({ text: 'Awesomeness' }),
+            this.Label.create({ text: 'Epicness' })
+          ])
+
+          chainer.run().success(function(results, article, label1, label2) {
+            article.addLabel(label1).success(function() {
+              article.hasLabels([label1, label2]).success(function(result) {
+                expect(result).toBeFalse()
+                done()
+              })
+            })
+          })
+        })
+
+        it('answers true if all label have been assigned', function(done) {
+          var self = this
+
+          var chainer = new Sequelize.Utils.QueryChainer([
+            this.Article.create({ title: 'Article' }),
+            this.Label.create({ text: 'Awesomeness' }),
+            this.Label.create({ text: 'Epicness' })
+          ])
+
+          chainer.run().success(function(results, article, label1, label2) {
+            article.setLabels([label1, label2]).success(function() {
+              article.hasLabels([label1, label2]).success(function(result) {
+                expect(result).toBeTrue()
+                done()
+              })
+            })
+          })
+        })
+      })
+    })
   })
 })
