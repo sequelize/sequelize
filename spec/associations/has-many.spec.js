077242c0bec00dff9e93f7c8f99101a527eb5c3a
sdepold <sascha@depold.com>
2012-07-03 20:06:18 +0200

    clearing associations for many to many is working

--- spec/associations/has-many.spec.js
+++ spec/associations/has-many.spec.js
@@ -146,6 +146,8 @@ describe('HasMany', function() {
     })
 
     it("adds three items to the query chainer when calling sync", function() {
+      sequelize.daoFactoryManager.daos = []
+
       var User = sequelize.define('User', { username: Sequelize.STRING })
         , Task = sequelize.define('Task', { title: Sequelize.STRING })
 
@@ -160,44 +162,28 @@ describe('HasMany', function() {
       expect(add).toHaveBeenCalledThrice()
     })
 
-    it("=>clears associations when passing null to the set-method", function(done) {
+    it("clears associations when passing null to the set-method", function(done) {
       var User = sequelize.define('User', { username: Sequelize.STRING })
         , Task = sequelize.define('Task', { title: Sequelize.STRING })
 
       User.hasMany(Task)
       Task.hasMany(User)
 
-      console.log(sequelize.daoFactoryManager.daos.length)
-
       sequelize.sync({ force: true }).success(function() {
-        console.log('sync ok')
         User.create({ username: 'foo' }).success(function(user) {
-          console.log('user was created')
           Task.create({ title: 'task' }).success(function(task) {
-            console.log('task was created')
             task.setUsers([ user ]).success(function() {
-                console.log('set users done')
+              task.getUsers().success(function(_users) {
+                expect(_users.length).toEqual(1)
 
-                task.getUsers().success(function(_users) {
-                  console.log(_users.length)
-                  expect(_users.length).toEqual(1)
-
-                  task.setUsers(null).success(function() {
-
-                    task.getUsers().success(function(_users) {
-                      expect(_users.length).toEqual(0)
-                      done()
-                    })
-                  }).error(function(err) {
-                    console.log(err)
+                task.setUsers(null).success(function() {
+                  task.getUsers().success(function(_users) {
+                    expect(_users.length).toEqual(0)
+                    done()
                   })
-                }).error(function(err) {
-                  console.log(err)
                 })
               })
-              .error(function(err) {
-                console.log(err)
-              })
+            })
           })
         })
       })
