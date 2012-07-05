2dec5888c335873204f54cf5c0c30c811f261347
sdepold <sascha@depold.com>
2012-07-05 21:13:04 +0200

    minor refactoring + tests for escaping

--- spec/dao-factory.spec.js
+++ spec/dao-factory.spec.js
@@ -12,104 +12,95 @@ dialects.forEach(function(dialect) {
     before(function(done) {
       var self = this
 
-      this.sequelize = new Sequelize(config.database, config.username, config.password, {
-        logging: false
-      })
-
-      this.User = this.sequelize.define('User', {
-        username: Sequelize.STRING,
-        secretValue: Sequelize.STRING
-      })
-
-      self.sequelize
+      this.sequelize = new Sequelize(config.database, config.username, config.password, { logging: false })
+      this.sequelize
         .getQueryInterface()
         .dropAllTables()
         .success(function() {
           self.sequelize.daoFactoryManager.daos = []
-          self.User
-            .sync({ force: true })
-            .success(done)
-            .error(function(err) {
-              console.log(err)
-            })
+          done()
         })
         .error(function(err) { console.log(err) })
     })
 
-    describe('create with whitelist', function() {
-      before(function() {
-        this.data = {
-          username: 'Peter',
-          secretValue: '42'
-        }
+    describe('create', function() {
+      before(function(done) {
+        this.User = this.sequelize.define('User', {
+          username: Sequelize.STRING,
+          secretValue: Sequelize.STRING,
+          data: Sequelize.STRING
+        })
+
+        this.User
+          .sync({ force: true })
+          .success(done)
+          .error(function(err) { console.log(err) })
       })
 
       it('should only store the values passed in the witelist', function(done) {
-        var self = this;
+        var self = this
+          , data = { username: 'Peter', secretValue: '42' }
 
-        this.User.create(this.data, ['username']).success(function(user) {
+        this.User.create(data, ['username']).success(function(user) {
           self.User.find(user.id).success(function(_user) {
-            expect(_user.username).toEqual(self.data.username);
-            expect(_user.secretValue).not.toEqual(self.data.secretValue);
-            expect(_user.secretValue).toEqual(null);
-            done();
+            expect(_user.username).toEqual(data.username)
+            expect(_user.secretValue).not.toEqual(data.secretValue)
+            expect(_user.secretValue).toEqual(null)
+            done()
           })
         })
       })
 
       it('should store all values if no whitelist is specified', function(done) {
-        var self = this;
+        var self = this
+          , data = { username: 'Peter', secretValue: '42' }
 
-        this.User.create(this.data).success(function(user) {
+        this.User.create(data).success(function(user) {
           self.User.find(user.id).success(function(_user) {
-            expect(_user.username).toEqual(self.data.username);
-            expect(_user.secretValue).toEqual(self.data.secretValue);
-            done();
+            expect(_user.username).toEqual(data.username)
+            expect(_user.secretValue).toEqual(data.secretValue)
+            done()
           })
         })
       })
 
-      describe('handle quoted data', function() {
+      it('saves data with single quote', function(done) {
+        var quote = "single'quote"
+          , self  = this
 
-        it('saves data with single quote', function() {
-          setup({ data: {type: Sequelize.STRING} })
-          var quote = "single'quote"
-          Helpers.async(function(done) {
-            User.create({ data: quote}).success(function(user) {
-              expect(user.data).toEqual(quote, 'memory single quote')
-              User.find({where: { id: user.id }}).success(function(user) {
-                expect(user.data).toEqual(quote, 'SQL single quote')
-                done()
-              })
-            })
+        this.User.create({ data: quote }).success(function(user) {
+          expect(user.data).toEqual(quote, 'memory single quote')
+
+          self.User.find({where: { id: user.id }}).success(function(user) {
+            expect(user.data).toEqual(quote, 'SQL single quote')
+            done()
           })
         })
+      })
 
-        it('saves data with double quote', function() {
-          setup({ data: {type: Sequelize.STRING} })
-          var quote = 'double"quote'
-          Helpers.async(function(done) {
-            User.create({ data: quote}).success(function(user) {
-              expect(user.data).toEqual(quote, 'memory double quote')
-              User.find({where: { id: user.id }}).success(function(user) {
-                expect(user.data).toEqual(quote, 'SQL double quote')
-                done()
-              })
-            })
+      it('saves data with double quote', function(done) {
+        var quote = 'double"quote'
+          , self  = this
+
+        this.User.create({ data: quote }).success(function(user) {
+          expect(user.data).toEqual(quote, 'memory double quote')
+
+          self.User.find({where: { id: user.id }}).success(function(user) {
+            expect(user.data).toEqual(quote, 'SQL double quote')
+            done()
           })
         })
+      })
 
-        it('saves stringified JSON data', function() {
-          setup({ data: {type: Sequelize.STRING} })
-          var json = JSON.stringify({ key: 'value' })
-          Helpers.async(function(done) {
-            User.create({ data: json}).success(function(user) {
-              expect(user.data).toEqual(json, 'memory data')
-              User.find({where: { id: user.id }}).success(function(user) {
-                expect(user.data).toEqual(json, 'SQL data')
-                done()
-              })
-            })
+      it('saves stringified JSON data', function(done) {
+        var json = JSON.stringify({ key: 'value' })
+          , self = this
+
+        this.User.create({ data: json }).success(function(user) {
+          expect(user.data).toEqual(json, 'memory data')
+          self.User.find({where: { id: user.id }}).success(function(user) {
+            expect(user.data).toEqual(json, 'SQL data')
+            done()
           })
         })
       })
