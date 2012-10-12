c2d29dbe9326afaf23024a707fa979dfe1ec5a93
Sascha Depold <sascha@depold.com>
2012-10-12 21:42:53 +0200

    exec specs for all dialects

--- spec/associations/mixin.spec.js
+++ spec/associations/mixin.spec.js
@@ -2,29 +2,33 @@ if (typeof require === 'function') {
   const buster    = require("buster")
       , Helpers   = require('../buster-helpers')
       , Sequelize = require('../../index')
+      , dialects  = Helpers.getSupportedDialects()
 }
 
 buster.spec.expose()
 
-describe('Mixin', function() {
-  before(function(done) {
-    Helpers.initTests({
-      beforeComplete: function(sequelize) {
-        this.sequelize = sequelize
-      }.bind(this),
-      onComplete: done
+dialects.forEach(function(dialect) {
+  describe('Mixin@' + dialect, function() {
+    before(function(done) {
+      Helpers.initTests({
+        dialect: dialect,
+        beforeComplete: function(sequelize) {
+          this.sequelize = sequelize
+        }.bind(this),
+        onComplete: done
+      })
     })
-  })
 
-  describe('getAssociation', function() {
-    it('returns the respective part of the association for 1:1 associations', function() {
-      var User = this.sequelize.define('User', {})
-      var Task = this.sequelize.define('Task', {})
+    describe('getAssociation', function() {
+      it('returns the respective part of the association for 1:1 associations', function() {
+        var User = this.sequelize.define('User', {})
+        var Task = this.sequelize.define('Task', {})
 
-      User.hasOne(Task)
-      Task.belongsTo(User)
+        User.hasOne(Task)
+        Task.belongsTo(User)
 
-      expect(User.getAssociation(Task).target).toEqual(Task)
+        expect(User.getAssociation(Task).target).toEqual(Task)
+      })
     })
   })
 })
