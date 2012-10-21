0f9e5f303b04428085fb6462401ff7aa1fc2fc89
Sascha Depold <sascha@depold.com>
2012-10-21 13:49:28 +0200

    do not check the autoIncrementField if no callee was passed. fix #291

--- spec/sequelize.spec.js
+++ spec/sequelize.spec.js
@@ -7,10 +7,8 @@ buster.spec.expose()
 
 describe('Sequelize', function() {
   before(function(done) {
-    var self = this
-
     Helpers.initTests({
-      beforeComplete: function(sequelize) { self.sequelize = sequelize },
+      beforeComplete: function(sequelize) { this.sequelize = sequelize }.bind(this),
       onComplete: done
     })
   })
@@ -25,6 +23,24 @@ describe('Sequelize', function() {
         name: Helpers.Sequelize.STRING
       })
       expect(this.sequelize.isDefined('Project')).toBeTrue()
+    })
+  })
+
+  describe('query', function() {
+    before(function(done) {
+      this.User = this.sequelize.define('User', {
+        username: Helpers.Sequelize.STRING
+      })
+
+      this.User.sync().success(done)
+    })
+
+    it('=>executes a query the internal way', function(done) {
+      var sql = "INSERT INTO " + this.User.tableName + " (username) VALUES ('john')"
+      this.sequelize.query(sql, null, { raw: true }).success(function(result) {
+        expect(result).toBeNull()
+        done()
+      })
     })
   })
 })
