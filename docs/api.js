156e63a4bce988a88c9f1e5ba404219923401127
Sascha Depold <sascha@depold.com>
2013-02-26 16:33:19 +0100

    yuidoc based documentation

---
+++ docs/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Sequelize"
+    ],
+    "modules": [
+        "sequelize"
+    ],
+    "allModules": [
+        {
+            "displayName": "sequelize",
+            "name": "sequelize",
+            "description": "The entry point."
+        }
+    ]
+} };
+});
