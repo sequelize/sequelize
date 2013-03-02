1746311436a3692b261c02d190f671fb7519e28e
Sascha Depold <sascha@depold.com>
2013-03-02 21:25:58 +0100

    watchr is now generating the docs as well

--- .watchr.js
+++ .watchr.js
@@ -2,11 +2,14 @@ var watchr = require('watchr')
   , spawn  = require('child_process').spawn
 
 watchr.watch({
-  path: __dirname,
-  listener: function(eventName, filePath) {
+  paths: [__dirname + '/lib', __dirname + '/spec', __dirname + '/index.js'],
+  listener: function(eventName) {
     if (['new', 'change'].indexOf(eventName) > -1) {
-      var buster = spawn('./node_modules/.bin/buster-test', ['--reporter', 'specification'], { env: process.ENV })
+      // generate the documentation
+      spawn('npm', ['run', 'docs'])
 
+      // run the tests
+      var buster = spawn('./node_modules/.bin/buster-test', ['--reporter', 'specification'], { env: process.ENV })
       buster.stderr.on('data', function(data) { console.log(data.toString()) })
       buster.stdout.on('data', function(data) { console.log(data.toString()) })
     }
