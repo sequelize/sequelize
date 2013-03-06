757da44f610c851ffdba38ead6e24990a35d5a95
Alex Young <alex@helicoid.net>
2013-03-06 15:18:22 +0000

    Removes MySQL dialect condition in the data types test

--- spec/data-types.spec.js
+++ spec/data-types.spec.js
@@ -8,13 +8,11 @@ if(typeof require === 'function') {
 buster.spec.expose()
 
 describe(Helpers.getTestDialectTeaser('Data types'), function() {
-  if (dialect == 'mysql') {
-    it('should return DECIMAL for the default decimal type', function() {
-      expect(Sequelize.DECIMAL).toEqual('DECIMAL');
-    });
+  it('should return DECIMAL for the default decimal type', function() {
+    expect(Sequelize.DECIMAL).toEqual('DECIMAL');
+  });
 
-    it('should return DECIMAL(10,2) for the default decimal type with arguments', function() {
-      expect(Sequelize.DECIMAL(10, 2)).toEqual('DECIMAL(10,2)');
-    });
-  }
+  it('should return DECIMAL(10,2) for the default decimal type with arguments', function() {
+    expect(Sequelize.DECIMAL(10, 2)).toEqual('DECIMAL(10,2)');
+  });
 });
