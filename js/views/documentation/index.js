2a719fb54cae9ffff16d57e5fe2b67c13c21fee5
Sascha Depold <sascha@depold.com>
2013-03-10 20:46:14 +0100

    added scrollbar

--- js/views/documentation/index.js
+++ js/views/documentation/index.js
@@ -30,6 +30,40 @@ define([
             // wait a tiny bit and highlight the code
             window.setTimeout(function() {
               Prism.highlightAll()
+
+              $('[data-spy="scroll"]').each(function () {
+                $(this).scrollspy('refresh')
+              })
+
+              ;(function() {
+                // fix sub nav on scroll
+                var $win = $(window),
+                    $body = $('body'),
+                    $nav = $('.subnav'),
+                    navHeight = $('.navbar').first().height(),
+                    subnavHeight = $('.subnav').first().height(),
+                    subnavTop = $('.subnav').length && $('.subnav').offset().top - navHeight,
+                    marginTop = parseInt($body.css('margin-top'), 10),
+                    isFixed = 0;
+
+                processScroll();
+
+                $win.on('scroll', processScroll);
+
+                function processScroll() {
+                  var i, scrollTop = $win.scrollTop();
+
+                  if (scrollTop >= subnavTop && !isFixed) {
+                    isFixed = 1;
+                    $nav.addClass('subnav-fixed');
+                    $body.css('margin-top', marginTop + subnavHeight + 'px');
+                  } else if (scrollTop <= subnavTop && isFixed) {
+                    isFixed = 0;
+                    $nav.removeClass('subnav-fixed');
+                    $body.css('margin-top', marginTop + 'px');
+                  }
+                }
+              })()
             }, 50)
           })
         })
