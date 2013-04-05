7e4a6f74e1e27508f843d17c04c1cfcb3261de0c
Sascha Depold <sascha@depold.com>
2013-04-05 21:58:18 +0200

    sequelize on heroku

--- js/controllers/pages_controller.js
+++ js/controllers/pages_controller.js
@@ -6,25 +6,6 @@ define([
 ], function(Controller, ViewHelper) {
   'use strict';
 
-  Prism.languages.bash = {
-    'important': /(^#!\s*\/bin\/bash)|(^#!\s*\/bin\/sh)/g,
-    'comment': /(^|[^"{\\])(#.*?(\r?\n|$))/g,
-    'string': {
-      //allow multiline string
-      pattern: /("|')(\\?[\s\S])*?\1/g,
-      inside: {
-        'property': /\$([a-zA-Z0-9_#\?\-\*!@]+|\{[^\}]+\})/g
-      }
-    },
-    'property': /\$([a-zA-Z0-9_#\?\-\*!@]+|\{[^}]+\})/g,
-    'keyword': /\b(if|then|else|elif|fi|for|break|continue|while|in|case|function|select|do|done|until|echo|exit|return|set|declare)\b/g,
-    'boolean': /\b(true|false)\b/g,
-    'number': /\b(0x[\da-fA-F]+|-?\d*\.?\d+)\b/g,
-    'operator': /[-+]{1,2}|!|=?<|=?>|={1,2}|(&){1,2}|\|?\||\?|\*|\//g,
-    'ignore': /&(lt|gt|amp);/gi,
-    'punctuation': /[{}[\];(),.:]/g
-  };
-
   return Controller.extend({
     heroku: function() {
       this.title = 'Sequelize on Heroku'
