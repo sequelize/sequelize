9cfcc80f4af6d9e95684574d2730cd34522fdd36
tjmehta <tejesh.mehta@gmail.com>
2013-04-29 13:21:09 -0700

    Updated config directory to correct location in examples. * other line changes are whitespace trims (auto by ide)

--- examples/Count/app.js
+++ examples/Count/app.js
@@ -1,5 +1,5 @@
 var Sequelize = require(__dirname + "/../../index")
-  , config    = require(__dirname + "/../../test/config")
+  , config    = require(__dirname + "/../../spec/config/config")
   , sequelize = new Sequelize(config.database, config.username, config.password, {logging: false})
 
 var Person  = sequelize.define('Person', { name: Sequelize.STRING })
@@ -8,12 +8,12 @@ var Person  = sequelize.define('Person', { name: Sequelize.STRING })
 sequelize.sync({force: true}).on('success', function() {
   var count   = 10,
       queries = []
-  
+
   for(var i = 0; i < count; i++)
     chainer.add(Person.create({name: 'someone' + (i % 3)}))
-  
+
   console.log("Begin to save " + count + " items!")
-  
+
   chainer.run().on('success', function() {
     console.log("finished")
     Person.count().on('success', function(count) {
