334a0471f44ad760a525d7b5468ee492948ef40e
Daniel Durante <officedebo@gmail.com>
2013-07-17 01:40:08 -0400

    Officially removed Jasmine.

--- spec-jasmine/assets/migrations/20111205064000-renamePersonToUser.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: function(migration, DataTypes) {
-    migration.renameTable('Person', 'User')
-  },
-
-  down: function(migration, DataTypes) {
-    migration.renameTable('User', 'Person')
-  }
-}
