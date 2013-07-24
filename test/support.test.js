e667bbf6a59b4ae8fc0ccf43c76b8c63ce934361
Daniel Durante <officedebo@gmail.com>
2013-07-24 00:54:46 -0400

    Support will now properly clear the database after each test.

--- test/support.test.js
+++ test/support.test.js
@@ -4,6 +4,9 @@ var Support   = require(__dirname + '/support')
 before(function(done) {
   var sequelize = Support.createSequelizeInstance({ dialect: dialect })
   this.sequelize = sequelize
+  done()
+})
 
+beforeEach(function(done) {
   Support.clearDatabase(this.sequelize, done)
 })
