754de90097d3587491246c68bc0e231a8a55b668
Sascha Depold <sascha@depold.com>
2013-07-30 07:20:31 +0200

    removed specs

--- spec/assets/migrations/20111206061400-removeShopIdColumnFromUser.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: function(migration, DataTypes, done) {
-    migration.removeColumn('User', 'shopId').complete(done)
-  },
-
-  down: function(migration, DataTypes, done) {
-    migration.addColumn('User', 'shopId', { type: DataTypes.INTEGER, allowNull: true }).complete(done)
-  }
-}
--- spec/assets/migrations/20111206163300-renameSignatureColumnOfUserToSig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: function(migration, DataTypes, done) {
-    migration.renameColumn('User', 'signature', 'sig').complete(done)
-  },
-
-  down: function(migration, DataTypes, done) {
-    migration.renameColumn('User', 'sig', 'signature').complete(done)
-  }
-}
