6a831c546e08d4a085701b2d181eb0bf55dcece0
Sascha Depold <sascha@depold.com>
2013-08-23 09:35:27 +0200

    added test for enum validation

--- test/dao.validations.test.js
+++ test/dao.validations.test.js
@@ -446,5 +446,26 @@ describe(Support.getTestDialectTeaser("DaoValidator"), function() {
       expect(successfulFoo.validate()).to.be.null
       done()
     })
+
+    it('validates enums', function() {
+      var values = ['value1', 'value2']
+
+      var Bar = this.sequelize.define('Bar' + config.rand(), {
+        field: {
+          type: Sequelize.ENUM,
+          values: values,
+          validate: {
+            isIn: [values]
+          }
+        }
+      })
+
+      var failingBar = Bar.build({ field: 'value3' })
+        , errors     = failingBar.validate()
+
+      expect(errors).not.to.be.null
+      expect(errors.field).to.have.length(1)
+      expect(errors.field[0]).to.equal("Unexpected value or invalid argument: field")
+    })
   })
 })
