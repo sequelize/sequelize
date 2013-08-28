fb37f064fda9aacdd697154e05b0c08d341831e2
Daniel Durante <officedebo@gmail.com>
2013-08-28 06:28:50 -0400

    ENUM types will no longer in Postgres
    Postgres will now add enum's values on .sync() if you were to add
    entires into the attribute's.value. It'll also add the enum type in
    the proper order. For example:
    
    ```js
    Table = sequelize.define...
      mood: {
        type: DataTypes.ENUM,
        values: ['happy', 'sad']
      }
    
    // For some reason down the road you redefine the same table
    // ...
      mood: {
        type: DataTypes.ENUM,
        values: ['neutral', 'happy', 'sad', 'joyful']
      }
    ```
    
    Will result in the following SQL:
    
    ```sql
    ALTER TYPE "...enumTypeName..." ADD VALUE 'neutral' BEFORE 'happy';
    ALTER TYPE "...enumTypeName..." ADD VALUE 'jouful' AFTER 'sad';
    ```
    
    - ```.drop()``` will now pass the options object through (optional)
    - Added Postgres.QueryGenerator.pgListEnums(tableName=null,
       attrName=null, options) which lists all of the ENUM types for
       Postgres, if you don't specify an attribute and tablename then
       Sequelize will list all of the Enums
    - Added Postgres.QueryGenerator.pgEnumAdd(tableName, attr, value,
       options) which will alter the enum type and add values
    - Added Postgres.QueryGenerator.pgEnumDrop(tableName, attr) which
       will drop the enum type
    - Postgres.QueryGenerator.pgEnum() will no longer try to drop the
       type unless {force: true} within the ```.sync()``` commands
    - Refactored ```QueryInterface.createTable()``` in order to allow
       Postgres to create enum types explicitly rather than from
       Postgres.```QueryGenerator.createTable()```
    - Refactored QueryInterface.dropTable() -- same as ```createTable()```
       changes .. also {force: true/force} will now set options.cascade
    - QueryInterface.dropAllTables() will now correctly add a quote
       identifier to table names for MySQL
    
    Closes https://github.com/sequelize/sequelize/issues/546

--- test/postgres/dao.test.js
+++ test/postgres/dao.test.js
@@ -3,6 +3,7 @@ var chai      = require('chai')
   , Support   = require(__dirname + '/../support')
   , dialect   = Support.getTestDialect()
   , DataTypes = require(__dirname + "/../../lib/data-types")
+  , _         = require('lodash')
 
 chai.Assertion.includeStack = true
 
@@ -29,6 +30,67 @@ if (dialect.match(/^postgres/)) {
       this.sequelize.queryInterface.describeTable('Users').success(function(table) {
         expect(table.document.type).to.equal('HSTORE')
         done()
+      })
+    })
+
+    describe('enums', function() {
+      it('should be able to ignore enum types that already exist', function(done) {
+        var User = this.sequelize.define('UserEnums', {
+          mood: DataTypes.ENUM('happy', 'sad', 'meh')
+        })
+
+        User.sync({ force: true }).success(function() {
+          User.sync().success(function() {
+            done()
+          })
+        })
+      })
+
+      it('should be able to create/drop enums multiple times', function(done) {
+        var User = this.sequelize.define('UserEnums', {
+          mood: DataTypes.ENUM('happy', 'sad', 'meh')
+        })
+
+        User.sync({ force: true }).success(function() {
+          User.sync({ force: true }).success(function() {
+            done()
+          })
+        })
+      })
+
+      it('should be able to add enum types', function(done) {
+        var self = this
+          , User = this.sequelize.define('UserEnums', {
+          mood: DataTypes.ENUM('happy', 'sad', 'meh')
+        })
+
+        var _done = _.after(4, function() {
+          done()
+        })
+
+        User.sync({ force: true }).success(function() {
+          User = self.sequelize.define('UserEnums', {
+            mood: DataTypes.ENUM('neutral', 'happy', 'sad', 'ecstatic', 'meh', 'joyful')
+          })
+
+          User.sync().success(function() {
+            expect(User.rawAttributes.mood.values).to.deep.equal(['neutral', 'happy', 'sad', 'ecstatic', 'meh', 'joyful'])
+            _done()
+          }).on('sql', function(sql) {
+            if (sql.indexOf('neutral') > -1) {
+              expect(sql).to.equal("ALTER TYPE \"enum_UserEnums_mood\" ADD VALUE 'neutral' BEFORE 'happy'")
+              _done()
+            }
+            else if (sql.indexOf('ecstatic') > -1) {
+              expect(sql).to.equal("ALTER TYPE \"enum_UserEnums_mood\" ADD VALUE 'ecstatic' BEFORE 'meh'")
+              _done()
+            }
+            else if (sql.indexOf('joyful') > -1) {
+              expect(sql).to.equal("ALTER TYPE \"enum_UserEnums_mood\" ADD VALUE 'joyful' AFTER 'meh'")
+              _done()
+            }
+          })
+        })
       })
     })
 
