41eaf467591f2d1dd3e8049ee3fa498bcd204119
Sascha Depold <sascha@depold.com>
2013-10-11 09:53:27 +0200

    semicolon!

--- test/support.js
+++ test/support.js
@@ -57,7 +57,7 @@ var Support = {
   },
 
   getSequelizeInstance: function(db, user, pass, options) {
-    options = options || {};
+    options = options || {}
     options.dialect = options.dialect || this.getTestDialect()
     return new Sequelize(db, user, pass, options)
   },
