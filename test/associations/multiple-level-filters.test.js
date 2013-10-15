ebbfd6e4e4aa7d9545a8dc3aafc1101a1e102785
Rafael Martins <snit.ram@gmail.com>
2013-10-15 00:27:34 -0700

    Adding support for filter through hasMany connector

--- test/associations/multiple-level-filters.test.js
+++ test/associations/multiple-level-filters.test.js
@@ -137,4 +137,58 @@ describe(Support.getTestDialectTeaser("Multiple Level Filters"), function() {
       });
     })
   })
+
+  it('can filter through hasMany connector', function(done) {
+    var User = this.sequelize.define('User', {username: DataTypes.STRING })
+      , Project = this.sequelize.define('Project', { title: DataTypes.STRING })
+
+    Project.hasMany(User);
+    User.hasMany(Project)
+
+    this.sequelize.sync({ force: true }).success(function() {
+      User.bulkCreate([{
+        id: 101,
+        username: 'leia'
+      }, {
+        id: 102,
+        username: 'vader'
+      }]).success(function() {
+        Project.bulkCreate([{
+          id: 201,
+          title: 'republic'
+        },{
+          id: 202,
+          title: 'empire'
+        }]).success(function() {
+          User.find(101).success(function(user){
+            Project.find(201).success(function(project){
+              user.setProjects([project]).success(function(){
+
+                User.find(102).success(function(user){
+                  Project.find(202).success(function(project){
+                    user.setProjects([project]).success(function(){
+                      User.findAll({
+                        where: {
+                          'projects.title': 'republic'
+                        }
+                      }).success(function(users){
+                        try{
+                          expect(users.length).to.be.equal(1);
+                          expect(users[0].username).to.be.equal('leia');
+                          done();
+                        }catch(e){
+                          done(e);
+                        }
+                      })
+                    });
+                  });
+                });
+
+              });
+            });
+          });
+        });
+      });
+    })
+  })
 })
