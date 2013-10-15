ea8dba066ad8906d77236372f4da2b902ba8e27a
Rafael Martins <snit.ram@gmail.com>
2013-10-15 01:29:30 -0700

    Filter through relations prevents duplicate table in query

--- test/associations/multiple-level-filters.test.js
+++ test/associations/multiple-level-filters.test.js
@@ -73,6 +73,73 @@ describe(Support.getTestDialectTeaser("Multiple Level Filters"), function() {
     })
   })
 
+  it('avoids duplicated tables in query', function(done) {
+    var User = this.sequelize.define('User', {username: DataTypes.STRING })
+      , Task = this.sequelize.define('Task', {title: DataTypes.STRING })
+      , Project = this.sequelize.define('Project', { title: DataTypes.STRING })
+
+    Project.belongsTo(User);
+    User.hasMany(Project)
+
+    Task.belongsTo(Project);
+    Project.hasMany(Task);
+
+
+    this.sequelize.sync({ force: true }).success(function() {
+      User.bulkCreate([{
+        id: 101,
+        username: 'leia'
+      }, {
+        id: 102,
+        username: 'vader'
+      }]).success(function() {
+        Project.bulkCreate([{
+          id: 201,
+          UserId: 101,
+          title: 'republic'
+        },{
+          id: 202,
+          UserId: 102,
+          title: 'empire'
+        }]).success(function() {
+          Task.bulkCreate([{
+            id: 301,
+            ProjectId: 201,
+            title: 'fight empire'
+          },{
+            id: 302,
+            ProjectId: 201,
+            title: 'stablish republic'
+          },{
+            id: 303,
+            ProjectId: 202,
+            title: 'destroy rebel alliance'
+          },{
+            id: 304,
+            ProjectId: 202,
+            title: 'rule everything'
+          }]).success(function() {
+            Task.findAll({
+              where: {
+                'project.user.username': 'leia',
+                'project.user.id': 101
+              }
+            }).success(function(tasks){
+              try{
+                expect(tasks.length).to.be.equal(2);
+                expect(tasks[0].title).to.be.equal('fight empire');
+                expect(tasks[1].title).to.be.equal('stablish republic');
+                done();
+              }catch(e){
+                done(e);
+              }
+            })
+          });
+        });
+      });
+    })
+  })
+
   it('can filter through hasMany', function(done) {
     var User = this.sequelize.define('User', {username: DataTypes.STRING })
       , Task = this.sequelize.define('Task', {title: DataTypes.STRING })
