71e146130eeece8e5f3309109951f584dfd4c1c5
Michael Schonfeld <michael@schonfeld.org>
2014-01-19 23:56:57 -0500

    add tests

--- examples/Sum/app.js
+++ examples/Sum/app.js
@@ -4,8 +4,8 @@ var Sequelize = require(__dirname + "/../../index")
 
 var Person  = sequelize.define('Person',
   { name: Sequelize.STRING,
-    age : Sequelize.INTEGER
-
+    age : Sequelize.INTEGER,
+    gender: Sequelize.ENUM('male', 'female')
   })
   , chainer = new Sequelize.Utils.QueryChainer
 
@@ -14,14 +14,17 @@ sequelize.sync({force: true}).on('success', function() {
       queries = []
 
   for(var i = 0; i < count; i++)
-    chainer.add(Person.create({name: 'someone' + (i % 3), age : i+5}))
+    chainer.add(Person.create({name: 'someone' + (i % 3), age : i+5, gender: (i % 2 == 0) ? 'male' : 'female'}))
 
   console.log("Begin to save " + count + " items!")
 
   chainer.run().on('success', function() {
     console.log("finished")
     Person.sum('age').on('success', function(sum) {
-      console.log("Sum of all people's ages: " + sum)
+      console.log("Sum of all peoples' ages: " + sum)
+    });
+    Person.sum('age', { where: { 'gender': 'male' } }).on('success', function(sum) {
+      console.log("Sum of all males' ages: " + sum)
     });
   })
 })
