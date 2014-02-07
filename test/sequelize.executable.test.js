3fc6c8bdeb3169f89fd9bc45070429067679afd6
Sascha Depold <sascha@depold.com>
2014-02-07 09:47:34 +0100

    Add coffee support for --create-migration

--- test/sequelize.executable.test.js
+++ test/sequelize.executable.test.js
@@ -142,6 +142,53 @@ describe(Support.getTestDialectTeaser("Executable"), function() {
     })
   })(['--create-migration', '-c'])
 
+    ;(function(flags) {
+    flags.forEach(function(flag) {
+      var prepare = function(callback) {
+        exec("rm -rf ./*", { cwd: __dirname + '/tmp' }, function() {
+          exec("../../bin/sequelize --init", { cwd: __dirname + '/tmp' }, function() {
+            exec("../../bin/sequelize " + flag + " 'foo'", { cwd: __dirname + '/tmp' }, callback)
+          })
+        })
+      }
+
+      describe(flag, function() {
+        it("creates a new file with the current timestamp", function(done) {
+          prepare(function() {
+            exec("ls -1 migrations", { cwd: __dirname + '/tmp' }, function(err, stdout) {
+              var date   = new Date()
+                , format = function(i) { return (parseInt(i, 10) < 10 ? '0' + i : i)  }
+                , sDate  = [date.getFullYear(), format(date.getMonth() + 1), format(date.getDate()), format(date.getHours()), format(date.getMinutes())].join('')
+
+              expect(stdout).to.match(new RegExp(sDate + "..-foo.coffee"))
+              done()
+            })
+          })
+        })
+
+        it("adds a skeleton with an up and a down method", function(done) {
+          prepare(function() {
+            exec("cat migrations/*-foo.coffee", { cwd: __dirname + '/tmp' }, function(err, stdout) {
+              expect(stdout).to.include('up: (migration, DataTypes, done) ->')
+              expect(stdout).to.include('down: (migration, DataTypes, done) ->')
+              done()
+            })
+          })
+        })
+
+        it("calls the done callback", function(done) {
+          prepare(function() {
+            exec("cat migrations/*-foo.coffee", { cwd: __dirname + '/tmp' }, function(err, stdout) {
+              expect(stdout).to.include('done()')
+              expect(stdout.match(/(done\(\))/)).to.have.length(2)
+              done()
+            })
+          })
+        })
+      })
+    })
+  })(['--coffee --create-migration', '--coffee -c'])
+
   ;(function(flags) {
     flags.forEach(function(flag) {
       var prepare = function(callback) {
