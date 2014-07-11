6c774dff1083ec8d118dbf1801df99287e78a0a9
Jan Aagaard Meier <janzeh@gmail.com>
2014-07-11 15:33:33 +0200

    Changed the alias tests a bit to prevent race condition

--- test/associations/alias.test.js
+++ test/associations/alias.test.js
@@ -20,14 +20,14 @@ describe(Support.getTestDialectTeaser("Alias"), function() {
     Task.belongsTo(User, { as: 'owner', foreignKey: 'userId' });
 
     return this.sequelize.sync({ force: true }).then(function () {
-      return Promise.all([
-        User.create({ id: 1 }),
-        Task.create({ id: 1, userId: 1 })
-      ]);
-    }).spread(function (user, task) {
+      return User.create({ id: 1 });
+    }).then(function (user) {
       expect(user.getAssignments).to.be.ok;
+
+      return Task.create({ id: 1, userId: 1 });
+    }).then(function (task) {
       expect(task.getOwner).to.be.ok;
-    }).then(function () {
+
       return Promise.all([
         User.find({ where: { id: 1 }, include: [{model: Task, as: 'assignments'}] }),
         Task.find({ where: { id: 1 }, include: [{model: User, as: 'owner'}] }),
@@ -71,18 +71,14 @@ describe(Support.getTestDialectTeaser("Alias"), function() {
     User.hasMany(Task, { as: { singular: 'task', plural: 'taskz'} });
 
     return this.sequelize.sync({ force: true }).then(function () {
-      return Promise.all([
-        User.create({ id: 1 }),
-      ]);
-    }).spread(function (user, task) {
+      return User.create({ id: 1 });
+    }).then(function (user) {
       expect(user.getTaskz).to.be.ok;
       expect(user.addTask).to.be.ok;
       expect(user.addTaskz).to.be.ok;
     }).then(function () {
-      return Promise.all([
-        User.find({ where: { id: 1 }, include: [{model: Task, as: 'taskz'}] }),
-      ]);
-    }).spread(function (user, task) {
+      return User.find({ where: { id: 1 }, include: [{model: Task, as: 'taskz'}] });
+    }).then(function (user) {
       expect(user.taskz).to.be.ok;
     });
   });
@@ -99,18 +95,14 @@ describe(Support.getTestDialectTeaser("Alias"), function() {
     User.hasMany(Task);
 
     return this.sequelize.sync({ force: true }).then(function () {
-      return Promise.all([
-        User.create({ id: 1 }),
-      ]);
-    }).spread(function (user, task) {
+      return User.create({ id: 1 });
+    }).then(function (user) {
       expect(user.getAssignments).to.be.ok;
       expect(user.addAssignment).to.be.ok;
       expect(user.addAssignments).to.be.ok;
     }).then(function () {
-      return Promise.all([
-        User.find({ where: { id: 1 }, include: [Task] }),
-      ]);
-    }).spread(function (user, task) {
+      return User.find({ where: { id: 1 }, include: [Task] });
+    }).then(function (user) {
       expect(user.assignments).to.be.ok;
     });
   });
