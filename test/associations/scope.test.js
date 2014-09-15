d348e3cf9da389a7f392779043cfc03c6417c704
Mick Hansen <mick.kasper.hansen@gmail.com>
2014-09-15 19:49:16 +0200

    feat(associations/scope): add support for scope on N:M through model

--- test/associations/scope.test.js
+++ test/associations/scope.test.js
@@ -28,11 +28,6 @@ describe(Support.getTestDialectTeaser("associations"), function() {
           }
         }
       });
-      this.Tag = this.sequelize.define('tag', {
-        name: Sequelize.STRING,
-        taggable: Sequelize.STRING,
-        taggable_id: Sequelize.INTEGER
-      });
 
       this.Post.hasMany(this.Comment, {
         foreignKey: 'commentable_id',
@@ -42,10 +37,7 @@ describe(Support.getTestDialectTeaser("associations"), function() {
       });
       this.Comment.belongsTo(this.Post, {
         foreignKey: 'commentable_id',
-        as: 'post',
-        scope: {
-          commentable: 'post'
-        }
+        as: 'post'
       });
 
       this.Image.hasMany(this.Comment, {
@@ -56,10 +48,7 @@ describe(Support.getTestDialectTeaser("associations"), function() {
       });
       this.Comment.belongsTo(this.Image, {
         foreignKey: 'commentable_id',
-        as: 'image',
-        scope: {
-          commentable: 'image'
-        }
+        as: 'image'
       });
 
       this.Question.hasMany(this.Comment, {
@@ -70,10 +59,7 @@ describe(Support.getTestDialectTeaser("associations"), function() {
       });
       this.Comment.belongsTo(this.Question, {
         foreignKey: 'commentable_id',
-        as: 'question',
-        scope: {
-          commentable: 'question'
-        }
+        as: 'question'
       });
     });
     
@@ -258,7 +244,6 @@ describe(Support.getTestDialectTeaser("associations"), function() {
                 })
               );
             }).spread(function (postA, postB, postC) {
-              //console.log(postA.get('categories'));
               expect(postA.get('categories').length).to.equal(1);
               expect(postA.get('tags').length).to.equal(1);
               expect(postB.get('categories').length).to.equal(1);
@@ -276,9 +261,158 @@ describe(Support.getTestDialectTeaser("associations"), function() {
           });
         });
 
-        describe.skip('on the through model', function () {
-          it('should create, find and include associations with scope values', function () {
+        describe('on the through model', function () {
+          beforeEach(function () {
+            this.Post = this.sequelize.define('post', {});
+            this.Image = this.sequelize.define('image', {});
+            this.Question = this.sequelize.define('question', {});
 
+            this.ItemTag = this.sequelize.define('item_tag', {
+              id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+              },
+              tag_id: {
+                type: DataTypes.INTEGER,
+                unique: 'item_tag_taggable'
+              },
+              taggable: {
+                type: DataTypes.STRING,
+                unique: 'item_tag_taggable'
+              },
+              taggable_id: {
+                type: DataTypes.INTEGER,
+                unique: 'item_tag_taggable',
+                references: null
+              }
+            });
+            this.Tag = this.sequelize.define('tag', {
+              name: DataTypes.STRING
+            });
+
+            this.Post.hasMany(this.Tag, {
+              through: {
+                model: this.ItemTag,
+                unique: false,
+                scope: {
+                  taggable: 'post'
+                }
+              },
+              foreignKey: 'taggable_id',
+              constraints: false
+            });
+            this.Tag.hasMany(this.Post, {
+              through: {
+                model: this.ItemTag,
+                unique: false
+              },
+              foreignKey: 'tag_id'
+            });
+
+            this.Image.hasMany(this.Tag, {
+              through: {
+                model: this.ItemTag,
+                unique: false,
+                scope: {
+                  taggable: 'image'
+                }
+              },
+              foreignKey: 'taggable_id',
+              constraints: false
+            });
+            this.Tag.hasMany(this.Image, {
+              through: {
+                model: this.ItemTag,
+                unique: false
+              },
+              foreignKey: 'tag_id'
+            });
+
+            this.Question.hasMany(this.Tag, {
+              through: {
+                model: this.ItemTag,
+                unique: false,
+                scope: {
+                  taggable: 'question'
+                }
+              },
+              foreignKey: 'taggable_id',
+              constraints: false
+            });
+            this.Tag.hasMany(this.Question, {
+              through: {
+                model: this.ItemTag,
+                unique: false
+              },
+              foreignKey: 'tag_id'
+            });
+          });
+          it('should create, find and include associations with scope values', function () {
+            var self = this;
+            return Promise.join(
+              this.Post.sync({force: true}),
+              this.Image.sync({force: true}),
+              this.Question.sync({force: true}),
+              this.Tag.sync({force: true})
+            ).bind(this).then(function () {
+              return this.ItemTag.sync({force: true});
+            }).then(function () {
+              return Promise.join(
+                this.Post.create(),
+                this.Image.create(),
+                this.Question.create(),
+                this.Tag.create({name: 'tagA'}),
+                this.Tag.create({name: 'tagB'}),
+                this.Tag.create({name: 'tagC'})
+              );
+            }).spread(function (post, image, question, tagA, tagB, tagC) {
+              this.post = post;
+              this.image = image;
+              this.question = question;
+              return Promise.join(
+                post.setTags([tagA]).then(function () {
+                  return Promise.join(
+                    post.createTag({name: 'postTag'}),
+                    post.addTag(tagB)
+                  );
+                }),
+                image.setTags([tagB]).then(function () {
+                  return Promise.join(
+                    image.createTag({name: 'imageTag'}),
+                    image.addTag(tagC)
+                  );
+                }),
+                question.setTags([tagC]).then(function () {
+                  return Promise.join(
+                    question.createTag({name: 'questionTag'}),
+                    question.addTag(tagA)
+                  );
+                })
+              );
+            }).then(function () {
+              return Promise.join(
+                this.post.getTags(),
+                this.image.getTags(),
+                this.question.getTags()
+              ).spread(function (postTags, imageTags, questionTags) {
+                expect(postTags.length).to.equal(3);
+                expect(imageTags.length).to.equal(3);
+                expect(questionTags.length).to.equal(3);
+
+                expect(postTags.map(function (tag) {
+                  return tag.name;
+                }).sort()).to.deep.equal(['postTag', 'tagA', 'tagB']);
+
+                expect(imageTags.map(function (tag) {
+                  return tag.name;
+                }).sort()).to.deep.equal(['imageTag', 'tagB', 'tagC']);
+
+                expect(questionTags.map(function (tag) {
+                  return tag.name;
+                }).sort()).to.deep.equal(['questionTag', 'tagA', 'tagC']);
+              });
+            });
           });
         });
       });
