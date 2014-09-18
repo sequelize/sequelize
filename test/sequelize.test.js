fa509566d9003f6a51ca0dc3472490fb9c92b84a
Jan Aagaard Meier <janzeh@gmail.com>
2014-09-18 15:46:33 +0200

    Made the set tests mysql specific, and more promisy

--- test/sequelize.test.js
+++ test/sequelize.test.js
@@ -449,64 +449,45 @@ describe(Support.getTestDialectTeaser("Sequelize"), function () {
     }
   })
 
-  describe('set', function() {
-
-    it("should return an promised error if transaction isn't defined", function ( done ) {
-      var S = this.sequelize;
-
-      S.set({ foo: 'bar' })
-        .done( function ( err ) {
-          expect( err ).to.be.an.instanceof( TypeError )
-
-          done();
-        });
-    })
-
-    it("one value", function ( done ) {
-      var S = this.sequelize;
-
-      S.transaction().then(function ( t ) {
-        S
-          .set({ foo: 'bar' }, { transaction: t })
-          .then(function () {
-            return S.query( 'SELECT @foo as `foo`', null, { raw: true, plain: true, transaction: t })
-          })
-          .then(function ( data ) {
-            expect( data ).to.be.ok
-            expect( data.foo ).to.be.equal( 'bar' )
-          })
-          .then(function () {
-            return t.commit()
-          })
-          .then( done )
+  if (Support.dialectIsMySQL()) {
+    describe('set', function() {
+      it("should return an promised error if transaction isn't defined", function () {
+        expect(function () {
+          this.sequelize.set({ foo: 'bar' })
+        }.bind(this)).to.throw(TypeError, "options.transaction is required")
       })
-    })
 
-    it("multiple values", function ( done ) {
-      var S = this.sequelize;
+      it("one value", function () {
+        return this.sequelize.transaction().bind(this).then(function (t) {
+          this.t = t;
+          return this.sequelize.set({ foo: 'bar' }, { transaction: t });
+        }).then(function () {
+          return this.sequelize.query( 'SELECT @foo as `foo`', null, { raw: true, plain: true, transaction: this.t });
+        }).then(function (data) {
+          expect(data).to.be.ok
+          expect(data.foo).to.be.equal('bar')
+          return this.t.commit();
+        });
+      });
 
-      S.transaction().then(function ( t ) {
-        S
-          .set({
+      it("multiple values", function () {
+        return this.sequelize.transaction().bind(this).then(function (t) {
+          this.t = t;
+          return this.sequelize.set({
             foo: 'bar',
             foos: 'bars',
-          }, { transaction: t })
-          .then(function () {
-            return S.query( 'SELECT @foo as `foo`, @foos as `foos`', null, { raw: true, plain: true, transaction: t })
-          })
-          .then(function ( data ) {
-            expect( data ).to.be.ok
-            expect( data.foo ).to.be.equal( 'bar' )
-            expect( data.foos ).to.be.equal( 'bars' )
-          })
-          .then(function () {
-            return t.commit()
-          })
-          .then( done )
-      })
-    })
-
-  })
+          }, { transaction: t });
+        }).then(function () {
+          return this.sequelize.query( 'SELECT @foo as `foo`, @foos as `foos`', null, { raw: true, plain: true, transaction: this.t });
+        }).then(function (data) {
+          expect(data).to.be.ok;
+          expect(data.foo).to.be.equal('bar');
+          expect(data.foos).to.be.equal('bars');
+          return this.t.commit();
+        });
+      });
+    });
+  }
 
   describe('define', function() {
     it("adds a new dao to the dao manager", function(done) {
