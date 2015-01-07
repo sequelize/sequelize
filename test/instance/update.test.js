5cb09bbe541e657ed1e823096b9f971b3812f03b
Mick Hansen <mick.kasper.hansen@gmail.com>
2015-01-07 22:18:34 +0100

    test(instance): more cases for update with hooks

--- test/instance/update.test.js
+++ test/instance/update.test.js
@@ -143,6 +143,38 @@ describe(Support.getTestDialectTeaser('Instance'), function() {
         });
       });
 
+      it('should update attributes changed in hooks when default fields are used', function () {
+        var User = this.sequelize.define('User' + config.rand(), {
+          name: DataTypes.STRING,
+          bio: DataTypes.TEXT,
+          email: DataTypes.STRING
+        });
+
+        User.beforeUpdate(function(instance, options) {
+          instance.set('email', 'C');
+        });
+
+        return User.sync({force: true}).then(function() {
+          return User.create({
+            name: 'A',
+            bio: 'A',
+            email: 'A'
+          }).then(function (user) {
+            return user.update({
+              name: 'B',
+              bio: 'B',
+              email: 'B'
+            });
+          }).then(function () {
+            return User.findOne({});
+          }).then(function (user) {
+            expect(user.get('name')).to.equal('B');
+            expect(user.get('bio')).to.equal('B');
+            expect(user.get('email')).to.equal('C');
+          });
+        });
+      });
+
       it('should validate attributes added in hooks when default fields are used', function () {
         var User = this.sequelize.define('User' + config.rand(), {
           name: DataTypes.STRING,
@@ -167,6 +199,40 @@ describe(Support.getTestDialectTeaser('Instance'), function() {
           }).then(function (user) {
             return expect(user.update({
               name: 'B'
+            })).to.be.rejectedWith(Sequelize.ValidationError);
+          }).then(function () {
+            return User.findOne({}).then(function (user) {
+              expect(user.get('email')).to.equal('valid.email@gmail.com');
+            });
+          });
+        });
+      });
+
+      it('should validate attributes changed in hooks when default fields are used', function () {
+        var User = this.sequelize.define('User' + config.rand(), {
+          name: DataTypes.STRING,
+          bio: DataTypes.TEXT,
+          email: {
+            type: DataTypes.STRING,
+            validate: {
+              isEmail: true
+            }
+          }
+        });
+
+        User.beforeUpdate(function(instance, options) {
+          instance.set('email', 'B');
+        });
+
+        return User.sync({force: true}).then(function () {
+          return User.create({
+            name: 'A',
+            bio: 'A',
+            email: 'valid.email@gmail.com'
+          }).then(function (user) {
+            return expect(user.update({
+              name: 'B',
+              email: 'still.valid.email@gmail.com'
             })).to.be.rejectedWith(Sequelize.ValidationError);
           }).then(function () {
             return User.findOne({}).then(function (user) {
