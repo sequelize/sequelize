cd1206b13d6210cd8c2f7d4c2de14d12a13881c3
Jan Aagaard Meier <janzeh@gmail.com>
2015-02-09 15:36:48 +0100

    Change JSON unit test to use dialect.supports

--- test/unit/sql/json.test.js
+++ test/unit/sql/json.test.js
@@ -5,28 +5,29 @@ var Support   = require(__dirname + '/../support')
   , util      = require('util')
   , expectsql = Support.expectsql
   , current   = Support.sequelize
-  , sql       = current.dialect.QueryGenerator;
+  , sql       = current.dialect.QueryGenerator
+  , current = Support.sequelize;
 
 // Notice: [] will be replaced by dialect specific tick/quote character when there is not dialect specific expectation but only a default expectation
 
-if (Support.getTestDialect() === 'postgres') {
+if (current.dialect.supports.JSON) {
   suite(Support.getTestDialectTeaser('SQL'), function() {
     suite('JSON', function () {
       suite('escape', function () {
         test('plain string', function () {
-          expectsql(sql.escape('string', { type: DataTypes.JSON}), {
+          expectsql(sql.escape('string', { type: new DataTypes.JSON() }), {
             default: '\'"string"\''
           });
         });
 
         test('plain int', function () {
-          expectsql(sql.escape(123, { type: DataTypes.JSON}), {
+          expectsql(sql.escape(123, { type: new DataTypes.JSON() }), {
             default: "'123'"
           });
         });
 
         test('nested object', function () {
-          expectsql(sql.escape({ some: 'nested', more: { nested: true }, answer: 42 }, { type: DataTypes.JSON}), {
+          expectsql(sql.escape({ some: 'nested', more: { nested: true }, answer: 42 }, { type: new DataTypes.JSON() }), {
             default: "'{\"some\":\"nested\",\"more\":{\"nested\":true},\"answer\":42}'"
           });
         });
