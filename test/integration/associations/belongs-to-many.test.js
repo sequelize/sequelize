bb959f4594acacf79f446f7a7a8ffb974a0cfd18
Nick Acker <acker2@me.com>
2015-03-04 14:14:30 -0500

    Add failing test for N:M with renamed attributes

--- test/integration/associations/belongs-to-many.test.js
+++ test/integration/associations/belongs-to-many.test.js
@@ -246,7 +246,7 @@ describe(Support.getTestDialectTeaser('BelongsToMany'), function() {
       });
 
       var User_has_Group = this.sequelize.define('User_has_Group', {
-      
+
       }, {
         tableName: 'tbl_user_has_group'
       });
@@ -265,6 +265,57 @@ describe(Support.getTestDialectTeaser('BelongsToMany'), function() {
             where: {}
           }).then(function (user) {
             return user.getGroups();
+          });
+        });
+      });
+    });
+
+    it('supports primary key attributes with different field names', function () {
+      var User = this.sequelize.define('User', {
+        id: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          primaryKey: true,
+          defaultValue: DataTypes.UUIDV4,
+          field: 'user_id'
+        }
+      }, {
+        tableName: 'tbl_user'
+      });
+
+      var Group = this.sequelize.define('Group', {
+        id: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          primaryKey: true,
+          defaultValue: DataTypes.UUIDV4,
+          field: 'group_id'
+        }
+      }, {
+        tableName: 'tbl_group'
+      });
+
+      var User_has_Group = this.sequelize.define('User_has_Group', {
+
+      }, {
+        tableName: 'tbl_user_has_group'
+      });
+
+      User.belongsToMany(Group, {through: User_has_Group});
+      Group.belongsToMany(User, {through: User_has_Group});
+
+      return this.sequelize.sync({force: true}).then(function () {
+        return Promise.join(
+          User.create(),
+          Group.create()
+        ).spread(function (user, group) {
+          return user.addGroup(group);
+        }).then(function () {
+          return User.findOne({
+            where: {},
+            include: Group
+          }).then(function (user) {
+            return user;
           });
         });
       });
