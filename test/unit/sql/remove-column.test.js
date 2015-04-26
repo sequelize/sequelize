81319d76071f027d6034397f8f3e0a0c673b282f
Mick Hansen <mick.kasper.hansen@gmail.com>
2015-04-26 19:04:52 +0200

    fix: dont run removeColumn tests for sqlite

--- test/unit/sql/remove-column.test.js
+++ test/unit/sql/remove-column.test.js
@@ -7,16 +7,19 @@ var Support   = require(__dirname + '/../support')
 
 // Notice: [] will be replaced by dialect specific tick/quote character when there is not dialect specific expectation but only a default expectation
 
-suite(Support.getTestDialectTeaser('SQL'), function() {
-  suite('removeColumn', function () {
-    test('schema', function () {
-      expectsql(sql.removeColumnQuery({
-        schema: 'archive',
-        tableName: 'user'
-      }, 'email'), {
-        default: 'ALTER TABLE [archive].[user] DROP COLUMN [email];',
-        mysql: 'ALTER TABLE `archive.user` DROP `email`;'
+if (current.dialect.name !== 'sqlite') {
+  suite(Support.getTestDialectTeaser('SQL'), function() {
+    suite('removeColumn', function () {
+      test('schema', function () {
+        expectsql(sql.removeColumnQuery({
+          schema: 'archive',
+          tableName: 'user'
+        }, 'email'), {
+          mssql: 'ALTER TABLE [archive].[user] DROP [email];',
+          mysql: 'ALTER TABLE `archive.user` DROP `email`;',
+          postgres: 'ALTER TABLE "archive"."user" DROP COLUMN "email";',
+        });
       });
     });
   });
-});
+}
