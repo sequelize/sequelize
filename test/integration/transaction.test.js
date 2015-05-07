6700695d9727efa506467b312f6d3305803b08be
Ruben Bridgewater <ruben.bridgewater@fintura.de>
2015-05-07 17:01:26 +0200

    Fix a couple tests

--- test/integration/transaction.test.js
+++ test/integration/transaction.test.js
@@ -44,6 +44,7 @@ describe(Support.getTestDialectTeaser('Transaction'), function() {
         return Promise.resolve();
       });
     });
+
     it('supports automatically rolling back with a thrown error', function() {
       var t;
       return (expect(this.sequelize.transaction(function(transaction) {
@@ -53,15 +54,24 @@ describe(Support.getTestDialectTeaser('Transaction'), function() {
         expect(t.finished).to.be.equal('rollback');
       });
     });
-    it('supports automatically rolling back with a rejection', function() {
-      return expect(this.sequelize.transaction(function() {
-        return Promise.reject('Swag');
-      })).to.eventually.be.rejected;
-    });
-    it('errors when no promise chain is returned', function() {
-      return expect(this.sequelize.transaction(function() {
 
-      })).to.eventually.be.rejected;
+    it('supports automatically rolling back with a rejection', function() {
+      var t;
+      return (expect(this.sequelize.transaction(function(transaction) {
+        t = transaction;
+        return Promise.reject('Swag');
+      })).to.eventually.be.rejected).then(function() {
+        expect(t.finished).to.be.equal('rollback');
+      });
+    });
+
+    it('errors when no promise chain is returned', function() {
+      var t;
+      return (expect(this.sequelize.transaction(function(transaction) {
+        t = transaction;
+      })).to.eventually.be.rejected).then(function() {
+        expect(t.finished).to.be.equal('rollback');
+      });
     });
   });
 
