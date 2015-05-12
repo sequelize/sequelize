ff50d27daf0a3a41c24b4da78b58dc121de17aa6
Mike Ringrose <mike.ringrose@myfilip.com>
2015-05-12 10:59:19 -0400

    Switching from GeoJSON to WKT

--- test/unit/sql/data-types.test.js
+++ test/unit/sql/data-types.test.js
@@ -287,23 +287,8 @@ suite(Support.getTestDialectTeaser('SQL'), function() {
         default: 'GEOMETRY',
         postgres: 'GEOMETRY'
       });
-	
-      testsql('GEOMETRY(\'POINT\')', DataTypes.GEOMETRY('POINT'), {
-        default: 'GEOMETRY(POINT)',
-        postgres: 'GEOMETRY(POINT)'
-      });
 
-      testsql('GEOMETRY(\'LINESTRING\')', DataTypes.GEOMETRY('LINESTRING'), {
-        default: 'GEOMETRY(LINESTRING)',
-        postgres: 'GEOMETRY(LINESTRING)',
-      });
-
-      testsql('GEOMETRY(\'POLYGON\')', DataTypes.GEOMETRY('POLYGON'), {
-        default: 'GEOMETRY(POLYGON)',
-        postgres: 'GEOMETRY(POLYGON)'
-      });
-
-      testsql('GEOMETRY(\'POINT\',4326)', DataTypes.GEOMETRY('POINT', 4326), {
+      testsql('GEOMETRY(\'POINT\',4326)', DataTypes.GEOMETRY(4326), {
         default: 'GEOMETRY(POINT,4326)',
         postgres: 'GEOMETRY(POINT,4326)'
       });
