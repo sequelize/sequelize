218837598c9148f7a3c7d542dd78a28759d10a6b
Derek Barnes <derek@bigbento.com>
2015-05-15 16:46:51 -0700

    add support for named timezones

--- test/integration/timezone.test.js
+++ test/integration/timezone.test.js
@@ -15,6 +15,9 @@ if (dialect !== 'sqlite') {
       this.sequelizeWithTimezone = Support.createSequelizeInstance({
         timezone: '+07:00'
       });
+      this.sequelizeWithNamedTimezone = Support.createSequelizeInstance({
+        timezone: 'America/New_York'
+      });
     });
 
     it('returns the same value for current timestamp', function() {
@@ -50,6 +53,21 @@ if (dialect !== 'sqlite') {
           // This difference is expected since two instances, configured for each their timezone is trying to read the same timestamp
           // this test does not apply to PG, since it stores the timezone along with the timestamp.
           expect(normalUser.createdAt.getTime() - this.timezonedUser.createdAt.getTime()).to.be.closeTo(60 * 60 * 7 * 1000, 50);
+        });
+      });
+
+      it('handles named timezones', function() {
+        var NormalUser = this.sequelize.define('user', {})
+          , TimezonedUser = this.sequelizeWithNamedTimezone.define('user', {});
+
+        return this.sequelize.sync({ force: true }).bind(this).then(function() {
+          return TimezonedUser.create({});
+        }).then(function(timezonedUser) {
+          this.timezonedUser = timezonedUser;
+          return NormalUser.find(timezonedUser.id);
+        }).then(function(normalUser) {
+          // Expect 7 hours difference, in milliseconds, +/- 1 hour for DST
+          expect(normalUser.createdAt.getTime() - this.timezonedUser.createdAt.getTime()).to.be.closeTo(60 * 60 * 4 * 1000 * -1, 60 * 60 * 1000);
         });
       });
     }
