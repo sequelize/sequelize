055c8dc026c4fc96de6f04926958544149f88556
lebretr <lebretr@gmail.com>
2015-06-07 02:16:18 +0200

    fix pb "createdb mysql or postgres" for others dialects in travis

--- .travis/sequelize/before_script.sh
+++ .travis/sequelize/before_script.sh
@@ -4,4 +4,14 @@ if [ "DIALECT"${DIALECT} = "DIALECToracle" ]
 then
   #install oracledb only for dialect oracle
   npm install oracledb
+else
+	if [ "DIALECT"${DIALECT} = "DIALECTmysql" ]
+	then
+		mysql -e 'create database sequelize_test;'
+	else
+		if [ "DIALECT"${DIALECT} = "DIALECToracle" ]
+		then
+			psql -c 'create database sequelize_test;' -U postgres
+		fi
+	fi
 fi
