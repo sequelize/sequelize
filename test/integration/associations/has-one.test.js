269d5c65a01f7be824a558eb3801d8ac9ec3a6e2
John Giudici <jgiudici@devonit.com>
2015-06-22 15:37:52 -0400

    Throw ForeignKeyConstraintError in mssql when trying to set an association that doesn't exist

--- test/integration/associations/has-one.test.js
+++ test/integration/associations/has-one.test.js
@@ -182,6 +182,23 @@ describe(Support.getTestDialectTeaser('HasOne'), function() {
       });
     });
 
+    it('should throw a ForeignKeyConstraintError if the associated record does not exist', function() {
+      var User = this.sequelize.define('UserXYZ', { username: Sequelize.STRING })
+        , Task = this.sequelize.define('TaskXYZ', { title: Sequelize.STRING });
+
+      User.hasOne(Task);
+
+      return User.sync({ force: true }).then(function() {
+        return Task.sync({ force: true }).then(function() {
+          return expect(Task.create({ title: 'task', UserXYZId: 5 })).to.be.rejectedWith(Sequelize.ForeignKeyConstraintError).then(function () {
+            return Task.create({ title: 'task' }).then(function(task) {
+              return expect(Task.update({ title: 'taskUpdate', UserXYZId: 5 }, { where: { id: task.id } })).to.be.rejectedWith(Sequelize.ForeignKeyConstraintError);
+            });
+          });
+        });
+      });
+    });
+
     it('supports passing the primary key instead of an object', function() {
       var User = this.sequelize.define('UserXYZ', { username: Sequelize.STRING })
         , Task = this.sequelize.define('TaskXYZ', { title: Sequelize.STRING });
