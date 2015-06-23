7007c68cfbf6b9bfe70670a2168182f2f6755f94
Sanketh Katta <sanketh@smartcar.com>
2015-06-23 16:40:53 -0700

    Map raw fields back to attribute names when using `mapToModel`.

--- test/integration/model/update.test.js
+++ test/integration/model/update.test.js
@@ -2,12 +2,20 @@
 
 /* jshint -W030 */
 var Support = require(__dirname + '/../support')
-  , DataTypes = require(__dirname + '/../../../lib/data-types');
+  , DataTypes = require(__dirname + '/../../../lib/data-types')
+  , chai = require('chai')
+  , expect = chai.expect
+  , Support = require(__dirname + '/../support')
+  , current = Support.sequelize
+  , _ = require('lodash');
 
 describe(Support.getTestDialectTeaser('Model'), function() {
   describe('update', function () {
-    it('should only update the passed fields', function () {
-      var Account = this.sequelize.define('Account', {
+
+    var Account;
+
+    beforeEach(function() {
+      Account = this.sequelize.define('Account', {
         ownerId: {
           type: DataTypes.INTEGER,
           allowNull: false,
@@ -17,20 +25,44 @@ describe(Support.getTestDialectTeaser('Model'), function() {
           type: DataTypes.STRING
         }
       });
+      return Account.sync({force: true});
+    });
 
-      return Account.sync({force: true}).then(function () {
+    it('should only update the passed fields', function () {
+      return Account.create({
+        ownerId: 2
+      }).then(function (account) {
+        return Account.update({
+          name: Math.random().toString()
+        }, {
+          where: {
+            id: account.get('id')
+          }
+        });
+      });
+    });
+
+
+    if (_.get(current.dialect.supports, 'returnValues.returning')) {
+      it('should return the updated record', function () {
         return Account.create({
           ownerId: 2
         }).then(function (account) {
           return Account.update({
-            name: Math.random().toString()
+            name: 'FooBar'
           }, {
             where: {
               id: account.get('id')
-            }
+            },
+            returning: true
+          }).spread(function(count, accounts) {
+            var account = accounts[0];
+            expect(account.ownerId).to.be.equal(2);
+            expect(account.name).to.be.equal('FooBar');
           });
         });
       });
-    });
+    }
+
   });
 });
