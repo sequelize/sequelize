a6383513c7ad822006a09dff854f805773a7d55c
Matt Liszewski <mliszewski@peachworks.com>
2015-08-07 14:41:15 +0000

    add test for relationships to object in non public schema

--- test/integration/model/searchPath.test.js
+++ test/integration/model/searchPath.test.js
@@ -16,31 +16,44 @@ describe(Support.getTestDialectTeaser('Model'), function() {
 
     describe('SEARCH PATH', function() {
       before(function() {
-        this.Company = current.define('company', {
+        this.Restaurant = current.define('restaurant', {
             foo: DataTypes.STRING,
             bar: DataTypes.STRING
           },
-          {tableName: "company"});
+          {tableName: "restaurants"});
         this.Location = current.define('location', {
             name: DataTypes.STRING
           },
           {tableName: "locations"});
-        this.Company.belongsTo(this.Location,
+        this.Employee = current.define('employee', {
+            first_name: DataTypes.STRING,
+            last_name: DataTypes.STRING
+          },
+          {tableName: "employees"});
+        this.Restaurant.belongsTo(this.Location,
           {
             foreignKey: 'location_id',
             constraints: false
           });
+        this.Employee.belongsTo(this.Restaurant,
+          {
+            foreignKey: 'restaurant_id',
+            constraints: false
+          });
+        this.Restaurant.hasMany(this.Employee, {
+          foreignKey: 'restaurant_id',
+          constraints: false
+        });
       });
 
       beforeEach(function() {
-        var Company = this.Company;
-        return current.createSchema('schema_one').then(function()
-        {
+        var Restaurant = this.Restaurant;
+        return current.createSchema('schema_one').then(function() {
           return current.createSchema('schema_two');
         }).then(function() {
-          return Company.sync({force: true, searchPath: SEARCH_PATH_ONE})
+          return Restaurant.sync({force: true, searchPath: SEARCH_PATH_ONE})
             .then(function() {
-              return Company.sync({force: true, searchPath: SEARCH_PATH_TWO})
+              return Restaurant.sync({force: true, searchPath: SEARCH_PATH_TWO})
                 .then(function() {
                 })
                 .catch(function(err) {
@@ -54,20 +67,20 @@ describe(Support.getTestDialectTeaser('Model'), function() {
       });
 
       afterEach(function() {
-        return current.dropSchema('schema_one').then(function(){
+        return current.dropSchema('schema_one').then(function() {
           return current.dropSchema('schema_two');
         });
       });
 
-      describe('Add data via model.create, retrieve via model.findOne',function() {
+      describe('Add data via model.create, retrieve via model.findOne', function() {
         it('should be able to insert data into the table in schema_one using create', function() {
-          var Company = this.Company;
+          var Restaurant = this.Restaurant;
 
-          return Company.create({
+          return Restaurant.create({
             foo: 'one',
             location_id: locationId
           }, {searchPath: SEARCH_PATH_ONE}).then(function() {
-            return Company.findOne({
+            return Restaurant.findOne({
               where: {foo: 'one'}, searchPath: SEARCH_PATH_ONE
             }).then(function(obj) {
               expect(obj).to.not.be.null;
@@ -77,9 +90,9 @@ describe(Support.getTestDialectTeaser('Model'), function() {
         });
 
         it('should fail to insert data into schema_two using create', function() {
-          var Company = this.Company;
+          var Restaurant = this.Restaurant;
 
-          return Company.create({
+          return Restaurant.create({
             foo: 'test'
           }, {searchPath: SEARCH_PATH_TWO}).then(function() {
           }).catch(function(err) {
@@ -88,13 +101,13 @@ describe(Support.getTestDialectTeaser('Model'), function() {
         });
 
         it('should be able to insert data into the table in schema_two using create', function() {
-          var Company = this.Company;
+          var Restaurant = this.Restaurant;
 
-          return Company.create({
+          return Restaurant.create({
             foo: 'two',
             location_id: locationId
           }, {searchPath: SEARCH_PATH_TWO}).then(function() {
-            return Company.findOne({
+            return Restaurant.findOne({
               where: {foo: 'two'}, searchPath: SEARCH_PATH_TWO
             }).then(function(obj) {
               expect(obj).to.not.be.null;
@@ -104,23 +117,23 @@ describe(Support.getTestDialectTeaser('Model'), function() {
         });
 
         it('should fail to find schema_one object in schema_two', function() {
-          var Company = this.Company;
+          var Restaurant = this.Restaurant;
 
-          return Company.findOne({where: {foo: 'one'}, searchPath: SEARCH_PATH_TWO}).then(function(CompanyObj) {
-            expect(CompanyObj).to.be.null;
+          return Restaurant.findOne({where: {foo: 'one'}, searchPath: SEARCH_PATH_TWO}).then(function(RestaurantObj) {
+            expect(RestaurantObj).to.be.null;
           });
         });
 
         it('should fail to find schema_two object in schema_one', function() {
-          var Company = this.Company;
+          var Restaurant = this.Restaurant;
 
-          return Company.findOne({where: {foo: 'two'}, searchPath: SEARCH_PATH_ONE}).then(function(CompanyObj) {
-            expect(CompanyObj).to.be.null;
+          return Restaurant.findOne({where: {foo: 'two'}, searchPath: SEARCH_PATH_ONE}).then(function(RestaurantObj) {
+            expect(RestaurantObj).to.be.null;
           });
         });
       });
 
-      describe('Get shared associated data via include',function() {
+      describe('Get shared associated data via include', function() {
         beforeEach(function() {
           var Location = this.Location;
 
@@ -140,14 +153,14 @@ describe(Support.getTestDialectTeaser('Model'), function() {
         });
 
         it('should be able to insert and retrieve associated data into the table in schema_one', function() {
-          var Company = this.Company;
+          var Restaurant = this.Restaurant;
           var Location = this.Location;
 
-          return Company.create({
+          return Restaurant.create({
             foo: 'one',
             location_id: locationId
           }, {searchPath: SEARCH_PATH_ONE}).then(function() {
-            return Company.findOne({
+            return Restaurant.findOne({
               where: {foo: 'one'}, include: [{
                 model: Location, as: 'location'
               }], searchPath: SEARCH_PATH_ONE
@@ -162,14 +175,14 @@ describe(Support.getTestDialectTeaser('Model'), function() {
 
 
         it('should be able to insert and retrieve associated data into the table in schema_two', function() {
-          var Company = this.Company;
+          var Restaurant = this.Restaurant;
           var Location = this.Location;
 
-          return Company.create({
+          return Restaurant.create({
             foo: 'two',
             location_id: locationId
           }, {searchPath: SEARCH_PATH_TWO}).then(function() {
-            return Company.findOne({
+            return Restaurant.findOne({
               where: {foo: 'two'}, include: [{
                 model: Location, as: 'location'
               }], searchPath: SEARCH_PATH_TWO
@@ -178,6 +191,110 @@ describe(Support.getTestDialectTeaser('Model'), function() {
               expect(obj.foo).to.equal('two');
               expect(obj.location).to.not.be.null;
               expect(obj.location.name).to.equal('HQ');
+            });
+          });
+        });
+      });
+
+      describe('Get account specific associated data via include', function() {
+        beforeEach(function() {
+          var Employee = this.Employee;
+          return Employee.sync({force: true, searchPath: SEARCH_PATH_ONE})
+            .then(function() {
+              return Employee.sync({force: true, searchPath: SEARCH_PATH_TWO})
+                .then(function() {
+                })
+                .catch(function(err) {
+                  expect(err).to.be.null;
+                });
+            })
+            .catch(function(err) {
+              expect(err).to.be.null;
+            });
+        });
+
+        it('should be able to insert and retrieve associated data into the table in schema_one', function() {
+          var Restaurant = this.Restaurant;
+          var Employee = this.Employee;
+          var restaurantId;
+
+          return Restaurant.create({
+            foo: 'one'
+          }, {searchPath: SEARCH_PATH_ONE}).then(function() {
+            return Restaurant.findOne({
+              where: {foo: 'one'}, searchPath: SEARCH_PATH_ONE
+            }).then(function(obj) {
+              expect(obj).to.not.be.null;
+              expect(obj.foo).to.equal('one');
+              restaurantId = obj.id;
+              return Employee.create({
+                first_name: 'Restaurant',
+                last_name: 'one',
+                restaurant_id: restaurantId
+              }, {searchPath: SEARCH_PATH_ONE}).then(function() {
+                return Restaurant.findOne({
+                  where: {foo: 'one'}, searchPath: SEARCH_PATH_ONE, include: [{
+                    model: Employee, as: 'employees'
+                  }]
+                }).then(function(obj) {
+                  expect(obj).to.not.be.null;
+                  expect(obj.employees).to.not.be.null;
+                  expect(obj.employees.length).to.equal(1);
+                  expect(obj.employees[0].last_name).to.equal('one');
+                  return Employee.findOne({
+                    where: {last_name: 'one'}, searchPath: SEARCH_PATH_ONE, include: [{
+                      model: Restaurant, as: 'restaurant'
+                    }]
+                  }).then(function(obj) {
+                    expect(obj).to.not.be.null;
+                    expect(obj.restaurant).to.not.be.null;
+                    expect(obj.restaurant.foo).to.equal('one');
+                  });
+                });
+              });
+            });
+          });
+        });
+
+        it('should be able to insert and retrieve associated data into the table in schema_two', function() {
+          var Restaurant = this.Restaurant;
+          var Employee = this.Employee;
+          var restaurantId;
+
+          return Restaurant.create({
+            foo: 'two'
+          }, {searchPath: SEARCH_PATH_TWO}).then(function() {
+            return Restaurant.findOne({
+              where: {foo: 'two'}, searchPath: SEARCH_PATH_TWO
+            }).then(function(obj) {
+              expect(obj).to.not.be.null;
+              expect(obj.foo).to.equal('two');
+              restaurantId = obj.id;
+              return Employee.create({
+                first_name: 'Restaurant',
+                last_name: 'two',
+                restaurant_id: restaurantId
+              }, {searchPath: SEARCH_PATH_TWO}).then(function() {
+                return Restaurant.findOne({
+                  where: {foo: 'two'}, searchPath: SEARCH_PATH_TWO, include: [{
+                    model: Employee, as: 'employees'
+                  }]
+                }).then(function(obj) {
+                  expect(obj).to.not.be.null;
+                  expect(obj.employees).to.not.be.null;
+                  expect(obj.employees.length).to.equal(1);
+                  expect(obj.employees[0].last_name).to.equal('two');
+                  return Employee.findOne({
+                    where: {last_name: 'two'}, searchPath: SEARCH_PATH_TWO, include: [{
+                      model: Restaurant, as: 'restaurant'
+                    }]
+                  }).then(function(obj) {
+                    expect(obj).to.not.be.null;
+                    expect(obj.restaurant).to.not.be.null;
+                    expect(obj.restaurant.foo).to.equal('two');
+                  });
+                });
+              });
             });
           });
         });
