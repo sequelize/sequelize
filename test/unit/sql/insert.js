d24c902b687fe5dc0304a0bcd1e3814c21ea437a
Ryan Fink <ryanjfink@gmail.com>
2015-08-20 17:33:34 -0400

    # This is a combination of 4 commits. # The first commit's message is: Fix for onDuplicateKeyUpdate in mysql
    # The 2nd commit message will be skipped:
    
    #	Add test for onDuplicateKeyUpdate
    
    # The 3rd commit message will be skipped:
    
    #	Fix whitespace oops
    
    # The 4th commit message will be skipped:
    
    #	Fix for onDuplicateKeyUpdate in mysql

--- test/unit/sql/insert.js
+++ test/unit/sql/insert.js
@@ -35,4 +35,41 @@ describe(Support.getTestDialectTeaser('SQL'), function() {
     });
 
   });
+
+  describe('bulkCreate', function () {
+    it('bulk create with onDuplicateKeyUpdate', function () {
+      // Skip mssql for now, it seems broken
+      if (Support.getTestDialect() === 'mssql') {
+        return;
+      }
+
+      var User = Support.sequelize.define('user', {
+        username: {
+          type: DataTypes.STRING,
+          field: 'user_name'
+        },
+        password: {
+          type: DataTypes.STRING,
+          field: 'pass_word'
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          field: 'created_at'
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          field: 'updated_at'
+        }
+      },{
+        timestamps:true
+      });
+
+      expectsql(sql.bulkInsertQuery(User.tableName, [{ user_name: 'testuser', pass_word: '12345' }], { updateOnDuplicate: ['username', 'password', 'updatedAt'] }, User.rawAttributes),
+      {
+        default:'INSERT INTO `users` (`user_name`,`pass_word`) VALUES (\'testuser\',\'12345\');',
+        postgres:'INSERT INTO "users" ("user_name","pass_word") VALUES (\'testuser\',\'12345\');',
+        mysql:'INSERT INTO `users` (`user_name`,`pass_word`) VALUES (\'testuser\',\'12345\') ON DUPLICATE KEY UPDATE `user_name`=VALUES(`user_name`),`pass_word`=VALUES(`pass_word`),`updated_at`=VALUES(`updated_at`);'
+      });
+    });
+  });
 });
