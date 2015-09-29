e2f85fb8ddf9a6c7f78f965a0c121f6c7d44209a
Matt Liszewski <mliszewski@peachworks.com>
2015-09-29 13:50:39 +0000

    optimize index lookup when schema is known

--- test/unit/sql/index.test.js
+++ test/unit/sql/index.test.js
@@ -125,5 +125,26 @@ suite(Support.getTestDialectTeaser('SQL'), function() {
         });
       });
     }
+
+    if (current.dialect.name === 'postgres') {
+      test('show indexes', function () {
+        expectsql(sql.showIndexesQuery('table'), {
+          postgres: 'SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, ' +
+          'array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) ' +
+          'AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a ' +
+          'WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND ' +
+          't.relkind = \'r\' and t.relname = \'table\' GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;'
+        });
+
+        expectsql(sql.showIndexesQuery({tableName: 'table', schema: 'schema'}), {
+          postgres: 'SELECT i.relname AS name, ix.indisprimary AS primary, ix.indisunique AS unique, ix.indkey AS indkey, ' +
+          'array_agg(a.attnum) as column_indexes, array_agg(a.attname) AS column_names, pg_get_indexdef(ix.indexrelid) ' +
+          'AS definition FROM pg_class t, pg_class i, pg_index ix, pg_attribute a, pg_namespace s ' +
+          'WHERE t.oid = ix.indrelid AND i.oid = ix.indexrelid AND a.attrelid = t.oid AND ' +
+          't.relkind = \'r\' and t.relname = \'table\' AND s.oid = t.relnamespace AND s.nspname = \'schema\' ' +
+          'GROUP BY i.relname, ix.indexrelid, ix.indisprimary, ix.indisunique, ix.indkey ORDER BY i.relname;'
+        });
+      });
+    }
   });
 });
