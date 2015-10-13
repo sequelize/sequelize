bf7371e7197e9c77d4e0594b694d0e1f34666bc2
Jérôme Steunou <jerome.steunou@groupe-regards.com>
2015-10-13 10:28:47 +0200

    Add failing unit tests

--- test/unit/model/bulkcreate.test.js
+++ test/unit/model/bulkcreate.test.js
@@ -19,6 +19,26 @@ describe(Support.getTestDialectTeaser('Model'), function() {
       }
     }, { timestamps: false });
 
+    var ModelWithTS = current.define('modelwithts', {
+      accountId: {
+        type: DataTypes.INTEGER(11).UNSIGNED,
+        allowNull: false,
+        field: 'account_id'
+      },
+      createdAt: {
+          type: DataTypes.DATE,
+          field: 'created_at'
+      },
+      updatedAt: {
+          type: DataTypes.DATE,
+          field: 'updated_at'
+      },
+      deletedAt: {
+          type: DataTypes.DATE,
+          field: 'deleted_at'
+      }
+    }, { timestamps: true });
+
     before(function () {
 
       this.stub = sinon.stub(current.getQueryInterface(), 'bulkInsert', function () {
@@ -42,6 +62,19 @@ describe(Support.getTestDialectTeaser('Model'), function() {
           expect(this.stub.getCall(0).args[1]).to.deep.equal([
             { account_id: 42, id: null }
           ]);
+        });
+      });
+    });
+
+    describe('renamed fields', function () {
+      it('should not fail for timestamps', function () {
+        return ModelWithTS.bulkCreate([
+          { accountId: 42 }
+        ]).bind(this).then(function () {
+          var firstCreatedModel = this.stub.getCall(0).args[1][0];
+          expect(firstCreatedModel).to.have.property('account_id');
+          expect(firstCreatedModel).to.have.property('created_at');
+          expect(firstCreatedModel).to.have.property('updated_at');
         });
       });
     });
