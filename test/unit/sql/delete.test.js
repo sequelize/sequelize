7bd4c476716a60c9c271c9dbf97c400ebe1baf2c
Michael Robinson <mike@pagesofinterest.net>
2015-11-28 22:23:41 +1300

    Added some tests

--- test/unit/sql/delete.test.js
+++ test/unit/sql/delete.test.js
@@ -36,10 +36,85 @@ suite(Support.getTestDialectTeaser('SQL'), function() {
             options,
             User
           ), {
-            postgres: 'DELETE FROM "test_user" WHERE "test_user_id" IN (SELECT "test_user_id" FROM "test_user" WHERE "test_user_id" = 100 LIMIT 1)',
+            postgres: 'DELETE FROM "public"."test_user" WHERE "test_user_id" IN (SELECT "test_user_id" FROM "public"."test_user" WHERE "test_user_id" = 100 LIMIT 1)',
             sqlite:   'DELETE FROM `test_user` WHERE `test_user_id` = 100',
             mssql:    'DELETE TOP(1) FROM [test_user] WHERE [test_user_id] = 100; SELECT @@ROWCOUNT AS AFFECTEDROWS;',
             default:  'DELETE FROM [test_user] WHERE [test_user_id] = 100 LIMIT 1'
+          }
+        );
+      });
+
+    });
+
+    suite('include the correct schema during bulk delete when appropriate', function () {
+
+      var User = current.define('test_user', {
+        id: {
+          type:       Sequelize.INTEGER,
+          primaryKey: true,
+          field:      "test_user_id",
+        }
+      }, {
+        schema: 'web',
+        freezeTableName: true,
+        timestamps:false
+      });
+
+      var options = {
+        table: 'test_user',
+        schema: 'web',
+        where: {
+          'test_user_id': 100
+        }
+      };
+
+      test(util.inspect(options, {depth: 2}), function () {
+        return expectsql(
+          sql.deleteQuery(
+            options.table,
+            options.where,
+            options,
+            User
+          ), {
+            postgres: 'DELETE FROM "web"."test_user" WHERE "test_user_id" IN (SELECT "test_user_id" FROM "web"."test_user" WHERE "test_user_id" = 100 LIMIT 1)',
+            sqlite:   'DELETE FROM `test_user` WHERE `test_user_id` = 100',
+            mssql:    'DELETE TOP(1) FROM [test_user] WHERE [test_user_id] = 100; SELECT @@ROWCOUNT AS AFFECTEDROWS;',
+            default:  'DELETE FROM [test_user] WHERE [test_user_id] = 100 LIMIT 1'
+          }
+        );
+      });
+
+    });
+
+    suite('include the correct schema during bulk delete + truncate when appropriate', function () {
+
+      var User = current.define('test_user', {
+        id: {
+          type:       Sequelize.INTEGER,
+          primaryKey: true,
+          field:      "test_user_id",
+        }
+      }, {
+        freezeTableName: true,
+        timestamps:false
+      });
+
+      var options = {
+        table: 'test_user',
+        truncate: true
+      };
+
+      test(util.inspect(options, {depth: 2}), function () {
+        return expectsql(
+          sql.deleteQuery(
+            options.table,
+            options.where,
+            options,
+            User
+          ), {
+            postgres: 'TRUNCATE "public"."test_user"',
+            sqlite:   'TRUNCATE `test_user`',
+            default:  'TRUNCATE [test_user]'
           }
         );
       });
