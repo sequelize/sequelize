f07511c4b9bbebd5c25e7433529d5ec50f030361
Sushant <sushantdhiman@outlook.com>
2015-12-15 12:25:45 +0530

    issue 4307 : Added the failing test

---
+++ test/integration/dialects/postgres/connection-manager.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* jshint -W030 */
+var chai = require('chai')
+  , expect = chai.expect
+  , Support = require(__dirname + '/../../support')
+  , dialect = Support.getTestDialect()
+  , config = require(__dirname + '/../../../config/config')
+  , DataTypes = require(__dirname + '/../../../../lib/data-types')
+  , _ = require('lodash');
+
+if (dialect.match(/^postgres/)) {
+  describe('[POSTGRES Specific] Sequelize', function() {
+    it('should correctly parse the moment based timezone', function() {
+      var options = _.extend(this.sequelize.options, { timezone: 'Asia/Kolkata', timestamps: true });
+      var sequelize = Support.createSequelizeInstance(options);
+
+      var tzTable = sequelize.define('tz_table', { foo: DataTypes.STRING });
+      return tzTable.sync({force: true}).then(function() {
+        return tzTable.create({foo: 'test'}).then(function(row) {
+          expect(row).to.be.not.null;
+        }).catch(function(e) {
+          expect(e).to.be.null;
+        });
+      })
+    });
+  });
+}
