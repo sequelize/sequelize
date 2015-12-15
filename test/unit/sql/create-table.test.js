a91d8b6875b6fac8a7d432d9de2673a174250221
Sushant <sushantdhiman@outlook.com>
2015-12-15 13:20:22 +0530

    issue(5015) Added unit tests

--- test/unit/sql/create-table.test.js
+++ test/unit/sql/create-table.test.js
@@ -5,19 +5,19 @@ var Support   = require(__dirname + '/../support')
   , DataTypes = require(__dirname + '/../../../lib/data-types')
   , expectsql = Support.expectsql
   , current   = Support.sequelize
-  , sql       = current.dialect.QueryGenerator;
+  , sql       = current.dialect.QueryGenerator
+  , _         = require('lodash');
 
 
 describe(Support.getTestDialectTeaser('SQL'), function() {
   describe('createTable', function () {
+    var FooUser = current.define('user', {
+      mood: DataTypes.ENUM('happy', 'sad')
+    },{
+      schema: 'foo',
+      timestamps: false
+    });
     describe('with enums', function () {
-      var FooUser = current.define('user', {
-        mood: DataTypes.ENUM('happy', 'sad')
-      },{
-        schema: 'foo',
-        timestamps: false
-      });
-
       it('references enum in the right schema #3171', function () {
         expectsql(sql.createTableQuery(FooUser.getTableName(), sql.attributesToSQL(FooUser.rawAttributes), { }), {
           sqlite: 'CREATE TABLE IF NOT EXISTS `foo.users` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `mood` TEXT);',
@@ -27,5 +27,29 @@ describe(Support.getTestDialectTeaser('SQL'), function() {
         });
       });
     });
+    if (current.dialect.name === 'postgres') {
+      describe('IF NOT EXISTS version check', function() {
+        var modifiedSQL = _.clone(sql);
+        var createTableQueryModified = sql.createTableQuery.bind(modifiedSQL);
+        it('it will not have IF NOT EXISTS for version 9.0 or below', function () {
+          modifiedSQL.sequelize.options.databaseVersion = '9.0.0';
+          expectsql(createTableQueryModified(FooUser.getTableName(), sql.attributesToSQL(FooUser.rawAttributes), { }), {
+            postgres: 'CREATE TABLE "foo"."users" ("id"   SERIAL , "mood" "foo"."enum_users_mood", PRIMARY KEY ("id"));'
+          });
+        });
+        it('it will have IF NOT EXISTS for version 9.1 or above', function () {
+          modifiedSQL.sequelize.options.databaseVersion = '9.1.0';
+          expectsql(createTableQueryModified(FooUser.getTableName(), sql.attributesToSQL(FooUser.rawAttributes), { }), {
+            postgres: 'CREATE TABLE IF NOT EXISTS "foo"."users" ("id"   SERIAL , "mood" "foo"."enum_users_mood", PRIMARY KEY ("id"));'
+          });
+        });
+        it('it will have IF NOT EXISTS for default version', function () {
+          modifiedSQL.sequelize.options.databaseVersion = 0;
+          expectsql(createTableQueryModified(FooUser.getTableName(), sql.attributesToSQL(FooUser.rawAttributes), { }), {
+            postgres: 'CREATE TABLE IF NOT EXISTS "foo"."users" ("id"   SERIAL , "mood" "foo"."enum_users_mood", PRIMARY KEY ("id"));'
+          });
+        });
+      });
+    }
   });
 });
