58146cc720835d2b69c84a8cf640e93be8eefffd
ming.deng <ming.deng@dji.com>
2016-03-18 11:00:23 +0800

    refactor addColumn unit test use the query interface directly

--- test/unit/sql/add-column.test.js
+++ test/unit/sql/add-column.test.js
@@ -4,9 +4,8 @@
 var Support   = require(__dirname + '/../support')
   , DataTypes = require('../../../lib/data-types')
   , expectsql = Support.expectsql
-  , sinon = require('sinon')
   , current   = Support.sequelize
-  , Promise = current.Promise;
+  , sql       = current.dialect.QueryGenerator;
 
 
 if (current.dialect.name === 'mysql') {
@@ -18,40 +17,20 @@ if (current.dialect.name === 'mysql') {
           type: DataTypes.INTEGER,
           primaryKey: true,
           autoIncrement: true
-        },
-        level_id: {
-          type: DataTypes.INTEGER
         }
       }, { timestamps: false });
 
-      before(function () {
-
-        this.stub = sinon.stub(current, 'query', function (sql) {
-          return Promise.resolve(sql);
-        });
-      });
-
-      beforeEach(function () {
-        this.stub.reset();
-      });
-
-      after(function () {
-        this.stub.restore();
-      });
-
       it('properly generate alter queries', function(){
-        return current.getQueryInterface().addColumn(Model.getTableName(), 'level_id', {
+        return expectsql(sql.addColumnQuery(Model.getTableName(), 'level_id', current.normalizeAttribute({
           type: DataTypes.FLOAT,
           allowNull: false,
-        }).then(function(sql){
-          expectsql(sql, {
+        })), {
             mysql: 'ALTER TABLE `users` ADD `level_id` FLOAT NOT NULL;',
-          });
         });
       });
 
       it('properly generate alter queries for foreign keys', function(){
-        return current.getQueryInterface().addColumn(Model.getTableName(), 'level_id', {
+        return expectsql(sql.addColumnQuery(Model.getTableName(), 'level_id', current.normalizeAttribute({
           type: DataTypes.INTEGER,
           references: {
             model: 'level',
@@ -59,10 +38,8 @@ if (current.dialect.name === 'mysql') {
           },
           onUpdate: 'cascade',
           onDelete: 'cascade'
-        }).then(function(sql){
-          expectsql(sql, {
+        })), {
             mysql: 'ALTER TABLE `users` ADD `level_id` INTEGER, ADD CONSTRAINT `level_id_foreign_idx` FOREIGN KEY (`level_id`) REFERENCES `level` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;',
-          });
         });
       });
 
