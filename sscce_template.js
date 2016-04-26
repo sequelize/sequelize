70e92ce2dcb55d70ae1a48eb44de30736cf17b27
Mick Hansen <doomschild@gmail.com>
2016-04-26 08:33:13 +0200

    adds a sscce template where code can easily be copied and run inside docker (#5781)

---
+++ sscce_template.js
@@ -0,0 +1,8 @@
+/*
+ * Copy this file to ./sscce.js
+ * Add code from issue
+ * npm run sscce-{dialect}
+ */
+
+var Sequelize = require('./index');
+var sequelize = require('./test/support').createSequelizeInstance();
