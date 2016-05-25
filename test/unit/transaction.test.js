463edf73e1b51ec17319f20d86f928f8611da50e
Sushant <sushantdhiman@outlook.com>
2016-05-25 18:09:59 +0530

    auto commit test refactor

--- test/unit/transaction.test.js
+++ test/unit/transaction.test.js
@@ -11,21 +11,15 @@ var chai = require('chai')
   , Promise = Sequelize.Promise;
 
 describe('Transaction', function() {
-  var sqlIndex = [];
 
   before(function () {
-    this.stub = sinon.stub(current, 'query', function (sql) {
-      sqlIndex.push(sql);
-      return Promise.resolve({});
-    });
+    this.stub = sinon.stub(current, 'query').returns(Promise.resolve({}));
 
-    this.stubConnection = sinon.stub(current.connectionManager, 'getConnection', function (options) {
-      return Promise.resolve({ uuid: options.uuid, close : function() { }});
-    });
+    this.stubConnection = sinon.stub(current.connectionManager, 'getConnection')
+                          .returns(Promise.resolve({ uuid: 'ssfdjd-434fd-43dfg23-2d', close : function() { }}));
   });
 
   beforeEach(function () {
-    sqlIndex = [];
     this.stub.reset();
     this.stubConnection.reset();
   });
@@ -40,7 +34,8 @@ describe('Transaction', function() {
       var self = this;
       return current.transaction(function(t) {
         expect(self.stub.calledTwice).to.be.ok;
-        expect(sqlIndex.join(' ').indexOf('SET autocommit = 1;')).to.be.ok;
+        expect(self.stub.args[0][0]).to.be.eql('START TRANSACTION;');
+        expect(self.stub.args[1][0]).to.be.eql('SET autocommit = 1;');
         return Promise.resolve({});
       });
     });
@@ -49,7 +44,8 @@ describe('Transaction', function() {
       var self = this;
       return current.transaction(function(t) {
         expect(self.stub.calledOnce).to.be.ok;
-        expect(sqlIndex.join(' ').indexOf('SET autocommit = 1;')).to.be.eql(-1);
+        expect(self.stub.args[1]).to.be.empty;
+        expect(self.stub.args[0].join(' ').indexOf('autocommit')).to.be.eql(-1);
         return Promise.resolve({});
       });
     });
