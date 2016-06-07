82fa86c2c3c3d04dcec3f9aeec833d2814890af1
Felix Becker <felix.b@outlook.com>
2016-06-07 11:41:28 +0200

    Add stack trace test

---
+++ test/unit/errors.test.js
@@ -0,0 +1,22 @@
+'use strict';
+/* jshint expr: true */
+const errors = require('../../lib/errors');
+const expect = require('chai').expect;
+
+describe('errors', () => {
+  it('should maintain stack trace', () => {
+    function throwError() {
+      throw new errors.ValidationError('this is a message');
+    }
+    let err;
+    try {
+      throwError();
+    } catch (error) {
+      err = error;
+    }
+    expect(err).to.exist;
+    const stackParts = err.stack.split('\n');
+    expect(stackParts[0]).to.equal('SequelizeValidationError: this is a message');
+    expect(stackParts[1]).to.match(/^    at throwError \(.*errors.test.js:\d+:\d+\)$/);
+  });
+});
