97de7c6d4c45874ac1fe074f77db5294dc1a5278
Ruben Bridgewater <ruben.bridgewater@fintura.de>
2016-06-30 21:38:13 +0200

    Fix model.count with includes and specified column

--- test/integration/model/count.test.js
+++ test/integration/model/count.test.js
@@ -150,5 +150,28 @@ describe(Support.getTestDialectTeaser('Model'), function() {
       });
     });
 
+    it('should be able to specify column for COUNT() with includes', function() {
+      return this.sequelize.sync({ force: true }).then(() =>
+        this.User.bulkCreate([
+          { username: 'ember' , age: 10},
+          { username: 'angular' , age: 20},
+          { username: 'mithril' , age: 10}
+        ])
+      ).then(() =>
+        this.User.count({
+          col: 'username',
+          distinct: true,
+          include: [this.Project]
+        })
+      ).then((count) => {
+        expect(parseInt(count)).to.be.eql(3);
+        return this.User.count({
+          col: 'age',
+          distinct: true,
+          include: [this.Project]
+        });
+      }).then((count) => expect(parseInt(count)).to.be.eql(2));
+    });
+
   });
 });
