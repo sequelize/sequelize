81d572859553f86ab188f6c241569d60b2a3d3bb
fogine <fogine@opmbx.org>
2016-07-14 13:00:54 +0200

    fix test case bug for mssql dialect

--- test/unit/sql/select.test.js
+++ test/unit/sql/select.test.js
@@ -247,6 +247,7 @@ suite(Support.getTestDialectTeaser('SQL'), function() {
         order: '[user].[last_name] ASC'.replace(/\[/g, Support.sequelize.dialect.TICK_CHAR_LEFT).replace(/\]/g, Support.sequelize.dialect.TICK_CHAR_RIGHT),
         limit: 30,
         offset: 10,
+        hasMultiAssociation: true,//must be set only for mssql dialect here
         subQuery: true
       }, {
           default: 'SELECT [user].*, [POSTS].[id] AS [POSTS.id], [POSTS].[title] AS [POSTS.title] FROM (' +
