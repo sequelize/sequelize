76254f8d3407f9f5921af1515de00cc40c428d52
Sushant <sushantdhiman@outlook.com>
2016-07-28 18:29:33 +0530

    used domains to catch errors properly

--- test/integration/sequelize.test.js
+++ test/integration/sequelize.test.js
@@ -991,7 +991,6 @@ describe(Support.getTestDialectTeaser('Sequelize'), function() {
           } else if (dialect === 'mssql') {
             expect(err.message).to.match(/.*ECONNREFUSED.*/);
           } else {
-            console.log(err)
             expect(err.message.toString()).to.match(/.*Access\ denied.*/);
           }
         });
