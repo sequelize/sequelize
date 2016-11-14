e6aa24677048b8561597f681ac885c54b87c4c9b
Guillaume Grimbert <ggrimbert@kimoce.com>
2016-11-14 16:14:30 +0100

    feat(oracle) : Connection-manager fonctionnel + suppression test spécifique mssql

--- test/unit/dialects/oracle/connection-manager.js
+++ test/unit/dialects/oracle/connection-manager.js
@@ -9,32 +9,112 @@ var tedious = require('tedious')
   , sinon = require('sinon')
   , connectionStub = sinon.stub(tedious, 'Connection');
 
-connectionStub.returns({on: function () {}});
+connectionStub.returns({ on: function () { } });
 
 describe('[ORACLE] Connection Manager', function () {
   var instance
     , config;
-  beforeEach(function () {
+
+  this.timeout(50000);
+
+  it('full connectString, should connect to Oracle', function (done) {
+    //  expect(config.dialectOptions.domain).to.equal('TEST.COM');
     config = {
       dialect: 'oracle',
       database: 'none',
       username: 'DEV_RD',
       password: 'DEV_RD',
-      host: '',
-      port: 1521,
-      pool: {},
-      connecString : 'vm2008ora12hot:1521/ORCL12HOT.kimdomain.local'
+      connectString: 'vm2008ora12hot:1521/ORCL12HOT.kimdomain.local'
     };
-    instance = new Sequelize(config.database
-                             , config.username
-                             , config.password
-                             , config);    
+    instance = new Sequelize(config.database, config.username, config.password, config);
+
+    instance.dialect.connectionManager.$connect(config)
+      .then(result => {
+        expect(instance.getDialect()).to.equal('oracle');
+        instance.dialect.connectionManager.$disconnect(result)
+          .then(result => {
+            done();
+          })
+      })
+      .catch(error => {
+        done(error);
+      });
   });
-  
-  it('connectionManager.$connect() Does not delete `domain` from config.dialectOptions',
-     function () {
-      //  expect(config.dialectOptions.domain).to.equal('TEST.COM');
-       instance.dialect.connectionManager.$connect(config);
-       expect(instance.getDialect()).to.equal('oracle');
-     });
+
+  it('connectString with only service_name, should connect to Oracle', function (done) {
+    //  expect(config.dialectOptions.domain).to.equal('TEST.COM');
+    config = {
+      dialect: 'oracle',
+      database: 'none',
+      host: 'vm2008ora12hot',
+      username: 'DEV_RD',
+      password: 'DEV_RD',
+      connectString: 'ORCL12HOT.kimdomain.local'
+    };
+    instance = new Sequelize(config.database, config.username, config.password, config);
+
+    instance.dialect.connectionManager.$connect(config)
+      .then(result => {
+        expect(instance.getDialect()).to.equal('oracle');
+        instance.dialect.connectionManager.$disconnect(result)
+          .then(result => {
+            done();
+          })
+      })
+      .catch(error => {
+        done(error);
+      });
+  });
+
+  it('connectString with only service_name no host, should fail', function (done) {
+    //  expect(config.dialectOptions.domain).to.equal('TEST.COM');
+    config = {
+      dialect: 'oracle',
+      database: 'none',
+      host: '',
+      username: 'DEV_RD',
+      password: 'DEV_RD',
+      connectString: 'ORCL12HOT.kimdomain.local'
+    };
+    instance = new Sequelize(config.database, config.username, config.password, config);
+
+    instance.dialect.connectionManager.$connect(config)
+      .then(result => {
+        done("You shall not pass");
+        expect(instance.getDialect()).to.equal('oracle');
+        instance.dialect.connectionManager.$disconnect(result)
+          .then(result => {
+            done("You shall not pass");
+          })
+      })
+      .catch(error => {
+        done();
+      });
+  });
+
+  it('connectString empty, should fail', function (done) {
+    //  expect(config.dialectOptions.domain).to.equal('TEST.COM');
+    config = {
+      dialect: 'oracle',
+      database: 'none',
+      host: 'vm2008ora12hot',
+      username: 'DEV_RD',
+      password: 'DEV_RD',
+      connectString: ''
+    };
+    instance = new Sequelize(config.database, config.username, config.password, config);
+
+    instance.dialect.connectionManager.$connect(config)
+      .then(result => {
+        done("You shall not pass");
+        expect(instance.getDialect()).to.equal('oracle');
+        instance.dialect.connectionManager.$disconnect(result)
+          .then(result => {
+            done("You shall not pass");
+          })
+      })
+      .catch(error => {
+        done();
+      });
+  });
 });
