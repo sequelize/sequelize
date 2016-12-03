7c622dfca6c1e2cc675c6b7c30e3b4cf8fa6bc79
Sushant <sushantdhiman@outlook.com>
2016-12-03 16:34:25 +0530

    fix: no value in built-in/custom validator

--- test/unit/model/validation.test.js
+++ test/unit/model/validation.test.js
@@ -201,6 +201,7 @@ describe(Support.getTestDialectTeaser('InstanceValidator'), function() {
 
           return expect(failingUser.validate()).to.be.rejected.then(function(_errors) {
             expect(_errors.get('name')[0].message).to.equal(message);
+            expect(_errors.get('name')[0].value).to.equal(failingValue);
           });
         });
       }
