dcbd45258c4e0ab9becf4111e9ab5d2999cdaa12
David Tsai <david@convoy.com>
2016-12-05 11:53:18 -0800

    Use ES6 style in tests

--- test/integration/model/optimistic_locking.test.js
+++ test/integration/model/optimistic_locking.test.js
@@ -20,48 +20,48 @@ describe(Support.getTestDialectTeaser('Model'), function() {
     });
 
     it('should increment the version on save', function() {
-      return Account.create({number: 1}).then(function(account) {
+      return Account.create({number: 1}).then(account => {
         account.number += 1;
         expect(account.version).to.eq(0);
         return account.save();
-      }).then(function(account) {
+      }).then(account => {
         expect(account.version).to.eq(1);
       });
     });
 
     it('should increment the version on update', function() {
-      return Account.create({number: 1}).then(function(account) {
+      return Account.create({number: 1}).then(account => {
         expect(account.version).to.eq(0);
         return account.update({ number: 2 });
-      }).then(function(account) {
+      }).then(account => {
         expect(account.version).to.eq(1);
         account.number += 1;
         return account.save();
-      }).then(function(account) {
+      }).then(account => {
         expect(account.number).to.eq(3);
         expect(account.version).to.eq(2);
       });
     });
 
     it('prevents stale instances from being saved', function() {
-      return expect(Account.create({number: 1}).then(function(accountA) {
-        return Account.findById(accountA.id).then(function(accountB) {
+      return expect(Account.create({number: 1}).then(accountA => {
+        return Account.findById(accountA.id).then(accountB => {
           accountA.number += 1;
           return accountA.save().then(function() { return accountB; });
         });
-      }).then(function(accountB) {
+      }).then(accountB => {
         accountB.number += 1;
         return accountB.save();
       })).to.eventually.be.rejectedWith(Support.Sequelize.OptimisticLockError);
     });
 
     it('increment() also increments the version', function() {
-      return Account.create({number: 1}).then(function(account) {
+      return Account.create({number: 1}).then(account => {
         expect(account.version).to.eq(0);
         return account.increment('number', { by: 1} );
-      }).then(function(account) {
+      }).then(account => {
         return account.reload();
-      }).then(function(account) {
+      }).then(account => {
         expect(account.version).to.eq(1);
       });
     });
