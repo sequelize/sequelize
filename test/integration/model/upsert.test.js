e8f83a2b17fefcf0e17f2f249ff221bececea352
daleoooo <daleoooo.z@gmail.com>
2016-12-08 15:52:28 +0800

    Fixed: 6310

--- test/integration/model/upsert.test.js
+++ test/integration/model/upsert.test.js
@@ -331,6 +331,28 @@ describe(Support.getTestDialectTeaser('Model'), function() {
         });
       });
 
+      it('does not update when setting current values', function() {
+        return this.User.create({ id: 42, username: 'john'  }).bind(this).then(function() {
+          return this.User.findById(42);
+
+        }).then(function(user) {
+          return this.User.upsert({ id: user.id, username: user.username  });
+
+        }).then(function(created) {
+          if (dialect === 'sqlite') {
+            expect(created).to.be.undefined;
+
+          } else {
+            // After set node-mysql flags = '-FOUND_ROWS' in connection of mysql,
+            // result from upsert should be false when upsert a row to its current value
+            // https://dev.mysql.com/doc/refman/5.7/en/insert-on-duplicate.html
+            expect(created).to.equal(false);
+          }
+
+        });
+
+      });
+
       it('Works when two separate uniqueKeys are passed', function() {
         var User = this.sequelize.define('User', {
           username: {
@@ -352,18 +374,18 @@ describe(Support.getTestDialectTeaser('Model'), function() {
               if (dialect === 'sqlite') {
                 expect(created).to.be.undefined;
               } else {
-                expect(created).to.be.ok;              
+                expect(created).to.be.ok;
               }
-              clock.tick(1000);            
+              clock.tick(1000);
               return User.upsert({ username: 'user1', email: 'user1@domain.ext', city: 'New City' });
             }).then(function(created) {
               if (dialect === 'sqlite') {
                 expect(created).to.be.undefined;
               } else {
-                expect(created).not.to.be.ok;              
+                expect(created).not.to.be.ok;
               }
               clock.tick(1000);
-              return User.findOne({ where: { username: 'user1', email: 'user1@domain.ext' }});              
+              return User.findOne({ where: { username: 'user1', email: 'user1@domain.ext' }});
             })
             .then(function(user) {
               expect(user.createdAt).to.be.ok;
