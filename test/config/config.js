3e4116ec8e49821df00040f6982b7eed628a6df0
Harshith Kashyap <harshithkashyap@hotmail.com>
2016-12-18 00:14:48 +0530

    Added sequelize_test as default db in test/config/config for MSSQL dialect

--- test/config/config.js
+++ test/config/config.js
@@ -23,11 +23,7 @@ module.exports = {
   },
 
   mssql: mssqlConfig || {
-    database: process.env.SEQ_MSSQL_DB   || process.env.SEQ_DB   || (function () {
-      var db = 'sequelize-test-' + ~~(Math.random() * 100);
-      console.log('Using database: ', db);
-      return db;
-    }()),
+    database: process.env.SEQ_MSSQL_DB   || process.env.SEQ_DB   || 'sequelize_test',
     username: process.env.SEQ_MSSQL_USER || process.env.SEQ_USER || 'sequelize',
     password: process.env.SEQ_MSSQL_PW   || process.env.SEQ_PW   || 'nEGkLma26gXVHFUAHJxcmsrK',
     host:     process.env.SEQ_MSSQL_HOST || process.env.SEQ_HOST || 'mssql.sequelizejs.com',
