c8a56f9d3d817c1d364f44e95cb607f0eb7cc2d3
Nick Drane <nickdrane@Nicks-MacBook-Pro.local>
2016-12-24 14:31:47 -0600

    eslint fixes

--- test/unit/associations/association.test.js
+++ test/unit/associations/association.test.js
@@ -11,7 +11,7 @@ describe(Support.getTestDialectTeaser('belongsTo'), function() {
   it('should throw an AssociationError when two associations have the same alias', function() {
     const User = current.define('User');
     const Task = current.define('Task');
-    
+
     User.belongsTo(Task, { as: 'task' });
     const errorFunction = User.belongsTo.bind(User, Task, { as: 'task' });
     const errorMessage = `You have used the alias task in two separate associations. Aliased associations must have unique aliases.`;
