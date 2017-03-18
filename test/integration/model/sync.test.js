eaf3b7ccf7bb5acea34a1be215001d8dd2bb9895
Julian Meyer <julianmeyer2000@gmail.com>
2017-03-18 15:12:17 -0700

    Changed all vars to lets and replaced anonymous functions with arrow functions

--- test/integration/model/sync.test.js
+++ test/integration/model/sync.test.js
@@ -7,9 +7,9 @@ const chai = require('chai')
   , expect = chai.expect
   , Support = require(__dirname + '/../support');
 
-describe(Support.getTestDialectTeaser('Model'), function() {
-  describe('sync', function () {
-    beforeEach(function() {
+describe(Support.getTestDialectTeaser('Model'), () => {
+  describe('sync', () => {
+    beforeEach(() => {
       this.testSync = this.sequelize.define('testSync', {
         dummy: Sequelize.STRING
       });
@@ -17,61 +17,61 @@ describe(Support.getTestDialectTeaser('Model'), function() {
       return this.testSync.drop();
     });
 
-    it('should remove a column if it exists in the databases schema but not the model', function() {
+    it('should remove a column if it exists in the databases schema but not the model', () => {
       let User = this.sequelize.define('testSync', {
         name: Sequelize.STRING,
         age: Sequelize.INTEGER
       });
       return this.sequelize.sync()
         .bind(this)
-        .then(function() {
+        .then(() => {
           this.sequelize.define('testSync', {
             name: Sequelize.STRING
           });
         })
         .then(() => this.sequelize.sync({alter: true}))
-        .then(function() {
+        .then(() => {
           return User.describe().then((data) => {
             expect(data).to.not.ownProperty('age');
           });
         });
     });
 
-    it('should add a column if it exists in the model but not the database', function() {
+    it('should add a column if it exists in the model but not the database', () => {
       let testSync = this.sequelize.define('testSync', {
         name: Sequelize.STRING
       });
       return this.sequelize.sync()
         .bind(this)
-        .then(function() {
+        .then(() => {
           this.sequelize.define('testSync', {
             name: Sequelize.STRING,
             age: Sequelize.INTEGER
           });
         })
         .then(() => this.sequelize.sync({alter: true}))
-        .then(function() {
+        .then(() => {
           return testSync.describe().then((data) => {
             expect(data).to.ownProperty('age');
           });
         });
     });
 
-    it('should change a column if it exists in the model but is different in the database', function() {
+    it('should change a column if it exists in the model but is different in the database', () => {
       let testSync = this.sequelize.define('testSync', {
         name: Sequelize.STRING,
         age: Sequelize.INTEGER
       });
       return this.sequelize.sync()
         .bind(this)
-        .then(function() {
+        .then(() => {
           this.sequelize.define('testSync', {
             name: Sequelize.STRING,
             age: Sequelize.STRING
           });
         })
         .then(() => this.sequelize.sync({alter: true}))
-        .then(function() {
+        .then(() => {
           return testSync.describe().then((data) => {
             expect(data).to.ownProperty('age');
             expect(data.age.type).to.have.string('CHAR'); // CHARACTER VARYING, VARCHAR(n)
@@ -79,7 +79,7 @@ describe(Support.getTestDialectTeaser('Model'), function() {
         });
     });
 
-    it('should not modify data if the data type does not change', function() {
+    it('should not modify data if the data type does not change', () => {
       let testSync = this.sequelize.define('testSync', {
         name: Sequelize.STRING,
         age: Sequelize.STRING
