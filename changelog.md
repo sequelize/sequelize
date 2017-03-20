b31b662870c30338a266c47f9f8b2057b4c1e10a
Julian Meyer <julianmeyer2000@gmail.com>
2017-03-20 09:39:16 -0700

    Added feature to alter tables on sync. Fixes #537 (#7230)
    * Added feature to alter tables on sync. Fixes #537
    
    * Added an entry to future in changelog
    
    * Added integration test for syncing with alter
    
    * Fixed tests and model with sqlite and psql
    
    * Updated docs and updated some code style
    
    * Updated test.sync.js to use let instead of var
    
    * Changed all vars to lets and replaced anonymous functions with arrow functions
    
    * Added back some anonymous functions to use scope and fixed more coding style
    
    * Updated code style again - removed binds and nested then's
    
    * Updated changelog; renamed and improved tests; Added option to comment
    
    * Changed debug vars to consts
    
    * Switched changes to const
    
    * Added some missing semi-colons

--- changelog.md
+++ changelog.md
@@ -1,4 +1,5 @@
 # Future
+- [ADDED] `options.alter` to sequelize.sync() to alter existing tables.[#537](https://github.com/sequelize/sequelize/issues/537)
 - [ADDED] Ability to run transactions on a read-replica by marking transactions as read only [#7323](https://github.com/sequelize/sequelize/issues/7323)
 - [FIXED] Show a reasonable message when using renameColumn with a missing column  [#6606](https://github.com/sequelize/sequelize/issues/6606)
 - [PERFORMANCE] more efficient array handing for certain large queries [#7175](https://github.com/sequelize/sequelize/pull/7175)
---
+++ test/integration/model/sync.test.js
@@ -0,0 +1,85 @@
+'use strict';
+
+/* jshint -W030 */
+/* jshint -W110 */
+const chai = require('chai')
+  , Sequelize = require('../../../index')
+  , expect = chai.expect
+  , Support = require(__dirname + '/../support');
+
+describe(Support.getTestDialectTeaser('Model'), function() {
+  describe('sync', function() {
+    beforeEach(function() {
+      this.testSync = this.sequelize.define('testSync', {
+        dummy: Sequelize.STRING
+      });
+      return this.testSync.drop();
+    });
+
+    it('should remove a column if it exists in the databases schema but not the model', function() {
+      const User = this.sequelize.define('testSync', {
+        name: Sequelize.STRING,
+        age: Sequelize.INTEGER
+      });
+      return this.sequelize.sync()
+        .then(() => {
+          this.sequelize.define('testSync', {
+            name: Sequelize.STRING
+          });
+        })
+        .then(() => this.sequelize.sync({alter: true}))
+        .then(() => User.describe())
+        .then(data => {
+          expect(data).to.not.have.ownProperty('age');
+          expect(data).to.have.ownProperty('name');
+        });
+    });
+
+    it('should add a column if it exists in the model but not the database', function() {
+      const testSync = this.sequelize.define('testSync', {
+        name: Sequelize.STRING
+      });
+      return this.sequelize.sync()
+        .then(() => this.sequelize.define('testSync', {
+            name: Sequelize.STRING,
+            age: Sequelize.INTEGER
+          }))
+        .then(() => this.sequelize.sync({alter: true}))
+        .then(() => testSync.describe())
+        .then(data => expect(data).to.have.ownProperty('age'));
+    });
+
+    it('should change a column if it exists in the model but is different in the database', function() {
+      const testSync = this.sequelize.define('testSync', {
+        name: Sequelize.STRING,
+        age: Sequelize.INTEGER
+      });
+      return this.sequelize.sync()
+        .then(() => this.sequelize.define('testSync', {
+            name: Sequelize.STRING,
+            age: Sequelize.STRING
+          }))
+        .then(() => this.sequelize.sync({alter: true}))
+        .then(() => testSync.describe())
+        .then(data => {
+          expect(data).to.have.ownProperty('age');
+          expect(data.age.type).to.have.string('CHAR'); // CHARACTER VARYING, VARCHAR(n)
+        });
+    });
+
+    it('should not alter table if data type does not change', function() {
+      const testSync = this.sequelize.define('testSync', {
+        name: Sequelize.STRING,
+        age: Sequelize.STRING
+      });
+      return this.sequelize.sync()
+        .then(() => testSync.create({name: 'test', age: '1'}))
+        .then(() => this.sequelize.sync({alter: true}))
+        .then(() => testSync.findOne())
+        .then(data => {
+          expect(data.dataValues.name).to.eql('test');
+          expect(data.dataValues.age).to.eql('1');
+        });
+    });
+  });
+});
