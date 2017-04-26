82b6c505c569d788354be54d1c4ea8e522855329
Louis-Rémi Babé <lrbabe@gmail.com>
2017-04-26 21:22:55 +0200

    Fix upsert behavior to update all _changed_ fields by default (#7523)
    * Fix upsert behavior to update all _changed_ fields by default
    
    * Update changelog

--- test/unit/model/upsert.test.js
+++ test/unit/model/upsert.test.js
@@ -12,8 +12,19 @@ describe(Support.getTestDialectTeaser('Model'), () => {
 
   if (current.dialect.supports.upserts) {
     describe('method upsert', () => {
+      const self = this;
       const User = current.define('User', {
         name: DataTypes.STRING,
+        virtualValue: {
+          type: DataTypes.VIRTUAL,
+          set(val) {
+            return this.value = val;
+          },
+          get() {
+            return this.value;
+          }
+        },
+        value: DataTypes.STRING,
         secretValue: {
           type: DataTypes.INTEGER,
           allowNull: false
@@ -23,16 +34,39 @@ describe(Support.getTestDialectTeaser('Model'), () => {
       before(function() {
         this.query = current.query;
         current.query = sinon.stub().returns(Promise.resolve());
+
+        self.stub = sinon.stub(current.getQueryInterface(), 'upsert', () => {
+          return User.build({});
+        });
+      });
+
+      beforeEach(() => {
+        self.stub.reset();
       });
 
       after(function() {
         current.query = this.query;
+        self.stub.restore();
       });
+
 
       it('skip validations for missing fields', () => {
         return expect(User.upsert({
           name: 'Grumpy Cat'
         })).not.to.be.rejectedWith(current.ValidationError);
+      });
+
+      it('updates all changed fields by default', () => {
+        return User
+          .upsert({
+            name: 'Old Cat',
+            virtualValue: 111
+          })
+          .then(() => {
+            expect(Object.keys(self.stub.getCall(0).args[2])).to.deep.equal([
+              'name', 'value', 'updatedAt'
+            ]);
+          });
       });
     });
   }
