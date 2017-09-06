6826f47d2e7b087768fc33b284f21569c415e173
Josh <josh@phin.co>
2017-09-06 09:04:23 -0700

    feat(statement_timeout): oops. left an only in.

--- test/integration/dialects/postgres/connection-manager.test.js
+++ test/integration/dialects/postgres/connection-manager.test.js
@@ -8,7 +8,7 @@ const chai = require('chai'),
   _ = require('lodash');
 
 if ( dialect.match(/^postgres/) ) {
-  describe.only('[POSTGRES] Sequelize', () => {
+  describe('[POSTGRES] Sequelize', () => {
     function checkTimezoneParsing(baseOptions) {
       const options = _.extend({}, baseOptions, { timezone: 'Asia/Kolkata', timestamps: true });
       const sequelize = Support.createSequelizeInstance(options);
