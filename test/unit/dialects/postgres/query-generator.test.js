bdd971d0d55677ea50be507d98ccaa3bfe77f592
Ilia Ermolin <dotpack@gmail.com>
2017-09-08 09:42:48 +0300

    style(queryGenerator/arithmeticQuery): remove too verbose code

--- test/unit/dialects/postgres/query-generator.test.js
+++ test/unit/dialects/postgres/query-generator.test.js
@@ -17,17 +17,17 @@ if (dialect.match(/^postgres/)) {
         {
           title:'Should use the plus operator',
           arguments: ['+', 'myTable', { foo: 'bar' }, {}, {}],
-          expectation: 'UPDATE "myTable" SET "foo"="foo"+\'bar\'  RETURNING *'
+          expectation: 'UPDATE "myTable" SET "foo"="foo"+ \'bar\'  RETURNING *'
         },
         {
           title:'Should use the plus operator with where clause',
           arguments: ['+', 'myTable', { foo: 'bar' }, { bar: 'biz'}, {}],
-          expectation: 'UPDATE "myTable" SET "foo"="foo"+\'bar\' WHERE "bar" = \'biz\' RETURNING *'
+          expectation: 'UPDATE "myTable" SET "foo"="foo"+ \'bar\' WHERE "bar" = \'biz\' RETURNING *'
         },
         {
           title:'Should use the minus operator',
           arguments: ['-', 'myTable', { foo: 'bar' }, {}, {}],
-          expectation: 'UPDATE "myTable" SET "foo"="foo"-\'bar\'  RETURNING *'
+          expectation: 'UPDATE "myTable" SET "foo"="foo"- \'bar\'  RETURNING *'
         },
         {
           title:'Should use the minus operator with negative value',
@@ -37,7 +37,7 @@ if (dialect.match(/^postgres/)) {
         {
           title:'Should use the minus operator with where clause',
           arguments: ['-', 'myTable', { foo: 'bar' }, { bar: 'biz'}, {}],
-          expectation: 'UPDATE "myTable" SET "foo"="foo"-\'bar\' WHERE "bar" = \'biz\' RETURNING *'
+          expectation: 'UPDATE "myTable" SET "foo"="foo"- \'bar\' WHERE "bar" = \'biz\' RETURNING *'
         }
       ],
       attributesToSQL: [
