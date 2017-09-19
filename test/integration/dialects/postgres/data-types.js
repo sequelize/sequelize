2064c76c7fadbcd0d1b45e75bf61ff7c8e903ae6
mkaufmaner <mkaufmaner@gmail.com>
2017-09-19 18:41:28 -0400

    fix(DataTypes/DATE): Adds support for DATE with Infinity/-Infinity

---
+++ test/integration/dialects/postgres/data-types.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const Support = require(__dirname + '/../../support');
+const dialect = Support.getTestDialect();
+const DataTypes = require(__dirname + '/../../../../lib/data-types')[dialect];
+
+if (dialect.match(/^postgres/)) {
+  describe('[POSTGRES Specific] Data Types', () => {
+    describe('DATE', () => {
+      // quick test of DATE methods
+      it('should validate Infinity/-Infinity as true', () => {
+        expect(DataTypes.DATE().validate(Infinity)).to.equal(true);
+        expect(DataTypes.DATE().validate(-Infinity)).to.equal(true);
+      });
+
+      it('should stringify Infinity/-Infinity to infinity/-infinity', () => {
+        expect(DataTypes.DATE().stringify(Infinity)).to.equal('infinity');
+        expect(DataTypes.DATE().stringify(-Infinity)).to.equal('-infinity');
+      });
+
+      // create dummy user
+      it('should be able to create and update records with Infinity/-Infinity', function() {
+        const User = this.sequelize.define('User', {
+          username: DataTypes.STRING,
+          beforeTime: {
+            type: DataTypes.DATE,
+            defaultValue: -Infinity
+          },
+          sometime: {
+            type: DataTypes.DATE,
+            defaultValue: this.sequelize.fn('NOW')
+          },
+          afterTime: {
+            type: DataTypes.DATE,
+            defaultValue: Infinity
+          }
+        });
+
+        return User.sync({
+          force: true
+        }).then(() => {
+          return User.create({
+            username: 'bob'
+          });
+        }).then(user => {
+          expect(user.username).to.equal('bob');
+          expect(user.beforeTime).to.equal(-Infinity);
+          expect(user.afterTime).to.equal(Infinity);
+
+          return user.update({
+            sometime: Infinity
+          });
+        }).then(user => {
+          expect(user.sometime).to.equal(Infinity);
+
+          return user.update({
+            sometime: this.sequelize.fn('NOW')
+          });
+        }).then(user => {
+          expect(user.sometime).to.not.equal(Infinity);
+        });
+      });
+
+    });
+  });
+}
