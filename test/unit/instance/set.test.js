256de8cbd1e050a11e29e5b63c302a20824d1d52
knibb800 <32358232+knibb800@users.noreply.github.com>
2017-09-28 15:24:23 +0200

    fix(model): do not mark a column as changed if set to the same value with customer setter
    If a column with a custom setter is set, use the new column value to determine if the column has really changed

--- test/unit/instance/set.test.js
+++ test/unit/instance/set.test.js
@@ -4,7 +4,9 @@ const chai = require('chai'),
   expect = chai.expect,
   Support   = require(__dirname + '/../support'),
   DataTypes = require(__dirname + '/../../../lib/data-types'),
-  current   = Support.sequelize;
+  current   = Support.sequelize,
+  Promise = current.Promise,
+  sinon = require('sinon');
 
 describe(Support.getTestDialectTeaser('Instance'), () => {
   describe('set', () => {
@@ -73,6 +75,54 @@ describe(Support.getTestDialectTeaser('Instance'), () => {
       user.set('date', new Date());
       expect(user.get('date')).to.be.an.instanceof(Date);
       expect(user.get('date')).not.to.be.NaN;
+    });
+
+    describe('custom setter', () => {
+      before(function() {
+        this.stubCreate = sinon.stub(current.getQueryInterface(), 'insert', (instance) => {
+          return Promise.resolve([ instance, 1]);
+        });
+      });
+
+      after(function() {
+        this.stubCreate.restore();
+      });
+
+      const User = current.define('User', {
+        phoneNumber: {
+          type: DataTypes.STRING,
+          set (val) {
+            if (typeof value === 'string') {
+              val = val.replace(/^\+/, '00').replace(/\(0\)|[\s+\/.\-\(\)]/g, '');
+            }
+            this.setDataValue('phoneNumber', val.split(' ').reverse().join(' '));
+          }
+        }
+      });
+
+      it('does not set field to changed if field is set to the same value with custom setter', () => {
+        const user = User.build({
+          phoneNumber: 'hello world'
+        });
+        return user.save().then(() => {
+          expect(user.changed('phoneNumber')).to.be.false;
+
+          user.set('phoneNumber', 'hello world');
+          expect(user.changed('phoneNumber')).to.be.false;
+        });
+      });
+
+      it('sets field to changed if field is set to the another value with custom setter', () => {
+        const user = User.build({
+          phoneNumber: 'hello world'
+        });
+        return user.save().then(() => {
+          expect(user.changed('phoneNumber')).to.be.false;
+
+          user.set('phoneNumber', 'goodbye world');
+          expect(user.changed('phoneNumber')).to.be.true;
+        });
+      });
     });
   });
 });
