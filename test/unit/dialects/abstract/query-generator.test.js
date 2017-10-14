00024c51b7cd782b68c0724ebf6555b65b1e6954
Sushant <sushantdhiman@outlook.com>
2017-10-14 13:20:54 +0530

    fix: show inspected value for incorrect where query

--- test/unit/dialects/abstract/query-generator.test.js
+++ test/unit/dialects/abstract/query-generator.test.js
@@ -19,25 +19,25 @@ describe('QueryGenerator', () => {
     it('should not parse any strings as aliases  operators', function() {
       const QG = getAbstractQueryGenerator(this.sequelize);
       expect(() => QG.whereItemQuery('$or', [{test: 5}, {test: 3}]))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { test: 5 }');
 
       expect(() => QG.whereItemQuery('$and', [{test: 5}, {test: 3}]))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { test: 5 }');
 
       expect(() => QG.whereItemQuery('test', {$gt: 5}))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$gt\': 5 }');
 
       expect(() => QG.whereItemQuery('test', {$between: [2, 5]}))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$between\': [ 2, 5 ] }');
 
       expect(() => QG.whereItemQuery('test', {$ne: 3}))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$ne\': 3 }');
 
       expect(() => QG.whereItemQuery('test', {$not: 3}))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$not\': 3 }');
 
       expect(() => QG.whereItemQuery('test', {$in: [4]}))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$in\': [ 4 ] }');
     });
 
     it('should parse set aliases strings as operators', function() {
@@ -57,31 +57,31 @@ describe('QueryGenerator', () => {
         .should.be.equal('(test BETWEEN 2 AND 5 AND test != 3 AND test > 4)');
 
       expect(() => QG.whereItemQuery('OR', [{test: {'^^': 5}}, {test: {$not: 3}}, {test: {[Op.in]: [4]}}]))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$not\': 3 }');
 
       expect(() => QG.whereItemQuery('OR', [{test: {$gt: 5}}, {test: {'!': 3}}, {test: {[Op.in]: [4]}}]))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$gt\': 5 }');
 
       expect(() => QG.whereItemQuery('$or', [{test: 5}, {test: 3}]))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { test: 5 }');
 
       expect(() => QG.whereItemQuery('$and', [{test: 5}, {test: 3}]))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { test: 5 }');
 
       expect(() => QG.whereItemQuery('test', {$gt: 5}))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$gt\': 5 }');
 
       expect(() => QG.whereItemQuery('test', {$between: [2, 5]}))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$between\': [ 2, 5 ] }');
 
       expect(() => QG.whereItemQuery('test', {$ne: 3}))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$ne\': 3 }');
 
       expect(() => QG.whereItemQuery('test', {$not: 3}))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$not\': 3 }');
 
       expect(() => QG.whereItemQuery('test', {$in: [4]}))
-        .to.throw('Invalid value [object Object]');
+        .to.throw('Invalid value { \'$in\': [ 4 ] }');
     });
 
   });
