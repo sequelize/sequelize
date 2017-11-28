05f9284272d8786fef55a90cb3eca25c45103985
Sushant <sushantdhiman@outlook.com>
2017-11-28 17:28:39 +0530

    fix(unit): dont use .returns sugar for rejection

--- test/unit/model/find-create-find.test.js
+++ test/unit/model/find-create-find.test.js
@@ -53,7 +53,9 @@ describe(Support.getTestDialectTeaser('Model'), function() {
         , where = {prop: Math.random().toString()}
         , findSpy = this.sinon.stub(Model, 'findOne');
 
-      this.sinon.stub(Model, 'create').returns(Promise.reject(new UniqueConstraintError()));
+      this.sinon.stub(Model, 'create', function() {
+        return Promise.reject(new UniqueConstraintError());
+      });
 
       findSpy.onFirstCall().returns(Promise.resolve(null));
       findSpy.onSecondCall().returns(Promise.resolve(result));
