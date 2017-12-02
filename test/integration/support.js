9535a583c308ba746717cb612e847bc0a1dcd8d6
Sushant <sushantdhiman@outlook.com>
2017-12-02 23:38:21 +0530

    new image

--- test/integration/support.js
+++ test/integration/support.js
@@ -1,17 +1,6 @@
 'use strict';
 
 const Support = require('../support');
-const dialect = Support.getTestDialect();
-
-before(() => {
-  if (dialect !== 'postgres' && dialect !== 'postgres-native') {
-    return;
-  }
-  return Support.sequelize.Promise.all([
-    Support.sequelize.query('CREATE EXTENSION IF NOT EXISTS hstore', {raw: true}),
-    Support.sequelize.query('CREATE EXTENSION IF NOT EXISTS btree_gist', {raw: true})
-  ]);
-});
 
 beforeEach(function() {
   this.sequelize.test.trackRunningQueries();
