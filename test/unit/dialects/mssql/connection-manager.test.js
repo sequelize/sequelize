ecce0ead0ffae7c01a39c5450fa32ce5045f6ecc
Ratanak Lun <ratanakvlun@gmail.com>
2017-12-15 02:40:08 -0500

    test(mssql): wrap unit tests in dialect conditional

--- test/unit/dialects/mssql/connection-manager.test.js
+++ test/unit/dialects/mssql/connection-manager.test.js
@@ -9,36 +9,36 @@ const chai = require('chai'),
   sinon = require('sinon'),
   connectionStub = sinon.stub(tedious, 'Connection');
 
-if (dialect !== 'mssql') { return; }
-
 connectionStub.returns({on() {}});
 
-describe('[MSSQL Specific] Connection Manager', () => {
-  let instance,
-    config;
-  beforeEach(() => {
-    config = {
-      dialect: 'mssql',
-      database: 'none',
-      username: 'none',
-      password: 'none',
-      host: 'localhost',
-      port: 2433,
-      pool: {},
-      dialectOptions: {
-        domain: 'TEST.COM'
-      }
-    };
-    instance = new Sequelize(config.database
-      , config.username
-      , config.password
-      , config);
-  });
-
-  it('connectionManager._connect() Does not delete `domain` from config.dialectOptions',
-    () => {
-      expect(config.dialectOptions.domain).to.equal('TEST.COM');
-      instance.dialect.connectionManager._connect(config);
-      expect(config.dialectOptions.domain).to.equal('TEST.COM');
+if (dialect === 'mssql') {
+  describe('[MSSQL Specific] Connection Manager', () => {
+    let instance,
+      config;
+    beforeEach(() => {
+      config = {
+        dialect: 'mssql',
+        database: 'none',
+        username: 'none',
+        password: 'none',
+        host: 'localhost',
+        port: 2433,
+        pool: {},
+        dialectOptions: {
+          domain: 'TEST.COM'
+        }
+      };
+      instance = new Sequelize(config.database
+        , config.username
+        , config.password
+        , config);
     });
-});
+
+    it('connectionManager._connect() Does not delete `domain` from config.dialectOptions',
+      () => {
+        expect(config.dialectOptions.domain).to.equal('TEST.COM');
+        instance.dialect.connectionManager._connect(config);
+        expect(config.dialectOptions.domain).to.equal('TEST.COM');
+      });
+  });
+}
