3657f32e40d004f30adf8c6f826111a39e96d6ef
Ratanak Lun <ratanakvlun@gmail.com>
2017-12-15 02:56:27 -0500

    fix(mssql): improper parameter binding in sql generation (#8782)

---
+++ test/unit/dialects/mssql/connection-manager.test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const chai = require('chai'),
+  expect = chai.expect,
+  Sequelize = require(__dirname + '/../../../../index'),
+  Support = require(__dirname + '/../../support'),
+  dialect = Support.getTestDialect(),
+  tedious = require('tedious'),
+  sinon = require('sinon'),
+  connectionStub = sinon.stub(tedious, 'Connection');
+
+connectionStub.returns({on() {}});
+
+if (dialect === 'mssql') {
+  describe('[MSSQL Specific] Connection Manager', () => {
+    let instance,
+      config;
+    beforeEach(() => {
+      config = {
+        dialect: 'mssql',
+        database: 'none',
+        username: 'none',
+        password: 'none',
+        host: 'localhost',
+        port: 2433,
+        pool: {},
+        dialectOptions: {
+          domain: 'TEST.COM'
+        }
+      };
+      instance = new Sequelize(config.database
+        , config.username
+        , config.password
+        , config);
+    });
+
+    it('connectionManager._connect() Does not delete `domain` from config.dialectOptions',
+      () => {
+        expect(config.dialectOptions.domain).to.equal('TEST.COM');
+        instance.dialect.connectionManager._connect(config);
+        expect(config.dialectOptions.domain).to.equal('TEST.COM');
+      });
+  });
+}
