6abca829882d4e397078b2af5a63cb648c9954d4
aceew <a@aceew.me>
2018-01-10 14:15:14 +0000

    fix(model/upsert): Made the validation flag work for upserts
    Added:
     - Check on options.validate before choosing to validate the instance in: lib/model.js

--- test/integration/model/upsert.test.js
+++ test/integration/model/upsert.test.js
@@ -193,6 +193,26 @@ describe(Support.getTestDialectTeaser('Model'), () => {
         return expect(User.upsert({ email: 'notanemail' })).to.eventually.be.rejectedWith(this.sequelize.ValidationError);
       });
 
+      it('supports skipping validations', function() {
+        const User = this.sequelize.define('user', {
+          email: {
+            type: Sequelize.STRING,
+            validate: {
+              isEmail: true
+            }
+          }
+        });
+
+        const options = { validate: false };
+        const email = 'notanemail';
+
+        return User.sync({ force: true })
+          .then(() => User.upsert({ email }, options))
+          .then(created => {
+            expect(created).to.be.true;
+          });
+      });
+
       it('works with BLOBs', function() {
         return this.User.upsert({ id: 42, username: 'john', blob: new Buffer('kaj') }).bind(this).then(function(created) {
           if (dialect === 'sqlite') {
