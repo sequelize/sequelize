819dfa2fc9d5efbcd2eebe7869ec50c06e20b9f3
Jozef Hartinger <jharting@redhat.com>
2018-03-28 20:37:26 +0200

    fix(model): don't add LIMIT in findOne() queries on unique key

--- test/unit/model/findone.test.js
+++ test/unit/model/findone.test.js
@@ -67,5 +67,49 @@ describe(Support.getTestDialectTeaser('Model'), () => {
       });
     });
 
+    describe('should not add limit when querying on an unique key', () => {
+      it('with custom unique key', function() {
+        const Model = current.define('model', {
+          unique: {
+            type: DataTypes.INTEGER,
+            unique: true
+          }
+        });
+
+        return Model.findOne({ where: { unique: 42 }}).bind(this).then(function() {
+          expect(this.stub.getCall(0).args[0]).to.be.an('object').not.to.have.property('limit');
+        });
+      });
+
+      it('with blob unique key', function() {
+        const Model = current.define('model', {
+          unique: {
+            type: DataTypes.BLOB,
+            unique: true
+          }
+        });
+
+        return Model.findOne({ where: { unique: new Buffer('foo') }}).bind(this).then(function() {
+          expect(this.stub.getCall(0).args[0]).to.be.an('object').not.to.have.property('limit');
+        });
+      });
+    });
+
+    it('should add limit when using multi-column unique key', function() {
+      const Model = current.define('model', {
+        unique1: {
+          type: DataTypes.INTEGER,
+          unique: 'unique'
+        },
+        unique2: {
+          type: DataTypes.INTEGER,
+          unique: 'unique'
+        }
+      });
+
+      return Model.findOne({ where: { unique1: 42}}).bind(this).then(function() {
+        expect(this.stub.getCall(0).args[0]).to.be.an('object').to.have.property('limit');
+      });
+    });
   });
 });
