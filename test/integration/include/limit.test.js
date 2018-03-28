sequelize/sequelize

commit 20cac7a15d2044cac9de9f33ec9b13ab349c5567

fix(query-generator): generate subQuery filter for nested required joins (#9188)

lib/dialects/abstract/query-generator.js
@@ -1249,7 +1249,7 @@ const QueryGenerator = {
           ? this.quoteIdentifiers(attr)
           : this.escape(attr);
       }
-      if (options.include && attr.indexOf('.') === -1 && addTable) {
+      if (!_.isEmpty(options.include) && attr.indexOf('.') === -1 && addTable) {
         attr = mainTableAs + '.' + attr;
       }
 
@@ -1258,7 +1258,6 @@ const QueryGenerator = {
   },
 
   generateInclude(include, parentTableName, topLevelInfo) {
-    const association = include.association;
     const joinQueries = {
       mainQuery: [],
       subQuery: []
@@ -1334,43 +1333,7 @@ const QueryGenerator = {
     if (include.through) {
       joinQuery = this.generateThroughJoin(include, includeAs, parentTableName.internalAs, topLevelInfo);
     } else {
-      if (topLevelInfo.subQuery && include.subQueryFilter) {
-        const associationWhere = {};
-
-        associationWhere[association.identifierField] = {
-          [Op.eq]: this.sequelize.literal(`${this.quoteTable(parentTableName.internalAs)}.${this.quoteIdentifier(association.sourceKeyField || association.source.primaryKeyField)}`)
-        };
-
-        if (!topLevelInfo.options.where) {
-          topLevelInfo.options.where = {};
-        }
-
-        // Creating the as-is where for the subQuery, checks that the required association exists
-        const $query = this.selectQuery(include.model.getTableName(), {
-          attributes: [association.identifierField],
-          where: {
-            [Op.and]: [
-              associationWhere,
-              include.where || {}
-            ]
-          },
-          limit: 1,
-          tableAs: include.as
-        }, include.model);
-
-        const subQueryWhere = this.sequelize.asIs([
-          '(',
-          $query.replace(/\;$/, ''),
-          ')',
-          'IS NOT NULL'
-        ].join(' '));
-
-        if (_.isPlainObject(topLevelInfo.options.where)) {
-          topLevelInfo.options.where['__' + includeAs.internalAs] = subQueryWhere;
-        } else {
-          topLevelInfo.options.where = { [Op.and]: [topLevelInfo.options.where, subQueryWhere] };
-        }
-      }
+      this._generateSubQueryFilter(include, includeAs, topLevelInfo);
       joinQuery = this.generateJoin(include, topLevelInfo);
     }
 
@@ -1609,70 +1572,9 @@ const QueryGenerator = {
           joinCondition += ` AND ${targetWhere}`;
         }
       }
-      if (topLevelInfo.subQuery && include.required) {
-        if (!topLevelInfo.options.where) {
-          topLevelInfo.options.where = {};
-        }
-        let parent = include;
-        let child = include;
-        let nestedIncludes = [];
-        let query;
-
-        while ((parent = parent.parent)) { // eslint-disable-line
-          nestedIncludes = [_.extend({}, child, { include: nestedIncludes })];
-          child = parent;
-        }
-
-        const topInclude = nestedIncludes[0];
-        const topParent = topInclude.parent;
-
-        if (topInclude.through && Object(topInclude.through.model) === topInclude.through.model) {
-          query = this.selectQuery(topInclude.through.model.getTableName(), {
-            attributes: [topInclude.through.model.primaryKeyField],
-            include: Model._validateIncludedElements({
-              model: topInclude.through.model,
-              include: [{
-                association: topInclude.association.toTarget,
-                required: true
-              }]
-            }).include,
-            model: topInclude.through.model,
-            where: {
-              [Op.and]: [
-                this.sequelize.asIs([
-                  this.quoteTable(topParent.model.name) + '.' + this.quoteIdentifier(topParent.model.primaryKeyField),
-                  this.quoteIdentifier(topInclude.through.model.name) + '.' + this.quoteIdentifier(topInclude.association.identifierField)
-                ].join(' = ')),
-                topInclude.through.where
-              ]
-            },
-            limit: 1,
-            includeIgnoreAttributes: false
-          }, topInclude.through.model);
-        } else {
-          const isBelongsTo = topInclude.association.associationType === 'BelongsTo';
-          const join = [
-            this.quoteTable(topParent.model.name) + '.' + this.quoteIdentifier(isBelongsTo ? topInclude.association.identifierField : topParent.model.primaryKeyAttributes[0]),
-            this.quoteIdentifier(topInclude.model.name) + '.' + this.quoteIdentifier(isBelongsTo ? topInclude.model.primaryKeyAttributes[0] : topInclude.association.identifierField)
-          ].join(' = ');
-          query = this.selectQuery(topInclude.model.tableName, {
-            attributes: [topInclude.model.primaryKeyAttributes[0]],
-            include: topInclude.include,
-            where: {
-              [Op.join]: this.sequelize.asIs(join)
-            },
-            limit: 1,
-            includeIgnoreAttributes: false
-          }, topInclude.model);
-        }
-        topLevelInfo.options.where['__' + throughAs] = this.sequelize.asIs([
-          '(',
-          query.replace(/\;$/, ''),
-          ')',
-          'IS NOT NULL'
-        ].join(' '));
-      }
     }
+
+    this._generateSubQueryFilter(include, includeAs, topLevelInfo);
 
     return {
       join: joinType,
@@ -1680,6 +1582,123 @@ const QueryGenerator = {
       condition: joinCondition,
       attributes
     };
+  },
+
+  /*
+   * Generates subQueryFilter - a select nested in the where clause of the subQuery.
+   * For a given include a query is generated that contains all the way from the subQuery
+   * table to the include table plus everything that's in required transitive closure of the
+   * given include.
+   */
+  _generateSubQueryFilter(include, includeAs, topLevelInfo) {
+    if (!topLevelInfo.subQuery || !include.subQueryFilter) {
+      return;
+    }
+
+    if (!topLevelInfo.options.where) {
+      topLevelInfo.options.where = {};
+    }
+    let parent = include;
+    let child = include;
+    let nestedIncludes = this._getRequiredClosure(include).include;
+    let query;
+
+    while ((parent = parent.parent)) { // eslint-disable-line
+      if (parent.parent && !parent.required) {
+        return; // only generate subQueryFilter if all the parents of this include are required
+      }
+
+      if (parent.subQueryFilter) {
+        // the include is already handled as this parent has the include on its required closure
+        // skip to prevent duplicate subQueryFilter
+        return;
+      }
+
+      nestedIncludes = [_.extend({}, child, { include: nestedIncludes, attributes: [] })];
+      child = parent;
+    }
+
+    const topInclude = nestedIncludes[0];
+    const topParent = topInclude.parent;
+    const topAssociation = topInclude.association;
+    topInclude.association = undefined;
+
+    if (topInclude.through && Object(topInclude.through.model) === topInclude.through.model) {
+      query = this.selectQuery(topInclude.through.model.getTableName(), {
+        attributes: [topInclude.through.model.primaryKeyField],
+        include: Model._validateIncludedElements({
+          model: topInclude.through.model,
+          include: [{
+            association: topAssociation.toTarget,
+            required: true,
+            where: topInclude.where,
+            include: topInclude.include
+          }]
+        }).include,
+        model: topInclude.through.model,
+        where: {
+          [Op.and]: [
+            this.sequelize.asIs([
+              this.quoteTable(topParent.model.name) + '.' + this.quoteIdentifier(topParent.model.primaryKeyField),
+              this.quoteIdentifier(topInclude.through.model.name) + '.' + this.quoteIdentifier(topAssociation.identifierField)
+            ].join(' = ')),
+            topInclude.through.where
+          ]
+        },
+        limit: 1,
+        includeIgnoreAttributes: false
+      }, topInclude.through.model);
+    } else {
+      const isBelongsTo = topAssociation.associationType === 'BelongsTo';
+      const sourceField = isBelongsTo ? topAssociation.identifierField : (topAssociation.sourceKeyField || topParent.model.primaryKeyField);
+      const targetField = isBelongsTo ? (topAssociation.sourceKeyField || topInclude.model.primaryKeyField) : topAssociation.identifierField;
+
+      const join = [
+        this.quoteIdentifier(topInclude.as) + '.' + this.quoteIdentifier(targetField),
+        this.quoteTable(topParent.as || topParent.model.name) + '.' + this.quoteIdentifier(sourceField)
+      ].join(' = ');
+
+      query = this.selectQuery(topInclude.model.getTableName(), {
+        attributes: [targetField],
+        include: Model._validateIncludedElements(topInclude).include,
+        model: topInclude.model,
+        where: {
+          [Op.and]: [{
+            [Op.join]: this.sequelize.asIs(join)
+          }]
+        },
+        limit: 1,
+        tableAs: topInclude.as,
+        includeIgnoreAttributes: false
+      }, topInclude.model);
+    }
+
+    if (!topLevelInfo.options.where[Op.and]) {
+      topLevelInfo.options.where[Op.and] = [];
+    }
+
+    topLevelInfo.options.where[`__${includeAs.internalAs}`] = this.sequelize.asIs([
+      '(',
+      query.replace(/\;$/, ''),
+      ')',
+      'IS NOT NULL'
+    ].join(' '));
+  },
+
+  /*
+   * For a given include hierarchy creates a copy of it where only the required includes
+   * are preserved.
+   */
+  _getRequiredClosure(include) {
+    const copy = _.extend({}, include, {attributes: [], include: []});
+
+    if (Array.isArray(include.include)) {
+      copy.include = include.include
+        .filter(i => i.required)
+        .map(inc => this._getRequiredClosure(inc));
+    }
+
+    return copy;
   },
 
   getQueryOrders(options, model, subQuery) {

lib/model.js
@@ -390,6 +390,7 @@ class Model {
     options.include = options.include.map(include => {
       include = this._conformInclude(include);
       include.parent = options;
+      include.topLimit = options.topLimit;
 
       this._validateIncludedElement.call(options.model, include, tableNames, options);
 

commit 3f097a3b95ab3b926d1c389593f29a5b23d9f4cf

fix(query-generator): 1-to-many join in subQuery filter missing where clause (#9228)
fixing regression introduced in https://github.com/sequelize/sequelize/pull/9188

lib/dialects/abstract/query-generator.js
@@ -1773,9 +1773,10 @@ class QueryGenerator {
         include: Model._validateIncludedElements(topInclude).include,
         model: topInclude.model,
         where: {
-          [Op.and]: [{
-            [Op.join]: this.sequelize.asIs(join)
-          }]
+          [Op.and]: [
+            topInclude.where,
+            { [Op.join]: this.sequelize.asIs(join) }
+          ]
         },
         limit: 1,
         tableAs: topInclude.as,

test/integration/include/limit.test.js
@@ -31,6 +31,7 @@ describe(Support.getTestDialectTeaser('Include'), () => {
      *                            |
      *                            |
      *                            |
+     *                            N
      *                        [Footnote]
      */
     beforeEach(function () {
@@ -360,7 +361,7 @@ describe(Support.getTestDialectTeaser('Include'), () => {
     });
 
     /*
-     * one-to-one
+     * one-to-many
      */
     it('supports required one-to-many association', function () {
       return this.sequelize.sync({ force: true })
@@ -391,20 +392,23 @@ describe(Support.getTestDialectTeaser('Include'), () => {
       return this.sequelize.sync({ force: true })
         .then(() => Promise.join(
           this.Post.bulkCreate(build('alpha', 'bravo', 'charlie')),
-          this.Comment.bulkCreate(build('comment0', 'comment1'))
+          this.Comment.bulkCreate(build('comment0', 'comment1', 'comment2'))
         ))
         .spread((posts, comments) => Promise.join(
           posts[0].addComment(comments[0]),
-          posts[2].addComment(comments[1])
+          posts[1].addComment(comments[1]),
+          posts[2].addComment(comments[2])
         ))
         .then(() => this.Post.findAll({
           include: [{
             model: this.Comment,
             required: true,
             where: {
-              name: {
-                [this.sequelize.Op.like]: 'comment%'
-              }
+              [Op.or]: [{
+                name: 'comment0'
+              }, {
+                name: 'comment2'
+              }]
             }
           }],
           order: ['name'],
@@ -414,6 +418,31 @@ describe(Support.getTestDialectTeaser('Include'), () => {
         .then(result => {
           expect(result.length).to.equal(1);
           expect(result[0].name).to.equal('charlie');
+        });
+    });
+
+    it('supports required one-to-many association with where clause (findOne)', function () {
+      return this.sequelize.sync({ force: true })
+        .then(() => Promise.join(
+          this.Post.bulkCreate(build('alpha', 'bravo', 'charlie')),
+          this.Comment.bulkCreate(build('comment0', 'comment1', 'comment2'))
+        ))
+        .spread((posts, comments) => Promise.join(
+          posts[0].addComment(comments[0]),
+          posts[1].addComment(comments[1]),
+          posts[2].addComment(comments[2])
+        ))
+        .then(() => this.Post.findOne({
+          include: [{
+            model: this.Comment,
+            required: true,
+            where: {
+              name: 'comment2'
+            }
+          }]
+        }))
+        .then(post => {
+          expect(post.name).to.equal('charlie');
         });
     });
 