bb42b176baacca78322bbb2bbd921a1d037aa94b
ddolcimascolo <ddolcimascolo@linagora.com>
2018-04-17 20:46:38 +0200

    fix(destroy): Properties updated in a beforeDestroy hook are now persisted on soft delete.
    Closes #9318

--- test/integration/hooks/destroy.test.js
+++ test/integration/hooks/destroy.test.js
@@ -76,6 +76,48 @@ describe(Support.getTestDialectTeaser('Hooks'), () => {
         });
       });
     });
+
+    describe('with paranoid mode enabled', () => {
+      beforeEach(function() {
+        this.ParanoidUser = this.sequelize.define('ParanoidUser', {
+          username: DataTypes.STRING,
+          updatedBy: DataTypes.INTEGER,
+          virtualField: {
+            type: DataTypes.VIRTUAL(DataTypes.INTEGER, ['updatedBy']),
+            get() {
+              return this.updatedBy - 1;
+            }
+          }
+        }, {
+          paranoid: true,
+          hooks: {
+            beforeDestroy: instance => instance.updatedBy = 1
+          }
+        });
+      });
+
+      it('sets other changed values when soft deleting and a beforeDestroy hooks kicks in', function() {
+        return this.ParanoidUser.sync({ force: true })
+          .then(() => this.ParanoidUser.create({ username: 'user1' }))
+          .then(user => user.destroy())
+          .then(() => this.ParanoidUser.findOne({ paranoid: false }))
+          .then(user => {
+            expect(user.updatedBy).to.equal(1);
+          });
+      });
+
+      it('should not throw error when a beforeDestroy hook changes a virtual column', function() {
+        this.ParanoidUser.beforeDestroy(instance => instance.virtualField = 2);
+
+        return this.ParanoidUser.sync({ force: true })
+          .then(() => this.ParanoidUser.create({ username: 'user1' }))
+          .then(user => user.destroy())
+          .then(() => this.ParanoidUser.findOne({ paranoid: false }))
+          .then(user => {
+            expect(user.virtualField).to.equal(0);
+          });
+      });
+    });
   });
 
 });
