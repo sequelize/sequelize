5d0d6a4435594fb14c50076c5c89a0db24ef91e0
Dario <dario1985@users.noreply.github.com>
2018-08-16 17:31:34 +0200

    fix(count): duplicate mapping of fields break scopes (#9788)

--- test/integration/model/scope/count.test.js
+++ test/integration/model/scope/count.test.js
@@ -17,6 +17,10 @@ describe(Support.getTestDialectTeaser('Model'), () => {
         this.ScopeMe = this.sequelize.define('ScopeMe', {
           username: Sequelize.STRING,
           email: Sequelize.STRING,
+          aliasValue: {
+            field: 'alias_value',
+            type: Sequelize.INTEGER
+          },
           access_level: Sequelize.INTEGER,
           other_value: Sequelize.INTEGER
         }, {
@@ -66,7 +70,12 @@ describe(Support.getTestDialectTeaser('Model'), () => {
                   }
                 }]
               };
-            }
+            },
+            withAliasedField: {
+              where: {
+                aliasValue: { [Sequelize.Op.ne]: 1 }
+              }
+            },
           }
         });
         this.Child.belongsTo(this.ScopeMe);
@@ -74,10 +83,10 @@ describe(Support.getTestDialectTeaser('Model'), () => {
 
         return this.sequelize.sync({force: true}).then(() => {
           const records = [
-            {username: 'tony', email: 'tony@sequelizejs.com', access_level: 3, other_value: 7},
-            {username: 'tobi', email: 'tobi@fakeemail.com', access_level: 10, other_value: 11},
-            {username: 'dan', email: 'dan@sequelizejs.com', access_level: 5, other_value: 10},
-            {username: 'fred', email: 'fred@foobar.com', access_level: 3, other_value: 7}
+            {username: 'tony', email: 'tony@sequelizejs.com', access_level: 3, other_value: 7, aliasValue: 12 },
+            {username: 'tobi', email: 'tobi@fakeemail.com', access_level: 10, other_value: 11, aliasValue: 5 },
+            {username: 'dan', email: 'dan@sequelizejs.com', access_level: 5, other_value: 10, aliasValue: 1 },
+            {username: 'fred', email: 'fred@foobar.com', access_level: 3, other_value: 7, aliasValue: 10 }
           ];
           return this.ScopeMe.bulkCreate(records);
         }).then(() => {
@@ -112,6 +121,10 @@ describe(Support.getTestDialectTeaser('Model'), () => {
 
       it('should be able to merge scopes with where', function() {
         return expect(this.ScopeMe.scope('lowAccess').count({ where: { username: 'dan'}})).to.eventually.equal(1);
+      });
+
+      it('should be able to merge scopes with where on aliased fields', function() {
+        return expect(this.ScopeMe.scope('withAliasedField').count({ where: { aliasValue: 5 } })).to.eventually.equal(1);
       });
 
       it('should ignore the order option if it is found within the scope', function() {
