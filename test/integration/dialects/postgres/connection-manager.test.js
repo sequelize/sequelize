d4f1e2114da9fa9538f7c4b86c86ad7369808f91
Justin Kalland <justin@kalland.com>
2018-10-14 12:39:42 -0600

    test: add test for dynamic OIDs in postgres

--- test/integration/dialects/postgres/connection-manager.test.js
+++ test/integration/dialects/postgres/connection-manager.test.js
@@ -32,4 +32,35 @@ if (dialect.match(/^postgres/)) {
       return checkTimezoneParsing(this.sequelize.options);
     });
   });
+
+  describe('Dynamic OIDs', () => {
+    const dynamicTypesToCheck = [
+      DataTypes.GEOMETRY,
+      DataTypes.HSTORE,
+      DataTypes.GEOGRAPHY,
+      DataTypes.ENUM,
+      DataTypes.CITEXT
+    ];
+
+    it('should fetch dynamic oids from the database', () => {
+      dynamicTypesToCheck.forEach(type => {
+        type.types.postgres.oids = [];
+        type.types.postgres.array_oids = [];
+      });
+
+      // Model is needed to test the ENUM dynamic OID
+      const User = Support.sequelize.define('User', {
+        perms: DataTypes.ENUM([
+          'foo', 'bar'
+        ])
+      });
+
+      return User.sync({force: true}).then(() => {
+        dynamicTypesToCheck.forEach(type => {
+          expect(type.types.postgres.oids, `DataType.${type.key}`).to.not.be.empty;
+          expect(type.types.postgres.array_oids, `DataType.${type.key}`).to.not.be.empty;
+        });
+      });
+    });
+  });
 }
