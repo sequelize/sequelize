0fe2b38f34a57aa05ffcb0b54f53cd9cb7b2b969
David Gertmenian-Wong <dgertmenianwong@gmail.com>
2019-01-04 00:50:02 -0800

    test(instance): port over `set` and `changed` test cases

--- test/unit/instance/set.test.js
+++ test/unit/instance/set.test.js
@@ -5,7 +5,9 @@ var chai = require('chai')
   , expect = chai.expect
   , Support   = require(__dirname + '/../support')
   , DataTypes = require(__dirname + '/../../../lib/data-types')
-  , current   = Support.sequelize;
+  , current   = Support.sequelize
+  , Promise   = current.Promise
+  , sinon     = require('sinon');
 
 describe(Support.getTestDialectTeaser('Instance'), function() {
   describe('set', function () {
@@ -74,6 +76,82 @@ describe(Support.getTestDialectTeaser('Instance'), function() {
       user.set('date', new Date());
       expect(user.get('date')).to.be.an.instanceof(Date);
       expect(user.get('date')).not.to.be.NaN;
+    });
+
+    describe('custom setter', function() {
+      before(function() {
+        this.stubCreate = sinon.stub(current.getQueryInterface(), 'insert', function(instance) {
+          return Promise.resolve(instance);
+        });
+      });
+
+      after(function() {
+        this.stubCreate.restore();
+      });
+
+      var User = current.define('User', {
+        phoneNumber: {
+          type: DataTypes.STRING,
+          set: function(val) {
+            if (typeof val === 'object' && val !== null) {
+              val = '00' + val.country + val.area + val.local;
+            }
+            if (typeof val === 'string') {
+              // Canonicalize phone number
+              val = val.replace(/^\+/, '00').replace(/\(0\)|[\s+\/.\-\(\)]/g, '');
+            }
+            this.setDataValue('phoneNumber', val);
+          }
+        }
+      });
+
+      it('does not set field to changed if field is set to the same value with custom setter using primitive value', function() {
+        var user = User.build({
+          phoneNumber: '+1 234 567'
+        });
+        return user.save().then(function() {
+          expect(user.changed('phoneNumber')).to.be.false;
+
+          user.set('phoneNumber', '+1 (0) 234567'); // Canonical equivalent of existing phone number
+          expect(user.changed('phoneNumber')).to.be.false;
+        });
+      });
+
+      it('sets field to changed if field is set to the another value with custom setter using primitive value', function() {
+        var user = User.build({
+          phoneNumber: '+1 234 567'
+        });
+        return user.save().then(function() {
+          expect(user.changed('phoneNumber')).to.be.false;
+
+          user.set('phoneNumber', '+1 (0) 765432'); // Canonical non-equivalent of existing phone number
+          expect(user.changed('phoneNumber')).to.be.true;
+        });
+      });
+
+      it('does not set field to changed if field is set to the same value with custom setter using object', function() {
+        var user = User.build({
+          phoneNumber: '+1 234 567'
+        });
+        return user.save().then(function() {
+          expect(user.changed('phoneNumber')).to.be.false;
+
+          user.set('phoneNumber', { country: '1', area: '234', local: '567' }); // Canonical equivalent of existing phone number
+          expect(user.changed('phoneNumber')).to.be.false;
+        });
+      });
+
+      it('sets field to changed if field is set to the another value with custom setter using object', function() {
+        var user = User.build({
+          phoneNumber: '+1 234 567'
+        });
+        return user.save().then(function() {
+          expect(user.changed('phoneNumber')).to.be.false;
+
+          user.set('phoneNumber', { country: '1', area: '765', local: '432' }); // Canonical non-equivalent of existing phone number
+          expect(user.changed('phoneNumber')).to.be.true;
+        });
+      });
     });
   });
 });
