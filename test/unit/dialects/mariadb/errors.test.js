b47665418591d2a1f4b753c04191a37da30d9a32
zhangshichun <zhangshichun@360.net>
2019-01-15 18:41:38 +0800

    test: add  test with newline in err.msg (mysql &mariadb)

--- test/unit/dialects/mariadb/errors.test.js
+++ test/unit/dialects/mariadb/errors.test.js
@@ -40,5 +40,17 @@ if (dialect === 'mariadb') {
       expect(parsedErr.value).to.be.undefined;
       expect(parsedErr.index).to.equal('brothers_ibfk_1');
     });
+
+    it('newlines contained in err message are parsed correctly', () => {
+      const fakeErr = new Error("Duplicate entry '13888888888\r' for key 'num'");
+
+      fakeErr.code = 1062;
+
+      const parsedErr = queryProto.formatError(fakeErr);
+
+      expect(parsedErr).to.be.instanceOf(Sequelize.UniqueConstraintError);
+      expect(parsedErr.parent).to.equal(fakeErr);
+      expect(parsedErr.fields.num).to.equal('13888888888\r');
+    });
   });
 }
