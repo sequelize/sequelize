b42bbc7a24922d43ed85ef5771f1a09d114f2bd5
Igor Szymanski <igor.szymanski@me.com>
2019-02-28 08:31:37 +0200

    feat(types): enabled string association (#10481)

--- types/lib/model.d.ts
+++ types/lib/model.d.ts
@@ -319,7 +319,7 @@ export interface IncludeOptions extends Filterable, Projectable {
   /**
    * The association you want to eagerly load. (This can be used instead of providing a model/as pair)
    */
-  association?: Association;
+  association?: Association | string;
 
   /**
    * Custom `on` clause, overrides default.
