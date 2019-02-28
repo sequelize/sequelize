0033b9e3537c35bd3578aea0a507a112b4b2a19c
Igor Szymański <igor.warszawa@gmail.com>
2019-02-28 13:47:54 +0100

    test(types): include association by string

--- types/test/include.ts
+++ types/test/include.ts
@@ -1,21 +1,35 @@
-import { Model, Sequelize } from 'sequelize';
+import { Model, Sequelize, HasMany } from 'sequelize';
 
-class MyModel extends Model {}
+class MyModel extends Model {
+	public static associations: {
+		relation: HasMany
+	};
+}
 
-class AssociatedModel extends Model {}
+class AssociatedModel extends Model { }
 
 MyModel.findAll({
-    include: [
-        {
-            limit: 1,
-            model: AssociatedModel,
-            order: [['id', 'DESC']],
-            separate: true,
-            where: { state: Sequelize.col('project.state') },
-        },
-    ],
+	include: [
+		{
+			limit: 1,
+			model: AssociatedModel,
+			order: [['id', 'DESC']],
+			separate: true,
+			where: { state: Sequelize.col('project.state') },
+		},
+	],
 });
 
 MyModel.findAll({
-    include: [{ all: true }],
+	include: [{ all: true }],
+});
+
+MyModel.findAll({
+	include: [{
+		limit: 1,
+		association: 'relation',
+		order: [['id', 'DESC']],
+		separate: true,
+		where: { state: Sequelize.col('project.state') },
+	}]
 });
