830357553d15ecf41652ec997926bd5cf442eb17
Simon Schick <demwizzy@gmail.com>
2019-03-14 07:33:43 +0100

    fix(hooks): don't expose unsupported hooks on models (#10547)

--- test/unit/hooks.test.js
+++ test/unit/hooks.test.js
@@ -14,6 +14,12 @@ describe(Support.getTestDialectTeaser('Hooks'), () => {
     this.Model = current.define('m');
   });
 
+  it('does not expose non-model hooks', function() {
+    for (const badHook of ['beforeDefine', 'afterDefine', 'beforeConnect', 'afterConnect', 'beforeInit', 'afterInit']) {
+      expect(this.Model).to.not.have.property(badHook);
+    }
+  });
+
   describe('arguments', () => {
     it('hooks can modify passed arguments', function() {
       this.Model.addHook('beforeCreate', options => {
