sequelize/sequelize

commit 5adaf1093252797b50d6dfec4ed4da9fb1b7b81c

feat(data-types): handle numbers passed as objects (#10492)

lib/data-types.js
@@ -214,6 +214,17 @@ NUMBER.prototype.validate = function(value) {
 
   return true;
 };
+NUMBER.prototype._stringify = function _stringify(number) {
+  if (typeof number === 'number' || number === null || number === undefined) {
+    return number;
+  }
+
+  if (typeof number.toString === 'function') {
+    return number.toString();
+  }
+
+  return number;
+};
 Object.defineProperty(NUMBER.prototype, 'UNSIGNED', {
   get() {
     this._unsigned = true;

test/integration/data-types.test.js
@@ -12,6 +12,7 @@ const chai = require('chai'),
   uuid = require('uuid'),
   DataTypes = require('../../lib/data-types'),
   dialect = Support.getTestDialect(),
+  BigInt = require('big-integer'),
   semver = require('semver');
 
 describe(Support.getTestDialectTeaser('DataTypes'), () => {
@@ -225,6 +226,26 @@ describe(Support.getTestDialectTeaser('DataTypes'), () => {
     } else {
       return testSuccess(Type, 1);
     }
+  });
+
+  it('should handle JS BigInt type', function() {
+    const User = this.sequelize.define('user', {
+      age: Sequelize.BIGINT
+    });
+
+    const age = BigInt(Number.MAX_SAFE_INTEGER).add(Number.MAX_SAFE_INTEGER);
+
+    return User.sync({ force: true }).then(() => {
+      return User.create({ age });
+    }).then(user => {
+      expect(BigInt(user.age).toString()).to.equal(age.toString());
+      return User.findAll({
+        where: { age }
+      });
+    }).then(users => {
+      expect(users).to.have.lengthOf(1);
+      expect(BigInt(users[0].age).toString()).to.equal(age.toString());
+    });
   });
 
   it('calls parse and bindParam for DOUBLE', () => {

commit 3e94ac288cb8e22255a2124babfdc004acd6c850

fix(mysql): boolean TINYINT support
This regressed in #10492

Fixes #10600

lib/data-types.js
@@ -204,7 +204,7 @@ class NUMBER extends ABSTRACT {
     return true;
   }
   _stringify(number) {
-    if (typeof number === 'number' || number === null || number === undefined) {
+    if (typeof number === 'number' || typeof number === 'boolean' || number === null || number === undefined) {
       return number;
     }
     if (typeof number.toString === 'function') {

test/integration/data-types.test.js
@@ -248,6 +248,43 @@ describe(Support.getTestDialectTeaser('DataTypes'), () => {
     });
   });
 
+  if (dialect === 'mysql') {
+    it('should handle TINYINT booleans', function() {
+      const User = this.sequelize.define('user', {
+        id: { type: Sequelize.TINYINT, primaryKey: true },
+        isRegistered: Sequelize.TINYINT
+      });
+
+      return User.sync({ force: true }).then(() => {
+        return User.create({ id: 1, isRegistered: true });
+      }).then(registeredUser => {
+        expect(registeredUser.isRegistered).to.equal(true);
+        return User.findOne({
+          where: {
+            id: 1,
+            isRegistered: true
+          }
+        });
+      }).then(registeredUser => {
+        expect(registeredUser).to.be.ok;
+        expect(registeredUser.isRegistered).to.equal(1);
+
+        return User.create({ id: 2, isRegistered: false });
+      }).then(unregisteredUser => {
+        expect(unregisteredUser.isRegistered).to.equal(false);
+        return User.findOne({
+          where: {
+            id: 2,
+            isRegistered: false
+          }
+        });
+      }).then(unregisteredUser => {
+        expect(unregisteredUser).to.be.ok;
+        expect(unregisteredUser.isRegistered).to.equal(0);
+      });
+    });
+  }
+
   it('calls parse and bindParam for DOUBLE', () => {
     const Type = new Sequelize.DOUBLE();
 