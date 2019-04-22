sequelize/sequelize

commit 88a340dacec0b1b27d4f67866e2b416482ce7b85

refactor(scopes): just in time options conforming (#9735)

lib/model.js
@@ -757,6 +757,54 @@ class Model {
     return index;
   }
 
+
+  static _uniqIncludes(options) {
+    if (!options.include) return;
+
+    options.include = _(options.include)
+      .groupBy(include => `${include.model && include.model.name}-${include.as}`)
+      .map(includes => this._assignOptions(...includes))
+      .value();
+  }
+
+  static _baseMerge(...args) {
+    _.assignWith(...args);
+    this._conformOptions(args[0]);
+    this._uniqIncludes(args[0]);
+    return args[0];
+  }
+
+  static _mergeFunction(objValue, srcValue, key) {
+    if (Array.isArray(objValue) && Array.isArray(srcValue)) {
+      return _.union(objValue, srcValue);
+    } else if (key === 'where') {
+      if (srcValue instanceof Utils.SequelizeMethod) {
+        srcValue = { [Op.and]: srcValue };
+      }
+      if (_.isPlainObject(objValue) && _.isPlainObject(srcValue)) {
+        return Object.assign(objValue, srcValue);
+      }
+    } else if (key === 'attributes' && _.isPlainObject(objValue) && _.isPlainObject(srcValue)) {
+      return _.assignWith(objValue, srcValue, (objValue, srcValue) => {
+        if (Array.isArray(objValue) && Array.isArray(srcValue)) {
+          return _.union(objValue, srcValue);
+        }
+      });
+    }
+    return srcValue === undefined ? objValue : srcValue;
+  }
+
+  static _assignOptions(...args) {
+    return this._baseMerge(...args, this._mergeFunction);
+  }
+
+  static _defaultsOptions(...args) {
+    return this._baseMerge(...args, (...cbArgs) => {
+      [cbArgs[0], cbArgs[1]] = [cbArgs[1], cbArgs[0]];
+      return this._mergeFunction(...cbArgs);
+    });
+  }
+
   /**
    * Initialize a model, representing a table in the DB, with attributes and options.
    *
@@ -979,16 +1027,6 @@ class Model {
 
     this._scope = this.options.defaultScope;
     this._scopeNames = ['defaultScope'];
-
-    if (_.isPlainObject(this._scope)) {
-      this._conformOptions(this._scope, this);
-    }
-
-    _.each(this.options.scopes, scope => {
-      if (_.isPlainObject(scope)) {
-        this._conformOptions(scope, this);
-      }
-    });
 
     this.sequelize.modelManager.addModel(this);
     this.sequelize.runHooks('afterDefine', this);
@@ -1428,8 +1466,6 @@ class Model {
       throw new Error(`The scope ${name} already exists. Pass { override: true } as options to silence this error`);
     }
 
-    this._conformOptions(scope, this);
-
     if (name === 'defaultScope') {
       this.options.defaultScope = this._scope = scope;
     } else {
@@ -1525,29 +1561,12 @@ class Model {
 
           if (_.isFunction(scope)) {
             scope = scope();
-            this._conformOptions(scope, self);
           }
         }
       }
 
       if (scope) {
-        _.assignWith(self._scope, scope, (objectValue, sourceValue, key) => {
-          if (key === 'where') {
-            if (Array.isArray(sourceValue)) {
-              return sourceValue;
-            }
-            if (sourceValue instanceof Utils.SequelizeMethod) {
-              sourceValue = { [Op.and]: sourceValue };
-            }
-            return Object.assign(objectValue || {}, sourceValue);
-          }
-          if (['attributes', 'include', 'group'].includes(key) && Array.isArray(objectValue) && Array.isArray(sourceValue)) {
-            return objectValue.concat(sourceValue);
-          }
-
-          return objectValue ? objectValue : sourceValue;
-        });
-
+        this._assignOptions(self._scope, scope);
         self._scopeNames.push(scopeName ? scopeName : 'defaultScope');
       } else {
         throw new sequelizeErrors.SequelizeScopeError(`Invalid scope ${scopeName} called.`);
@@ -1925,7 +1944,6 @@ class Model {
    */
   static aggregate(attribute, aggregateFunction, options) {
     options = Utils.cloneDeep(options);
-    options = _.defaults(options, { attributes: [] });
 
     this._injectScope(options);
     this._conformOptions(options, this);
@@ -1943,6 +1961,7 @@ class Model {
       aggregateColumn = this.sequelize.fn('DISTINCT', aggregateColumn);
     }
 
+    options.attributes = _.intersection(options.attributes, options.group);
     options.attributes.push([this.sequelize.fn(aggregateFunction, aggregateColumn), aggregateFunction]);
 
     if (!options.dataType) {
@@ -3058,36 +3077,10 @@ class Model {
     }
   }
 
-  // Inject _scope into options. Includes should have been conformed (conformOptions) before calling this
+  // Inject _scope into options.
   static _injectScope(options) {
     const scope = Utils.cloneDeep(this._scope);
-
-    const filteredScope = _.omit(scope, 'include'); // Includes need special treatment
-
-    Utils.defaults(options, filteredScope);
-    Utils.defaults(options.where, filteredScope.where);
-
-    if (scope.include) {
-      options.include = options.include || [];
-
-      // Reverse so we consider the latest include first.
-      // This is used if several scopes specify the same include - the last scope should take precedence
-      for (const scopeInclude of scope.include.reverse()) {
-        if (scopeInclude.all || !options.include.some(item => {
-          const isSameModel = item.model && item.model.name === scopeInclude.model.name;
-          if (!isSameModel || !item.as) return isSameModel;
-
-          if (scopeInclude.as) {
-            return item.as === scopeInclude.as;
-          } else {
-            const association = scopeInclude.association || this.getAssociationForAlias(scopeInclude.model, scopeInclude.as);
-            return association ? item.as === association.as : false;
-          }
-        })) {
-          options.include.push(scopeInclude);
-        }
-      }
-    }
+    this._defaultsOptions(options, scope);
   }
 
   static inspect() {
@@ -3832,7 +3825,7 @@ class Model {
                   const values = {};
                   values[include.association.foreignKey] = this.get(this.constructor.primaryKeyAttribute, {raw: true});
                   values[include.association.otherKey] = instance.get(instance.constructor.primaryKeyAttribute, {raw: true});
-                  
+
                   // Include values defined in the association
                   Object.assign(values, include.association.through.scope);
                   if (instance[include.association.through.model.name]) {

test/integration/model/scope/associations.test.js
@@ -302,6 +302,122 @@ describe(Support.getTestDialectTeaser('Model'), () => {
             expect(user.company.projects).to.have.length(1);
           });
         });
+
+        describe('with different format', () => {
+          it('should not throw error', function() {
+            const Child = this.sequelize.define('Child');
+            const Parent = this.sequelize.define('Parent', {}, {
+              defaultScope: {
+                include: [{ model: Child }]
+              },
+              scopes: {
+                children: {
+                  include: [Child]
+                }
+              }
+            });
+            Parent.addScope('alsoChildren', {
+              include: [{ model: Child }]
+            });
+
+            Child.belongsTo(Parent);
+            Parent.hasOne(Child);
+
+            return this.sequelize.sync({ force: true }).then(() => {
+              return Promise.all([Child.create(), Parent.create()]);
+            }).then(([child, parent]) => {
+              return parent.setChild(child);
+            }).then(() => {
+              return Parent.scope('children', 'alsoChildren').findOne();
+            });
+          });
+        });
+
+        describe('with find options', () => {
+          it('should merge includes correctly', function() {
+            const Child = this.sequelize.define('Child', { name: Sequelize.STRING });
+            const Parent = this.sequelize.define('Parent', { name: Sequelize.STRING });
+            Parent.addScope('testScope1', {
+              include: [{
+                model: Child,
+                where: {
+                  name: 'child2'
+                }
+              }]
+            });
+            Parent.hasMany(Child);
+
+            return this.sequelize.sync({ force: true })
+              .then(() => {
+                return Promise.all([
+                  Parent.create({ name: 'parent1' }).then(parent => parent.createChild({ name: 'child1' })),
+                  Parent.create({ name: 'parent2' }).then(parent => parent.createChild({ name: 'child2' }))
+                ]);
+              })
+              .then(() => {
+                return Parent.scope('testScope1').findOne({
+                  include: [{
+                    model: Child,
+                    attributes: { exclude: ['name'] }
+                  }]
+                });
+              })
+              .then(parent => {
+                expect(parent.get('name')).to.equal('parent2');
+                expect(parent.Children).to.have.length(1);
+                expect(parent.Children[0].dataValues).not.to.have.property('name');
+              });
+          });
+        });
+      });
+
+      describe('scope with options', () => {
+        it('should return correct object included foreign_key', function() {
+          const Child = this.sequelize.define('Child', {
+            secret: Sequelize.STRING
+          }, {
+            scopes: {
+              public: {
+                attributes: {
+                  exclude: ['secret']
+                }
+              }
+            }
+          });
+          const Parent = this.sequelize.define('Parent');
+          Child.belongsTo(Parent);
+          Parent.hasOne(Child);
+
+          return this.sequelize.sync({ force: true })
+            .then(() => Child.create({ secret: 'super secret' }))
+            .then(() => Child.scope('public').findOne())
+            .then(user => {
+              expect(user.dataValues).to.have.property('ParentId');
+              expect(user.dataValues).not.to.have.property('secret');
+            });
+        });
+
+        it('should return correct object included foreign_key with defaultScope', function() {
+          const Child = this.sequelize.define('Child', {
+            secret: Sequelize.STRING
+          }, {
+            defaultScope: {
+              attributes: {
+                exclude: ['secret']
+              }
+            }
+          });
+          const Parent = this.sequelize.define('Parent');
+          Child.belongsTo(Parent);
+
+          return this.sequelize.sync({ force: true })
+            .then(() => Child.create({ secret: 'super secret' }))
+            .then(() => Child.findOne())
+            .then(user => {
+              expect(user.dataValues).to.have.property('ParentId');
+              expect(user.dataValues).not.to.have.property('secret');
+            });
+        });
       });
     });
   });

test/unit/model/scope.test.js
@@ -84,6 +84,7 @@ describe(Support.getTestDialectTeaser('Model'), () => {
     describe('attribute exclude / include', () => {
       const User = current.define('user', {
         password: DataTypes.STRING,
+        value: DataTypes.INTEGER,
         name: DataTypes.STRING
       }, {
         defaultScope: {
@@ -94,28 +95,22 @@ describe(Support.getTestDialectTeaser('Model'), () => {
         scopes: {
           aScope: {
             attributes: {
-              exclude: ['password']
+              exclude: ['value']
             }
           }
         }
       });
 
-      it('should be able to exclude in defaultScope #4735', () => {
-        expect(User._scope.attributes).to.deep.equal([
-          'id',
-          'name',
-          'createdAt',
-          'updatedAt'
-        ]);
+      it('should not expand attributes', () => {
+        expect(User._scope.attributes).to.deep.equal({ exclude: ['password'] });
       });
 
-      it('should be able to exclude in a scope #4925', () => {
-        expect(User.scope('aScope')._scope.attributes).to.deep.equal([
-          'id',
-          'name',
-          'createdAt',
-          'updatedAt'
-        ]);
+      it('should not expand attributes', () => {
+        expect(User.scope('aScope')._scope.attributes).to.deep.equal({ exclude: ['value'] });
+      });
+
+      it('should unite attributes with array', () => {
+        expect(User.scope('aScope', 'defaultScope')._scope.attributes).to.deep.equal({ exclude: ['value', 'password'] });
       });
     });
 
@@ -127,7 +122,7 @@ describe(Support.getTestDialectTeaser('Model'), () => {
 
     it('should apply default scope', () => {
       expect(Company._scope).to.deep.equal({
-        include: [{ model: Project }],
+        include: [Project],
         where: { active: true }
       });
     });
@@ -267,7 +262,7 @@ describe(Support.getTestDialectTeaser('Model'), () => {
         where: {
           this: 'that'
         },
-        include: [Project]
+        include: [{ model: Project }]
       });
 
       expect(Company.scope('newScope')._scope).to.deep.equal({
@@ -310,7 +305,7 @@ describe(Support.getTestDialectTeaser('Model'), () => {
 
     it('allows me to override a default scope', () => {
       Company.addScope('defaultScope', {
-        include: [Project]
+        include: [{ model: Project }]
       }, { override: true });
 
       expect(Company._scope).to.deep.equal({
@@ -318,7 +313,7 @@ describe(Support.getTestDialectTeaser('Model'), () => {
       });
     });
 
-    it('works with exclude and include attributes', () => {
+    it('should keep exclude and include attributes', () => {
       Company.addScope('newIncludeScope', {
         attributes: {
           include: ['foobar'],
@@ -327,15 +322,29 @@ describe(Support.getTestDialectTeaser('Model'), () => {
       });
 
       expect(Company.scope('newIncludeScope')._scope).to.deep.equal({
-        attributes: ['id', 'updatedAt', 'foobar']
+        attributes: {
+          include: ['foobar'],
+          exclude: ['createdAt']
+        }
       });
     });
 
+    it('should be able to merge scopes with the same include', () => {
+      Company.addScope('project', {
+        include: [{ model: Project, where: { something: false, somethingElse: 99 }}]
+      });
+      Company.addScope('alsoProject', {
+        include: [{ model: Project, where: { something: true }, limit: 1 }]
+      });
+      expect(Company.scope(['project', 'alsoProject'])._scope).to.deep.equal({
+        include: [{ model: Project, where: { something: true, somethingElse: 99 }, limit: 1 }]
+      });
+    });
   });
 
   describe('_injectScope', () => {
     it('should be able to merge scope and where', () => {
-      const scope = {
+      Sequelize.Model._scope = {
         where: {
           something: true,
           somethingElse: 42
@@ -351,9 +360,7 @@ describe(Support.getTestDialectTeaser('Model'), () => {
         limit: 9
       };
 
-      Sequelize.Model._injectScope.call({
-        _scope: scope
-      }, options);
+      Sequelize.Model._injectScope(options);
 
       expect(options).to.deep.equal({
         where: {
@@ -365,43 +372,39 @@ describe(Support.getTestDialectTeaser('Model'), () => {
       });
     });
 
-    it('should be able to overwrite multiple scopes with the same include', () => {
-      const scope = {
+    it('should be able to merge scopes with the same include', () => {
+      Sequelize.Model._scope = {
         include: [
-          { model: Project, where: { something: false }},
-          { model: Project, where: { something: true }}
+          { model: Project, where: { something: false, somethingElse: 99 }},
+          { model: Project, where: { something: true }, limit: 1 }
         ]
       };
 
       const options = {};
 
-      Sequelize.Model._injectScope.call({
-        _scope: scope
-      }, options);
+      Sequelize.Model._injectScope(options);
 
       expect(options.include).to.have.length(1);
-      expect(options.include[0]).to.deep.equal({ model: Project, where: { something: true }});
+      expect(options.include[0]).to.deep.equal({ model: Project, where: { something: true, somethingElse: 99 }, limit: 1 });
     });
 
-    it('should be able to override scoped include', () => {
-      const scope = {
-        include: [{ model: Project, where: { something: false }}]
+    it('should be able to merge scoped include', () => {
+      Sequelize.Model._scope = {
+        include: [{ model: Project, where: { something: false, somethingElse: 99 }}]
       };
 
       const options = {
-        include: [{ model: Project, where: { something: true }}]
+        include: [{ model: Project, where: { something: true }, limit: 1 }]
       };
 
-      Sequelize.Model._injectScope.call({
-        _scope: scope
-      }, options);
+      Sequelize.Model._injectScope(options);
 
       expect(options.include).to.have.length(1);
-      expect(options.include[0]).to.deep.equal({ model: Project, where: { something: true }});
+      expect(options.include[0]).to.deep.equal({ model: Project, where: { something: true, somethingElse: 99 }, limit: 1 });
     });
 
     it('should be able to merge aliased includes with the same model', () => {
-      const scope = {
+      Sequelize.Model._scope = {
         include: [{model: User, as: 'someUser'}]
       };
 
@@ -409,17 +412,15 @@ describe(Support.getTestDialectTeaser('Model'), () => {
         include: [{model: User, as: 'otherUser'}]
       };
 
-      Sequelize.Model._injectScope.call({
-        _scope: scope
-      }, options);
+      Sequelize.Model._injectScope(options);
 
       expect(options.include).to.have.length(2);
-      expect(options.include[0]).to.deep.equal({model: User, as: 'otherUser'});
-      expect(options.include[1]).to.deep.equal({model: User, as: 'someUser'});
+      expect(options.include[0]).to.deep.equal({model: User, as: 'someUser'});
+      expect(options.include[1]).to.deep.equal({model: User, as: 'otherUser'});
     });
 
     it('should be able to merge scoped include with include in find', () => {
-      const scope = {
+      Sequelize.Model._scope = {
         include: [
           { model: Project, where: { something: false }}
         ]
@@ -431,18 +432,16 @@ describe(Support.getTestDialectTeaser('Model'), () => {
         ]
       };
 
-      Sequelize.Model._injectScope.call({
-        _scope: scope
-      }, options);
+      Sequelize.Model._injectScope(options);
 
       expect(options.include).to.have.length(2);
-      expect(options.include[0]).to.deep.equal({ model: User, where: { something: true }});
-      expect(options.include[1]).to.deep.equal({ model: Project, where: { something: false }});
+      expect(options.include[0]).to.deep.equal({ model: Project, where: { something: false }});
+      expect(options.include[1]).to.deep.equal({ model: User, where: { something: true }});
     });
 
     describe('include all', () => {
       it('scope with all', () => {
-        const scope = {
+        Sequelize.Model._scope = {
           include: [
             { all: true }
           ]
@@ -454,36 +453,32 @@ describe(Support.getTestDialectTeaser('Model'), () => {
           ]
         };
 
-        Sequelize.Model._injectScope.call({
-          _scope: scope
-        }, options);
-
-        expect(options.include).to.have.length(2);
-        expect(options.include[0]).to.deep.equal({ model: User, where: { something: true }});
-        expect(options.include[1]).to.deep.equal({ all: true });
-      });
-
-
-      it('options with all', () => {
-        const scope = {
-          include: [
-            { model: User, where: { something: true }}
-          ]
-        };
-
-        const options = {
-          include: [
-            { all: true }
-          ]
-        };
-
-        Sequelize.Model._injectScope.call({
-          _scope: scope
-        }, options);
+        Sequelize.Model._injectScope(options);
 
         expect(options.include).to.have.length(2);
         expect(options.include[0]).to.deep.equal({ all: true });
         expect(options.include[1]).to.deep.equal({ model: User, where: { something: true }});
+      });
+
+
+      it('options with all', () => {
+        Sequelize.Model._scope = {
+          include: [
+            { model: User, where: { something: true }}
+          ]
+        };
+
+        const options = {
+          include: [
+            { all: true }
+          ]
+        };
+
+        Sequelize.Model._injectScope(options);
+
+        expect(options.include).to.have.length(2);
+        expect(options.include[0]).to.deep.equal({ model: User, where: { something: true }});
+        expect(options.include[1]).to.deep.equal({ all: true });
       });
     });
   });

commit 379b56c873d1c68bee1f48a828b14662de070834

fix(model): make aggregate merge attributes rather than override (#10662)
This was broken by:
https://github.com/sequelize/sequelize/commit/88a340dacec0b1b27d4f67866e2b416482ce7b85#diff-a140b60b6a99a8c69f4b5d0d1f1f2bfaR1964

So cc @u9r52sld

lib/model.js
@@ -18,6 +18,7 @@ const DataTypes = require('./data-types');
 const Hooks = require('./hooks');
 const associationsMixin = require('./associations/mixin');
 const Op = require('./operators');
+const { noDoubleNestedGroup } = require('./utils/deprecations');
 
 
 // This list will quickly become dated, but failing to maintain this list just means
@@ -814,10 +815,9 @@ class Model {
     return this._baseMerge(...args, this._mergeFunction);
   }
 
-  static _defaultsOptions(...args) {
-    return this._baseMerge(...args, (...cbArgs) => {
-      [cbArgs[0], cbArgs[1]] = [cbArgs[1], cbArgs[0]];
-      return this._mergeFunction(...cbArgs);
+  static _defaultsOptions(target, opts) {
+    return this._baseMerge(target, opts, (srcValue, objValue, key) => {
+      return this._mergeFunction(objValue, srcValue, key);
     });
   }
 
@@ -1932,7 +1932,10 @@ class Model {
   static aggregate(attribute, aggregateFunction, options) {
     options = Utils.cloneDeep(options);
 
+    // We need to preserve attributes here as the `injectScope` call would inject non aggregate columns.
+    const prevAttributes = options.attributes;
     this._injectScope(options);
+    options.attributes = prevAttributes;
     this._conformIncludes(options, this);
 
     if (options.include) {
@@ -1948,8 +1951,17 @@ class Model {
       aggregateColumn = this.sequelize.fn('DISTINCT', aggregateColumn);
     }
 
-    options.attributes = _.intersection(options.attributes, options.group);
-    options.attributes.push([this.sequelize.fn(aggregateFunction, aggregateColumn), aggregateFunction]);
+    let { group } = options;
+    if (Array.isArray(group) && Array.isArray(group[0])) {
+      noDoubleNestedGroup();
+      group = _.flatten(group);
+    }
+    options.attributes = _.unionBy(
+      options.attributes,
+      group,
+      [[this.sequelize.fn(aggregateFunction, aggregateColumn), aggregateFunction]],
+      a => Array.isArray(a) ? a[1] : a
+    );
 
     if (!options.dataType) {
       if (attrOptions) {

lib/utils/deprecations.js
@@ -8,3 +8,4 @@ exports.noRawAttributes = deprecate(noop, 'Use sequelize.fn / sequelize.literal 
 exports.noTrueLogging = deprecate(noop, 'The logging-option should be either a function or false. Default: console.log', 'SEQUELIZE0002');
 exports.noStringOperators = deprecate(noop, 'String based operators are deprecated. Please use Symbol based operators for better security, read more at http://docs.sequelizejs.com/manual/querying.html#operators', 'SEQUELIZE0003');
 exports.noBoolOperatorAliases = deprecate(noop, 'A boolean value was passed to options.operatorsAliases. This is a no-op with v5 and should be removed.', 'SEQUELIZE0004');
+exports.noDoubleNestedGroup = deprecate(noop, 'Passing a double nested nested array to `group` is unsupported and will be removed in v6.', 'SEQUELIZE0005');

test/integration/model.test.js
@@ -1914,6 +1914,19 @@ describe(Support.getTestDialectTeaser('Model'), () => {
       });
     });
 
+    describe('aggregate', () => {
+      if (dialect === 'mssql') {
+        return;
+      }
+      it('allows grouping by aliased attribute', function() {
+        return this.User.aggregate('id', 'count', {
+          attributes: [['id', 'id2']],
+          group: ['id2'],
+          logging: true
+        });
+      });
+    });
+
     describe('options sent to aggregate', () => {
       let options, aggregateSpy;
 