a6b2d01c8f2d40abb164fe4ac3648617b1ac0d60
Simon Schick <demwizzy@gmail.com>
2019-05-04 14:56:18 -0700

    fix: certain queries running in parallel where they should not be

--- test/integration/model.test.js
+++ test/integration/model.test.js
@@ -355,7 +355,6 @@ describe(Support.getTestDialectTeaser('Model'), () => {
     });
 
     it('should allow the user to specify indexes in options', function() {
-      this.retries(3);
       const indices = [{
         name: 'a_b_uniq',
         unique: true,
@@ -815,7 +814,7 @@ describe(Support.getTestDialectTeaser('Model'), () => {
 
   describe('save', () => {
     it('should mapping the correct fields when saving instance. see #10589', function() {
-      const User = this.sequelize.define('User', { 
+      const User = this.sequelize.define('User', {
         id3: {
           field: 'id',
           type: Sequelize.INTEGER,
@@ -862,7 +861,7 @@ describe(Support.getTestDialectTeaser('Model'), () => {
     });
 
     it('should mapping the correct fields when updating instance. see #10589', function() {
-      const User = this.sequelize.define('User', { 
+      const User = this.sequelize.define('User', {
         id3: {
           field: 'id',
           type: Sequelize.INTEGER,
