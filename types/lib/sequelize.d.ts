2f7b32bf3a371d409b0096b7cca7c1ed35cc776d
Abady <gamersspeaks@gmail.com>
2019-05-22 20:56:15 +0300

    fix(types): add missing types to SyncOptions (#10948)

--- types/lib/sequelize.d.ts
+++ types/lib/sequelize.d.ts
@@ -51,6 +51,21 @@ export interface SyncOptions extends Logging {
    * The schema that the tables should be created in. This can be overridden for each table in sequelize.define
    */
   schema?: string;
+  
+   /**
+   * An optional parameter to specify the schema search_path (Postgres only)
+   */
+  searchPath?: string;
+  
+   /**
+   * If hooks is true then beforeSync, afterSync, beforeBulkSync, afterBulkSync hooks will be called
+   */
+  hooks?: boolean;
+  
+   /**
+   * Alters tables to fit models. Not recommended for production use. Deletes data in columns that were removed or had their type changed in the model.
+   */
+  alter?: boolean;
 }
 
 export interface DefaultSetOptions {}
