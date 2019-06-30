32c7258ea54b90e55d878a3d36275e8e4e49f23b
Raphaël Ricard <rinot95@gmail.com>
2019-06-30 20:01:57 +0200

    test(query): add alias on base model

--- test/integration/dialects/postgres/query.test.js
+++ test/integration/dialects/postgres/query.test.js
@@ -14,7 +14,7 @@ if (dialect.match(/^postgres/)) {
     const executeTest = (options, test) => {
       const sequelize = Support.createSequelizeInstance(options);
 
-      const User = sequelize.define('User', { name: DataTypes.STRING });
+      const User = sequelize.define('User', { name: DataTypes.STRING, updatedAt: DataTypes.DATE }, { underscored: true });
       const Task = sequelize.define('Task', { title: DataTypes.STRING });
 
       User.belongsTo(Task, { as: alias, foreignKey: 'taskId' });
@@ -22,7 +22,7 @@ if (dialect.match(/^postgres/)) {
 
       return sequelize.sync({ force: true }).then(() => {
         return Task.create({ title: 'SuperTask' }).then(task => {
-          return User.create({ name: 'test', taskId: task.id }).then(() => {
+          return User.create({ name: 'test', taskId: task.id, updatedAt: new Date() }).then(() => {
             return User.findOne({
               include: {
                 model: Task,
