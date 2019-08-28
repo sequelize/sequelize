f2094c40d0d1d3a2652d77795939901b648a1fb6
Bill Li <me@billxinli.com>
2019-08-28 12:53:25 -0400

    test(test): added a type test for rejectOnEmpty

--- types/test/errors.ts
+++ types/test/errors.ts
@@ -25,6 +25,25 @@ async function test() {
         }
     }
 
+
+    class CustomError extends Error {}
+
+    try {
+        await User.findOne({
+            rejectOnEmpty: new CustomError('User does not exist'),
+            where: {
+                username: 'something_that_doesnt_exist',
+            },
+        });
+    } catch (e) {
+        if (!(e instanceof CustomError)) {
+            console.error('should return CustomError');
+        }
+        if (e.message !== 'User does not exist') {
+            console.error('should return CustomError with the proper message')
+        }
+    }
+
     try {
         const user: User | null = await User.findByPk(1);
         if (user != null) {
