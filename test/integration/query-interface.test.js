70dc2c7ddadbab4a1789603bcd779548262850ce
João Eiras <joao.eiras@gmail.com>
2019-09-17 10:00:54 +0200

    fix(mssql): filter tables in mssql and mysql showTablesQuery()
    Aligns behavior of mysql+mssql showTablesQuery() with postgresql which only return
    TABLEs and not VIEWs.
    
    Closes #11438

--- test/integration/query-interface.test.js
+++ test/integration/query-interface.test.js
@@ -41,6 +41,28 @@ describe(Support.getTestDialectTeaser('QueryInterface'), () => {
     });
   });
 
+  describe('showAllTables', () => {
+    it('should contain only tables', function() {
+      const cleanup = r => {
+        return this.sequelize.query('DROP VIEW V_Fail').then(() => r, () => r);
+      };
+      return this.queryInterface
+        .createTable('my_test_table', { name: DataTypes.STRING })
+        .then(cleanup)
+        .then(() => this.sequelize.query('CREATE VIEW V_Fail AS SELECT 1 Id'))
+        .then(() => this.queryInterface.showAllTables())
+        .then(cleanup)
+        .then(tableNames => {
+          if (tableNames[0] && tableNames[0].tableName) {
+            tableNames = tableNames.map(v => v.tableName);
+          }
+          expect(tableNames).to.have.length(1);
+          expect(tableNames).to.contain('my_test_table');
+          expect(tableNames).to.not.contain('V_Fail');
+        });
+    });
+  });
+
   describe('renameTable', () => {
     it('should rename table', function() {
       return this.queryInterface
