93822f50d07c05a077a8cfaa726eb754f29b1ac9
Eduard Castellanos <andrescastellanos112013gmail.com>
2020-01-22 12:16:12 -0500

    fix(typings): `comparator` arg of `Sequelize.where` (#10796)

--- types/test/where.ts
+++ types/test/where.ts
@@ -325,3 +325,13 @@ Sequelize.where(
         [Op.notILike]: Sequelize.literal('LIT')
     }
 )
+
+Sequelize.where(Sequelize.col("ABS"), Op.is, null);
+
+Sequelize.where(
+  Sequelize.fn("ABS", Sequelize.col("age")),
+  Op.like,
+  Sequelize.fn("ABS", Sequelize.col("age"))
+);
+
+Sequelize.where(Sequelize.col("ABS"), null);
