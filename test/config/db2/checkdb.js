dc021ccf4aab5bc2dce5c40aaf75be16ec7361b6
Bimal Jha <bjha@rocketsoftware.com>
2020-02-29 14:18:40 -0500

    fix: linting issue

--- test/config/db2/checkdb.js
+++ test/config/db2/checkdb.js
@@ -1,26 +1,26 @@
-#!/usr/bin/env node
+'use strict';
 
 const execSync = require('child_process').execSync;
 let isDbReady = false;
 
 function checkDb() {
-  let logs = execSync('docker logs db2server').toString();
+  const logs = execSync('docker logs db2server').toString();
   if (logs.match(/Setup has completed/)) {
     isDbReady = true;
     clearTimeout(timeoutObj);
     clearInterval(intervalObj);
-    console.log("Database is ready for use.");
+    console.log('Database is ready for use.');
   }
 }
 
-const intervalObj = setInterval(checkDb, 2000);
+const intervalObj = setInterval(checkDb, 10000);
 
 const timeoutObj = setTimeout(() => {
   clearInterval(intervalObj);
   if (isDbReady === false) {
-    console.log("Error: Db2 docker setup has not completed in 10 minutes.");
+    console.log('Error: Db2 docker setup has not completed in 10 minutes.');
   }
-}, 10*60*1000);
+}, 10 * 60 * 1000);
 
 checkDb();
 
