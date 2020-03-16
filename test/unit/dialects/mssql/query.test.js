b739613715c542c9f245ea2a33e5f9cc10edc36a
MartinJLee <therhymeorange@gmail.com>
2020-03-16 18:10:53 +1000

    fix(mssql): set correct scale for float (#11962)

--- test/unit/dialects/mssql/query.test.js
+++ test/unit/dialects/mssql/query.test.js
@@ -15,18 +15,22 @@ let sandbox, query;
 
 if (dialect === 'mssql') {
   describe('[MSSQL Specific] Query', () => {
-    describe('beginTransaction', () => {
-      beforeEach(() => {
-        sandbox = sinon.createSandbox();
-        const options = {
-          transaction: { name: 'transactionName' },
-          isolationLevel: 'REPEATABLE_READ',
-          logging: false
-        };
-        sandbox.stub(connectionStub, 'beginTransaction').callsArg(0);
-        query = new Query(connectionStub, sequelize, options);
-      });
+    beforeEach(() => {
+      sandbox = sinon.createSandbox();
+      const options = {
+        transaction: { name: 'transactionName' },
+        isolationLevel: 'REPEATABLE_READ',
+        logging: false
+      };
+      sandbox.stub(connectionStub, 'beginTransaction').callsArg(0);
+      query = new Query(connectionStub, sequelize, options);
+    });
 
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    describe('beginTransaction', () => {
       it('should call beginTransaction with correct arguments', () => {
         return query._run(connectionStub, 'BEGIN TRANSACTION')
           .then(() => {
@@ -34,10 +38,6 @@ if (dialect === 'mssql') {
             expect(connectionStub.beginTransaction.args[0][1]).to.equal('transactionName');
             expect(connectionStub.beginTransaction.args[0][2]).to.equal(tediousIsolationLevel.REPEATABLE_READ);
           });
-      });
-
-      afterEach(() => {
-        sandbox.restore();
       });
     });
 
@@ -64,5 +64,25 @@ if (dialect === 'mssql') {
         expect(result[0]).to.equal(expected);
       });
     });
+
+    describe('getSQLTypeFromJsType', () => {
+      const TYPES = tedious.TYPES;
+      it('should return correct parameter type', () => {
+        expect(query.getSQLTypeFromJsType(2147483647, TYPES)).to.eql({ type: TYPES.Int, typeOptions: {} });
+        expect(query.getSQLTypeFromJsType(-2147483648, TYPES)).to.eql({ type: TYPES.Int, typeOptions: {} });
+
+        expect(query.getSQLTypeFromJsType(2147483648, TYPES)).to.eql({ type: TYPES.BigInt, typeOptions: {} });
+        expect(query.getSQLTypeFromJsType(-2147483649, TYPES)).to.eql({ type: TYPES.BigInt, typeOptions: {} });
+
+        expect(query.getSQLTypeFromJsType(Buffer.from('abc'), TYPES)).to.eql({ type: TYPES.VarBinary, typeOptions: {} });
+      });
+
+      it('should return parameter type correct scale for float', () => {
+        expect(query.getSQLTypeFromJsType(1.23, TYPES)).to.eql({ type: TYPES.Numeric, typeOptions: { precision: 30, scale: 2 } });
+        expect(query.getSQLTypeFromJsType(0.30000000000000004, TYPES)).to.eql({ type: TYPES.Numeric, typeOptions: { precision: 30, scale: 17 } });
+        expect(query.getSQLTypeFromJsType(2.5e-15, TYPES)).to.eql({ type: TYPES.Numeric, typeOptions: { precision: 30, scale: 16 } });
+      });
+    });
+
   });
 }
