73d5c66f3076f322045d0c865809bf044dc9bd8e
Davide Mauri <damauri@microsoft.com>
2020-06-23 09:47:31 +0200

    test: removed erroneus async

--- test/integration/configuration.test.js
+++ test/integration/configuration.test.js
@@ -70,7 +70,7 @@ describe(Support.getTestDialectTeaser('Configuration'), () => {
   });
 
   describe('Instantiation with a URL string', () => { 
-    it('should handle options in URL', async () => {
+    it('should handle options in URL', () => {
       const sequelizeWithOptions = new Sequelize('mssql://user:password!@server.database.windows.net/database?options={"encrypt":true}&anotherOption=1');
       expect(sequelizeWithOptions.options.dialectOptions.options.encrypt).to.equal(true);
       expect(sequelizeWithOptions.options.dialectOptions.anotherOption).to.equal('1');
