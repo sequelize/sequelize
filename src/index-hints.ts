72bc739ae964a22658a5f78de9ece7a4a5453ae0
Sushant <sushantdhiman@outlook.com>
2020-07-05 15:15:27 +0530

    refactor: use enum for suitable options

--- src/index-hints.ts
+++ src/index-hints.ts
@@ -1,14 +1,10 @@
 /**
  * An enum of index hints to be used in mysql for querying with index hints
- *
- * @property USE
- * @property FORCE
- * @property IGNORE
  */
-const IndexHints = {
-  USE: 'USE',
-  FORCE: 'FORCE',
-  IGNORE: 'IGNORE'
-};
+enum IndexHints {
+  USE = 'USE',
+  FORCE = 'FORCE',
+  IGNORE = 'IGNORE'
+}
 
 module.exports = IndexHints;
