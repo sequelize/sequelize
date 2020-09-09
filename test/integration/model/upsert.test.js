23258273d3931a8749f57114a728f5d93f4900b3
Matthew Blasius <matthew.blasius@expel.io>
2020-09-09 18:32:57 -0500

    fix(upsert): do not overwrite an explcit created_at during upsert
    We found that when doing an upsert with model data that already included a `createdAt` timestampe, our explcit `createdAt` was being overwritten with the current time any time we also utilized the `fields` option.
    
    e.g.
    
    ```
    const instance = await MyModel.upsert({ id: 1, myfield: 'blah', createdAt: new Date('2010-01-01T12:00:00.000Z') },  { fields: [ 'myfield' ], returning: true });
    console.log(instance.createdAt); // expected 2010-01-01T12:00:00.000Z, but got a now()-ish timestamp.
    ```
    
    Issue appears to be that the check for a provided `createdAt` was being checked against the `updateValues` instead of the `insertValues`.  Most of the time, this is inconsequential because the `insertValues` and `updateValues` both contain the same fields.  But, when using the `fields` feature, the `createdAt` field is stripped away from the `updateValues`, so sequelize happily overwrites the `insertValues.createAt` value.

--- test/integration/model/upsert.test.js
+++ test/integration/model/upsert.test.js
@@ -302,6 +302,15 @@ describe(Support.getTestDialectTeaser('Model'), () => {
         clock.restore();
       });
 
+      it('does not overwrite createdAt when supplied as an explicit insert value when using fields', async function() {
+        const clock = sinon.useFakeTimers();
+        const originalCreatedAt = new Date('2010-01-01T12:00:00.000Z');
+        await this.User.upsert({ id: 42, username: 'john', createdAt: originalCreatedAt }, { fields: ['id', 'username'] });
+        const user = await this.User.findByPk(42);
+        expect(user.createdAt).to.deep.equal(originalCreatedAt);
+        clock.restore();
+      });
+
       it('does not update using default values', async function() {
         await this.User.create({ id: 42, username: 'john', baz: 'new baz value' });
         const user0 = await this.User.findByPk(42);
@@ -578,16 +587,16 @@ describe(Support.getTestDialectTeaser('Model'), () => {
           });
         });
 
-        it('should return default value set by the database (upsert)', async function() {      
+        it('should return default value set by the database (upsert)', async function() {
           const User = this.sequelize.define('User', {
             name: { type: DataTypes.STRING, primaryKey: true },
             code: { type: Sequelize.INTEGER, defaultValue: Sequelize.literal(2020) }
           });
-    
+
           await User.sync({ force: true });
-    
+
           const [user, created] = await User.upsert({ name: 'Test default value' }, { returning: true });
-      
+
           expect(user.name).to.be.equal('Test default value');
           expect(user.code).to.be.equal(2020);
 
