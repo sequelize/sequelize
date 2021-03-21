6a87f2ea4ead5c578448430cb0beea7f7db86c60
Constantin Metz <constantin@metzworld.com>
2021-03-21 15:13:25 +0100

    feat(tests): added tests with expectTypeOf

--- types/test/sequelize.ts
+++ types/test/sequelize.ts
@@ -1,4 +1,6 @@
-import { Config, Sequelize, Model, QueryTypes, ModelCtor } from 'sequelize';
+import { expectTypeOf } from 'expect-type';
+import { Config, Sequelize, Model, QueryTypes, ModelCtor, DataTypes, Optional } from 'sequelize';
+import { ModelDefined } from '../lib/model';
 import { Fn } from '../lib/utils';
 
 Sequelize.useCLS({
@@ -59,6 +61,47 @@ class Model2 extends Model{}
 const myModel: ModelCtor<Model1> = sequelize.models.asd;
 myModel.hasOne(Model2)
 myModel.findAll();
+
+// expect type tests
+/// implicitly specifying attributes
+const myModel2 = sequelize.define("myModel2", {
+  column: DataTypes.STRING,
+});
+
+expectTypeOf(myModel2).toEqualTypeOf<
+  ModelDefined<
+    | Record<string, unknown>
+    | ({
+        column: unknown;
+      } & Record<string, unknown>),
+    {
+      column: unknown;
+    }
+  >
+>();
+
+/// explicitly specifying attributes
+interface ModelAttributes {
+  id: number;
+  column: string;
+}
+
+interface CreationAttributes extends Optional<ModelAttributes, "id"> {}
+
+const myModel3 = sequelize.define<ModelAttributes, CreationAttributes>(
+  "myModel3",
+  {
+    column: DataTypes.STRING,
+  }
+);
+
+expectTypeOf(myModel3).toEqualTypeOf<
+  ModelDefined<
+    | (CreationAttributes & Record<string, unknown>)
+    | (ModelAttributes & Record<string, unknown>),
+    CreationAttributes & Record<string, unknown>
+  >
+>();
 
 async function test() {
   const [results, meta]: [unknown[], unknown] = await sequelize.query('SELECT * FROM `user`', { type: QueryTypes.RAW });
