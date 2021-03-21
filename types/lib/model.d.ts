88925077eba182814af2ec8f6fa6674ae1ee5b15
Tobin Brown <tobinjbrown@gmail.com>
2021-03-21 13:28:46 -0500

    fix(types): allow bigints in `WhereValue` (#13028)
    Co-authored-by: Pedro Augusto de Paula Barbosa <papb1996@gmail.com>

--- types/lib/model.d.ts
+++ types/lib/model.d.ts
@@ -332,11 +332,12 @@ export interface WhereGeometryOptions {
  * WhereAttributeHash is in there for JSON columns.
  */
 export type WhereValue<TAttributes = any> =
-  | string // literal value
-  | number // literal value
-  | boolean // literal value
-  | Date // literal value
-  | Buffer // literal value
+  | string
+  | number
+  | bigint
+  | boolean
+  | Date
+  | Buffer
   | null
   | WhereOperators
   | WhereAttributeHash<any> // for JSON columns
