035ae2e06589c9750b040f27a0be4cb6f1f870ac
Jacob Ley <jacob.ley@showcasejobs.com>
2021-03-22 09:01:08 -0700

    fix(typings): add tests to support raw finds

---
+++ types/test/findAll.ts
@@ -0,0 +1,24 @@
+import { expectTypeOf } from 'expect-type'
+import Sequelize from 'sequelize';
+import { User } from './models/User';
+
+async () => {
+    const users = await User.findAll({ raw: false });
+    expectTypeOf(users).toEqualTypeOf<User[]>()
+
+    interface RawUser {
+        foo: 'bar';
+    }
+    const rawUsers = await User.findAll<User, RawUser>({
+        attributes: [
+            ['bar', 'foo'],
+            'ignored',
+            [Sequelize.col('table.id'), 'xyz'],
+        ],
+        raw: true,
+    });
+    expectTypeOf(rawUsers).toEqualTypeOf<RawUser[]>();
+
+    // @ts-expect-error
+    rawUsers[0].id = 123; // not an instance
+};
--- types/test/findByPk.ts
+++ types/test/findByPk.ts
@@ -1,3 +1,16 @@
+import { expectTypeOf } from 'expect-type'
 import { User } from './models/User';
 
-User.findByPk(Buffer.from('asdf'));
+async () => {
+    const user = await User.findByPk(Buffer.from('asdf'));
+    expectTypeOf(user).toEqualTypeOf<User | null>()
+
+    interface RawUser {
+        foo: 'bar';
+    }
+    const rawUser = await User.findByPk<User, RawUser>(123, {
+        attributes: [['bar', 'foo']],
+        raw: true,
+    });
+    expectTypeOf(rawUser).toEqualTypeOf<RawUser | null>();
+};
