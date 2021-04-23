a9cfe56bfcd57e09b55d1a546dd50ad2d6362f75
Jacob Ley <jacob.ley@showcasejobs.com>
2021-04-23 12:02:11 -0700

    fix(typings): add order to findAll test

--- types/test/findAll.ts
+++ types/test/findAll.ts
@@ -1,10 +1,28 @@
 import { expectTypeOf } from 'expect-type'
-import Sequelize from 'sequelize';
+import Sequelize, { Op } from 'sequelize';
 import { User } from './models/User';
 
 async () => {
     const users = await User.findAll({ raw: false });
     expectTypeOf(users).toEqualTypeOf<User[]>()
+
+    await User.findAll({
+      order: [
+        ['id', 'DESC'],
+        ['AssociatedModel', User, 'id', 'DESC'],
+        ['AssociatedModel', User, Sequelize.col('MyModel.id')],
+        [User, 'id'],
+        'id',
+        Sequelize.col('id'),
+        Sequelize.fn('FN'),
+        Sequelize.literal('<literal>'),
+        Sequelize.where(Sequelize.col('id'), Op.eq, '<id>'),
+        [Sequelize.col('id'), 'ASC'],
+        [Sequelize.fn('FN'), 'DESC NULLS LAST'],
+        [Sequelize.literal('<literal>'), 'NULLS FIRST'],
+        [Sequelize.where(Sequelize.col('id'), Op.eq, '<id>'), 'DESC'],
+      ],
+    });
 
     interface RawUser {
         foo: 'bar';
