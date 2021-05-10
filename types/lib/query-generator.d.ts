b80205777db148fa4a392206d54d23e0fae121e9
Jacob Ley <jacob@getshowcase.io>
2021-05-10 12:48:11 -0700

    fix(typings): add whereItemsQuery to query generator

--- types/lib/query-generator.d.ts
+++ types/lib/query-generator.d.ts
@@ -1,3 +1,4 @@
+import { WhereOptions } from './model';
 import { Dialect, Sequelize } from './sequelize';
 import { SequelizeMethod } from './utils';
 
@@ -14,5 +15,26 @@ export class QueryGenerator {
    public readonly sequelize: Sequelize;
    public readonly dialect: Dialect;
 
+   /**
+    * @example
+    * queryGenerator.handleSequelizeMethod(fn('FOO', col('abc.xyz')) // FOO("abc"."xyz")
+    *
+    * @param {object} smth - sequelize method
+    * @returns {string} SQL
+    */
    public handleSequelizeMethod(smth: SequelizeMethod): string;
+
+   /**
+    * @example
+    * queryGenerator.handleSequelizeMethod({
+    *   [Op.and]: [
+    *       where(fn('FOO', { [Op.in]: [1, 2] })),
+    *       where(col('abc.xyz', Op.eq, 123),
+    *   ],
+    * }) // (FOO() IN (1,2) AND "abc.xyz" = 123)
+    *
+    * @param {object} where - where object
+    * @returns {string} SQL
+    */
+   public whereItemsQuery(where: WhereOptions): string;
 }
--- types/test/query-generator.ts
+++ types/test/query-generator.ts
@@ -1,5 +1,5 @@
 import { expectTypeOf } from 'expect-type';
-import { fn, literal, col, QueryGenerator } from 'sequelize';
+import { fn, literal, col, Op, QueryGenerator, where } from 'sequelize';
 
 declare let queryGenerator: QueryGenerator;
 
@@ -13,5 +13,24 @@ expectTypeOf(
     queryGenerator.handleSequelizeMethod(col('foo_bar'))
 ).toMatchTypeOf<string>();
 
+expectTypeOf(
+    queryGenerator.whereItemsQuery({
+        abc: 123,
+        [Op.and]: [
+            literal('CASE WHEN RAND() > 0.5 THEN TRUE ELSE FALSE END'),
+            where(col('abc.xyz'), Op.eq, '<id>'),
+        ],
+        [Op.or]: [
+            literal('TRUE'),
+            where(fn('FOO'), { [Op.in]: [1, 2] }),
+        ],
+    })
+).toMatchTypeOf<string>();
+expectTypeOf(
+    queryGenerator.whereItemsQuery(literal('TRUE'))
+).toMatchTypeOf<string>();
+
 // @ts-expect-error literal object
 queryGenerator.handleSequelizeMethod({ myCol: '<my-val>' })
+// @ts-expect-error literal string
+queryGenerator.whereItemsQuery('TRUE')
