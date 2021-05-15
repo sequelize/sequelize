66212cb395c86f7061cc3cc7068f6844af4bf16e
Jesse Peng <jesse.peng@dynatrace.com>
2021-05-14 22:29:50 -0400

    feat(dialect): working user practice

--- test/integration/dialects/snowflake/connector-manager.test.js
+++ test/integration/dialects/snowflake/connector-manager.test.js
@@ -4,29 +4,56 @@ const chai = require('chai');
 const expect = chai.expect;
 const Support = require('../../support');
 const dialect = Support.getTestDialect();
+const DataTypes = require('../../../../lib/data-types');
 
 if (dialect === 'snowflake') {
-  describe('[MYSQL Specific] Connection Manager', () => {
+  describe('[SNOWFLAKE Specific] Connection Manager', () => {
+    let User;
 
-    /*
-    it('-FOUND_ROWS can be suppressed to get back legacy behavior', async () => {
-      const sequelize = Support.createSequelizeInstance({ dialectOptions: { flags: '' } });
-      const User = sequelize.define('User', { username: DataTypes.STRING });
+    before(async () => {
+      const sequelize = Support.createSequelizeInstance();
+      User = sequelize.define('User', { username: DataTypes.STRING });
 
       await User.sync({ force: true });
       await User.create({ id: 1, username: 'jozef' });
+      await User.create({ id: 2, username: 'jeff' });
+    });
 
-      const [affectedCount] = await User.update({ username: 'jozef' }, {
+    after(async () =>{
+      await User.drop();
+    });
+
+    it('findOne with where', async () => {
+      const user = await User.findOne({
+        where:
+        {
+          username: 'jeff'
+        }
+      });
+      await user.id.should.equal(2);
+    });
+
+    it('findAll with orderby', async () => {
+      const username = 'test';
+      await User.create({ id: 3, username });
+      const users = await User.findAll({
+        order: [['createdAt', 'ASC']]
+      });
+      await users[users.length - 1].username.should.equal(username);
+    });
+
+    it('Update', async () => {
+      const res = await User.update({ username: 'jozef1' }, {
         where: {
           id: 1
         }
       });
-
       // https://github.com/sequelize/sequelize/issues/7184
-      await affectedCount.should.equal(1);
+      await res[0].should.equal(1);
     });
-    */
 
+
+    // fake test for now since keepDefaultTimezone is commented out in snowflake dialect
     it('should acquire a valid connection when keepDefaultTimezone is true', async () => {
       const sequelize = Support.createSequelizeInstance({ keepDefaultTimezone: true, pool: { min: 1, max: 1, handleDisconnects: true, idle: 5000 } });
       const cm = sequelize.connectionManager;
