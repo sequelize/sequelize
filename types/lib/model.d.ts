ab2e6bc9f8fb4f70e7932f4a6e40503a77f76ec6
Andre Azzolini <andre@azzolini.io>
2021-09-10 18:15:01 +0000

    Add hooks to InstanceDestroyOptions type

--- types/lib/model.d.ts
+++ types/lib/model.d.ts
@@ -939,7 +939,7 @@ export interface InstanceRestoreOptions extends Logging, Transactionable { }
 /**
  * Options used for Instance.destroy method
  */
-export interface InstanceDestroyOptions extends Logging, Transactionable {
+export interface InstanceDestroyOptions extends Logging, Transactionable, Hookable {
   /**
    * If set to true, paranoid models will actually be deleted
    */
