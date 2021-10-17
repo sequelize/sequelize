e4b8591d1ab1987b94b9f493f9b0b0ace653cd07
Dor Shay <dorshay6@gmail.com>
2021-10-17 13:15:54 +0300

    fix: add async hooks flag to bluebird to prevent conext issues

--- test/unit/promise.test.js
+++ test/unit/promise.test.js
@@ -5,12 +5,49 @@ const chai = require('chai'),
   Support = require('./support'),
   Sequelize = Support.Sequelize,
   Promise = Sequelize.Promise,
-  Bluebird = require('bluebird');
+  Bluebird = require('bluebird'),
+  AsyncHooks = require('async_hooks');
+
+
+const currentId = AsyncHooks.executionAsyncId;
+
+const tree = new Set();
+const hook = AsyncHooks.createHook({
+  init(asyncId, _, triggerId) {
+    if (tree.has(triggerId)) {
+      tree.add(asyncId);
+    }
+  }
+});
+
+
 
 describe('Promise', () => {
   it('should be an independent copy of bluebird library', () => {
     expect(Promise.prototype.then).to.be.a('function');
     expect(Promise).to.not.equal(Bluebird);
     expect(Promise.prototype).to.not.equal(Bluebird.prototype);
+  });
+
+  it('should keep async id across bluebird promises', () => {
+    // Based on the test from https://github.com/petkaantonov/bluebird/blob/808fdf8fce0cf4dbb1b95129607777a0cd53df36/test/mocha/async_hooks.js#L68
+    // Looks like there is no way to test if flag is on beside testing that it works
+    hook.enable();
+    tree.add(currentId());
+    const d1 = new Promise(resolve => {
+      setTimeout(() => {
+        setTimeout(resolve, 1);
+      }, 1);
+    });
+
+    return new Promise(resolve => {
+      resolve(Promise.map([d1, null, Promise.resolve(1), Promise.delay(1)], () => {
+        return currentId();
+      }).then(asyncIds => {
+        for (let i = 0; i < asyncIds.length; ++i) {
+          expect(asyncIds[i]).to.not.equal(true);
+        }
+      }));
+    });
   });
 });
