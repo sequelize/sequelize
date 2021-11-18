0312f8eac982b646842f89f56dc90f6c8f935c84
Mohamed El Mahallawy <mmahalwy@gmail.com>
2021-11-18 10:06:51 -0800

    fix: typing on creation within an association (#13678)
    * fix: typing on creation within an association
    
    * fix: tests
    
    Signed-off-by: Mohamed El Mahallawy <mmahalwy@gmail.com>

--- types/lib/associations/has-many.d.ts
+++ types/lib/associations/has-many.d.ts
@@ -209,8 +209,8 @@ export interface HasManyCreateAssociationMixinOptions extends CreateOptions<any>
  * @see https://sequelize.org/master/class/lib/associations/has-many.js~HasMany.html
  * @see Instance
  */
-export type HasManyCreateAssociationMixin<TModel> = (
-  values?: { [attribute: string]: unknown },
+export type HasManyCreateAssociationMixin<TModel extends Model> = (
+  values?: Model['_creationAttributes'],
   options?: HasManyCreateAssociationMixinOptions
 ) => Promise<TModel>;
 
