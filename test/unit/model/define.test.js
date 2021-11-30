1c60e2defccd02ffd82ba0737a082f256d2ded5a
bishwodahal <bishwodahal123@gmail.com>
2021-11-30 11:59:41 +0545

    trying to add getAttributes()

--- test/unit/model/define.test.js
+++ test/unit/model/define.test.js
@@ -1,125 +1,139 @@
-'use strict';
+"use strict";
 
-const chai = require('chai'),
+const chai = require("chai"),
   expect = chai.expect,
-  Support = require('../support'),
-  DataTypes = require('sequelize/lib/data-types'),
-  sinon = require('sinon'),
+  Support = require("../support"),
+  DataTypes = require("sequelize/lib/data-types"),
+  sinon = require("sinon"),
   current = Support.sequelize,
   dialect = Support.getTestDialect();
 
-describe(Support.getTestDialectTeaser('Model'), () => {
-  describe('define', () => {
-    it('should allow custom timestamps with underscored: true', () => {
-      const Model = current.define('User', {}, {
-        createdAt: 'createdAt',
-        updatedAt: 'updatedAt',
-        timestamps: true,
-        underscored: true
-      });
+describe(Support.getTestDialectTeaser("Model"), () => {
+  describe("define", () => {
+    it("should allow custom timestamps with underscored: true", () => {
+      const Model = current.define(
+        "User",
+        {},
+        {
+          createdAt: "createdAt",
+          updatedAt: "updatedAt",
+          timestamps: true,
+          underscored: true,
+        }
+      );
 
-      expect(Model.rawAttributes).to.haveOwnProperty('createdAt');
-      expect(Model.rawAttributes).to.haveOwnProperty('updatedAt');
+      expect(Model.getAttributes()).to.haveOwnProperty("createdAt");
+      expect(Model.getAttributes()).to.haveOwnProperty("updatedAt");
 
-      expect(Model._timestampAttributes.createdAt).to.equal('createdAt');
-      expect(Model._timestampAttributes.updatedAt).to.equal('updatedAt');
+      expect(Model._timestampAttributes.createdAt).to.equal("createdAt");
+      expect(Model._timestampAttributes.updatedAt).to.equal("updatedAt");
 
-      expect(Model.rawAttributes).not.to.have.property('created_at');
-      expect(Model.rawAttributes).not.to.have.property('updated_at');
+      expect(Model.getAttributes()).not.to.have.property("created_at");
+      expect(Model.getAttributes()).not.to.have.property("updated_at");
     });
 
-    it('should throw when id is added but not marked as PK', () => {
+    it("should throw when id is added but not marked as PK", () => {
       expect(() => {
-        current.define('foo', {
-          id: DataTypes.INTEGER
+        current.define("foo", {
+          id: DataTypes.INTEGER,
         });
-      }).to.throw("A column called 'id' was added to the attributes of 'foos' but not marked with 'primaryKey: true'");
+      }).to.throw(
+        "A column called 'id' was added to the attributes of 'foos' but not marked with 'primaryKey: true'"
+      );
 
       expect(() => {
-        current.define('bar', {
+        current.define("bar", {
           id: {
-            type: DataTypes.INTEGER
-          }
+            type: DataTypes.INTEGER,
+          },
         });
-      }).to.throw("A column called 'id' was added to the attributes of 'bars' but not marked with 'primaryKey: true'");
+      }).to.throw(
+        "A column called 'id' was added to the attributes of 'bars' but not marked with 'primaryKey: true'"
+      );
     });
 
     it('should defend against null or undefined "unique" attributes', () => {
       expect(() => {
-        current.define('baz', {
+        current.define("baz", {
           foo: {
             type: DataTypes.STRING,
-            unique: null
+            unique: null,
           },
           bar: {
             type: DataTypes.STRING,
-            unique: undefined
+            unique: undefined,
           },
           bop: {
-            type: DataTypes.DATE
-          }
+            type: DataTypes.DATE,
+          },
         });
       }).not.to.throw();
     });
 
-    it('should throw for unknown data type', () => {
+    it("should throw for unknown data type", () => {
       expect(() => {
-        current.define('bar', {
+        current.define("bar", {
           name: {
-            type: DataTypes.MY_UNKNOWN_TYPE
-          }
+            type: DataTypes.MY_UNKNOWN_TYPE,
+          },
         });
       }).to.throw('Unrecognized datatype for attribute "bar.name"');
     });
 
-    it('should throw for notNull validator without allowNull', () => {
+    it("should throw for notNull validator without allowNull", () => {
       expect(() => {
-        current.define('user', {
+        current.define("user", {
           name: {
             type: DataTypes.STRING,
             allowNull: true,
             validate: {
               notNull: {
-                msg: 'Please enter the name'
-              }
-            }
-          }
+                msg: "Please enter the name",
+              },
+            },
+          },
         });
-      }).to.throw('Invalid definition for "user.name", "notNull" validator is only allowed with "allowNull:false"');
+      }).to.throw(
+        'Invalid definition for "user.name", "notNull" validator is only allowed with "allowNull:false"'
+      );
 
       expect(() => {
-        current.define('part', {
+        current.define("part", {
           name: {
             type: DataTypes.STRING,
             validate: {
               notNull: {
-                msg: 'Please enter the part name'
-              }
-            }
-          }
+                msg: "Please enter the part name",
+              },
+            },
+          },
         });
-      }).to.throw('Invalid definition for "part.name", "notNull" validator is only allowed with "allowNull:false"');
+      }).to.throw(
+        'Invalid definition for "part.name", "notNull" validator is only allowed with "allowNull:false"'
+      );
     });
 
-    describe('datatype warnings', () => {
+    describe("datatype warnings", () => {
       beforeEach(() => {
-        sinon.spy(console, 'warn');
+        sinon.spy(console, "warn");
       });
 
       afterEach(() => {
         console.warn.restore();
       });
 
-      it('warn for unsupported INTEGER options', () => {
-        current.define('A', {
+      it("warn for unsupported INTEGER options", () => {
+        current.define("A", {
           age: {
-            type: DataTypes.TINYINT.UNSIGNED
-          }
+            type: DataTypes.TINYINT.UNSIGNED,
+          },
         });
 
-        if (['postgres', 'sqlite', 'mssql'].includes(dialect)) {
+        if (["postgres", "sqlite", "mssql"].includes(dialect)) {
           expect(true).to.equal(console.warn.calledOnce);
-          expect(console.warn.args[0][0]).to.contain("does not support 'TINYINT'");
+          expect(console.warn.args[0][0]).to.contain(
+            "does not support 'TINYINT'"
+          );
         } else {
           expect(false).to.equal(console.warn.calledOnce);
         }
--- test/unit/model/indexes.test.js
+++ test/unit/model/indexes.test.js
@@ -1,72 +1,88 @@
-'use strict';
+"use strict";
 
-const chai = require('chai'),
+const chai = require("chai"),
   expect = chai.expect,
-  Support = require('../support'),
+  Support = require("../support"),
   current = Support.sequelize,
-  DataTypes = require('sequelize/lib/data-types');
+  DataTypes = require("sequelize/lib/data-types");
 
-describe(Support.getTestDialectTeaser('Model'), () => {
-  describe('indexes', () => {
-    it('should automatically set a gin index for JSONB indexes', () => {
-      const Model = current.define('event', {
+describe(Support.getTestDialectTeaser("Model"), () => {
+  describe("indexes", () => {
+    it("should automatically set a gin index for JSONB indexes", () => {
+      const Model = current.define("event", {
         eventData: {
           type: DataTypes.JSONB,
           index: true,
-          field: 'data'
-        }
+          field: "data",
+        },
       });
 
-      expect(Model.rawAttributes.eventData.index).not.to.equal(true);
+      expect(Model.getAttributes().eventData.index).not.to.equal(true);
       expect(Model._indexes.length).to.equal(1);
-      expect(Model._indexes[0].fields).to.eql(['data']);
-      expect(Model._indexes[0].using).to.equal('gin');
+      expect(Model._indexes[0].fields).to.eql(["data"]);
+      expect(Model._indexes[0].using).to.equal("gin");
     });
 
-    it('should set the unique property when type is unique', () => {
-      const Model = current.define('m', {}, {
-        indexes: [
-          {
-            type: 'unique',
-            fields: ['name']
-          },
-          {
-            type: 'UNIQUE',
-            fields: ['name']
-          }
-        ]
-      });
+    it("should set the unique property when type is unique", () => {
+      const Model = current.define(
+        "m",
+        {},
+        {
+          indexes: [
+            {
+              type: "unique",
+              fields: ["name"],
+            },
+            {
+              type: "UNIQUE",
+              fields: ["name"],
+            },
+          ],
+        }
+      );
 
       expect(Model._indexes[0].unique).to.eql(true);
       expect(Model._indexes[1].unique).to.eql(true);
     });
 
-    it('should not set rawAttributes when indexes are defined via options', () => {
-      const User = current.define('User', {
-        username: DataTypes.STRING
-      }, {
-        indexes: [{
-          unique: true,
-          fields: ['username']
-        }]
-      });
+    it("should not set getAttributes() when indexes are defined via options", () => {
+      const User = current.define(
+        "User",
+        {
+          username: DataTypes.STRING,
+        },
+        {
+          indexes: [
+            {
+              unique: true,
+              fields: ["username"],
+            },
+          ],
+        }
+      );
 
-      expect(User.rawAttributes.username.unique).to.be.undefined;
+      expect(User.getAttributes().username.unique).to.be.undefined;
     });
 
-    it('should not set rawAttributes when composite unique indexes are defined via options', () => {
-      const User = current.define('User', {
-        name: DataTypes.STRING,
-        address: DataTypes.STRING
-      }, {
-        indexes: [{
-          unique: 'users_name_address',
-          fields: ['name', 'address']
-        }]
-      });
+    it("should not set getAttributes() when composite unique indexes are defined via options", () => {
+      const User = current.define(
+        "User",
+        {
+          name: DataTypes.STRING,
+          address: DataTypes.STRING,
+        },
+        {
+          indexes: [
+            {
+              unique: "users_name_address",
+              fields: ["name", "address"],
+            },
+          ],
+        }
+      );
 
-      expect(User.rawAttributes.name.unique).to.be.undefined;
-      expect(User.rawAttributes.address.unique).to.be.undefined;
+      expect(User.getAttributes().name.unique).to.be.undefined;
+      expect(User.getAttributes().address.unique).to.be.undefined;
     });
   });
 });
