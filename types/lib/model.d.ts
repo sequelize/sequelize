987d177ff4be2f17d6bc63abe6bb2e707375548b
George Zhao <zorji@users.noreply.github.com>
2021-12-03 12:49:28 +1100

    fix(model.d): fix findAndCountAll.count type
    `findAndCountAll` should only return `count` as an array when the `group` option is provided.
    
    This means in that case, `options` is no longer optional because when `options` is not provided, it's never a group count.
    I also rearrange `findAndCountAll` order to ensure the single count overload is applied first.

--- types/lib/model.d.ts
+++ types/lib/model.d.ts
@@ -1945,12 +1945,12 @@ export abstract class Model<TModelAttributes extends {} = any, TCreationAttribut
    */
   public static findAndCountAll<M extends Model>(
     this: ModelStatic<M>,
-    options?: FindAndCountOptions<M['_attributes']> & { group: GroupOption }
-  ): Promise<{ rows: M[]; count: number[] }>;
-  public static findAndCountAll<M extends Model>(
-    this: ModelStatic<M>,
     options?: FindAndCountOptions<M['_attributes']>
   ): Promise<{ rows: M[]; count: number }>;
+  public static findAndCountAll<M extends Model>(
+    this: ModelStatic<M>,
+    options: FindAndCountOptions<M['_attributes']> & { group: GroupOption }
+  ): Promise<{ rows: M[]; count: number[] }>;
 
   /**
    * Find the maximum value of field
