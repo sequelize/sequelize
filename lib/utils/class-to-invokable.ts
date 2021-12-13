e0876af1e510000df756eb2db7c7e885d4d6455a
AllAwesome497 <47748690+AllAwesome497@users.noreply.github.com>
2021-12-13 07:44:17 -0600

    refactor(class-to-invokable): make typing a little more accurate (#13766)
    * Refactor classToInvokable types
    
    refactor(class-to-invokable): make typing a little more accurate
    
    Updates types for the util `classToInvokable` function to be a bit more accurate.   Also removes the `get` trap since the handler was redundant.
    
    * Update class-to-invokable.ts

--- lib/utils/class-to-invokable.ts
+++ lib/utils/class-to-invokable.ts
@@ -1,4 +1,10 @@
-type Proxify = typeof Proxy;
+/**
+ * Utility type for a class which can be called in addion to being used as a constructor.
+ */
+interface Invokeable<Args extends Array<any>, Instance> {
+  (...args: Args): Instance;
+  new (...args: Args): Instance;
+}
 
 /**
  * Wraps a constructor to not need the `new` keyword using a proxy.
@@ -8,16 +14,15 @@ type Proxify = typeof Proxy;
  * @returns {Proxy} Wrapped class instance.
  * @private
  */
-export function classToInvokable(Class: any): Proxify {
-  return new Proxy(Class, {
-    apply(Target, thisArg, args) {
-      return new Target(...args);
+export function classToInvokable<Args extends Array<any>, Instance extends object>(
+  Class: new (...args: Args) => Instance,
+): Invokeable<Args, Instance> {
+  return new Proxy<Invokeable<Args, Instance>>(Class as any, {
+    apply(_target, _thisArg, args: Args) {
+      return new Class(...args);
     },
-    construct(Target, args) {
-      return new Target(...args);
+    construct(_target, args: Args) {
+      return new Class(...args);
     },
-    get(target, p) {
-      return target[p];
-    }
   });
 }
