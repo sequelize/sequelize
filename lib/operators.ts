b97f93469bee6c3429d8cab7d0cb6418cbf05892
Fauzan <fncolon@pm.me>
2021-12-18 02:01:05 +0700

    refactor(operators): convert operators to typescript (#13731)
    * refactor(operators): symbol keys prefix
    
    To avoid name clashes with your global symbol keys and other (library code) global symbols, it might be a good idea to prefix your symbols
    
    * feat: add operators.ts
    
    * feat: add operators.ts
    
    * Not an object
    
    * Change to string
    
    * refactor(operators): convert to typescript
    
    * fix(operators): revert values back to original values
    
    Co-authored-by: Sascha Depold <sdepold@users.noreply.github.com>
    Co-authored-by: Sascha Depold <sascha@depold.com>

--- lib/operators.ts
+++ lib/operators.ts
@@ -1,7 +1,4 @@
-/**
- * object that holds all operator symbols
- */
-declare const Op: {
+interface OpTypes {
   /**
    * Operator -|- (PG range is adjacent to operator)
    *
@@ -479,6 +476,49 @@ declare const Op: {
    * ```
    */
   readonly values: unique symbol;
-};
+}
+
+const Op: OpTypes = {
+  eq: Symbol.for('eq'),
+  ne: Symbol.for('ne'),
+  gte: Symbol.for('gte'),
+  gt: Symbol.for('gt'),
+  lte: Symbol.for('lte'),
+  lt: Symbol.for('lt'),
+  not: Symbol.for('not'),
+  is: Symbol.for('is'),
+  in: Symbol.for('in'),
+  notIn: Symbol.for('notIn'),
+  like: Symbol.for('like'),
+  notLike: Symbol.for('notLike'),
+  iLike: Symbol.for('iLike'),
+  notILike: Symbol.for('notILike'),
+  startsWith: Symbol.for('startsWith'),
+  endsWith: Symbol.for('endsWith'),
+  substring: Symbol.for('substring'),
+  regexp: Symbol.for('regexp'),
+  notRegexp: Symbol.for('notRegexp'),
+  iRegexp: Symbol.for('iRegexp'),
+  notIRegexp: Symbol.for('notIRegexp'),
+  between: Symbol.for('between'),
+  notBetween: Symbol.for('notBetween'),
+  overlap: Symbol.for('overlap'),
+  contains: Symbol.for('contains'),
+  contained: Symbol.for('contained'),
+  adjacent: Symbol.for('adjacent'),
+  strictLeft: Symbol.for('strictLeft'),
+  strictRight: Symbol.for('strictRight'),
+  noExtendRight: Symbol.for('noExtendRight'),
+  noExtendLeft: Symbol.for('noExtendLeft'),
+  and: Symbol.for('and'),
+  or: Symbol.for('or'),
+  any: Symbol.for('any'),
+  all: Symbol.for('all'),
+  values: Symbol.for('values'),
+  col: Symbol.for('col'),
+  placeholder: Symbol.for('placeholder'),
+  join: Symbol.for('join'),
+  match: Symbol.for('match')
+} as OpTypes;
 
 export = Op;
