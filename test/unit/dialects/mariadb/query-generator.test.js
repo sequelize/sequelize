5e932434ff14eb9b1fd83f344106900e4aabe852
Zoé <zoe@ephys.dev>
2022-01-11 19:24:59 +0100

    refactor: migrate Transaction, Enums, & AbstractDialect (#13922)

--- test/unit/dialects/mariadb/query-generator.test.js
+++ test/unit/dialects/mariadb/query-generator.test.js
@@ -7,8 +7,8 @@ const Support = require('../../support');
 
 const dialect = Support.getTestDialect();
 const _ = require('lodash');
-const Op = require('sequelize/lib/operators');
-const IndexHints = require('sequelize/lib/index-hints');
+const { Op } = require('sequelize/lib/operators');
+const { IndexHints } = require('sequelize/lib/index-hints');
 const QueryGenerator = require('sequelize/lib/dialects/mariadb/query-generator');
 
 if (dialect === 'mariadb') {
