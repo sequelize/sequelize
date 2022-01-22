a9cc26fd027809b923d1a69f9e812e413832ca15
ephys <zoe@ephys.dev>
2022-01-22 13:26:51 +0100

    test: gitignore ts test until 4.4

--- types/test/findOne.ts
+++ types/test/findOne.ts
@@ -16,4 +16,9 @@ User.findOne({ where: { '$blah$': 'blah2' } });
 //  note: this *must* be a ts-ignore, as it works in ts >= 4.4
 // @ts-ignore
 User.findOne({ where: { '$include1.includeAttr$': 'blah2' } });
+// $nested.nested.syntax$ is valid
+// TODO [2022-05-26]: Remove this ts-ignore once we drop support for TS < 4.4
+// TypeScript < 4.4 does not support using a Template Literal Type as a key.
+//  note: this *must* be a ts-ignore, as it works in ts >= 4.4
+// @ts-ignore
 User.findOne({ where: { '$include1.$include2.includeAttr$': 'blah2' } });
