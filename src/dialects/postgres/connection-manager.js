56715da2a423c43ef81095edded09f612c2210fa
Ivan Kuplevich <ikuplevich97@gmail.com>
2022-02-15 20:02:14 +0300

    feat(postgres): add connectionTimeoutMillis

--- src/dialects/postgres/connection-manager.js
+++ src/dialects/postgres/connection-manager.js
@@ -117,6 +117,8 @@ class ConnectionManager extends AbstractConnectionManager {
           'statement_timeout',
           // Times out queries after a set time in milliseconds in client end, query would be still running in database end.
           'query_timeout',
+          // Number of milliseconds to wait for connection, default is no timeout.
+          'connectionTimeoutMillis',
           // Terminate any session with an open transaction that has been idle for longer than the specified duration in milliseconds. Added in pg v7.17.0 only supported in postgres >= 10
           'idle_in_transaction_session_timeout',
           // Postgres allows additional session variables to be configured in the connection string in the `options` param.
