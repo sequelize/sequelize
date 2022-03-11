5f0802d88668067092f1c807fbd8330cf7547770
NILABJA BHATTACHARYA <nilabja.bhattacharya@oracle.com>
2022-03-11 10:40:47 +0530

    Fix: added files to setup oracle in docker

---
+++ dev/oracle/21c/stop.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
+
+
+docker-compose -p oraclexedb21c down --remove-orphans
+
+echo "Local Oracle DB - 21c instance stopped (if it was running)."
