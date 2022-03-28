803a79bb80ec6d0f5c1119760c11a7a02c7859cc
ephys <zoe@ephys.dev>
2022-03-28 17:14:11 +0200

    docs: revert accidental comment removal

--- test/unit/esm-named-exports.test.js
+++ test/unit/esm-named-exports.test.js
@@ -130,6 +130,10 @@ Either add these exports to "index.mjs" (and "types/index.d.ts"), or mark them a
       }
 
       expect(cjsKeys).to.include(key, `esm entry point is declaring export ${JSON.stringify(key)} that is missing from CJS`);
+
+      // exported values need to be the same instances
+      //  if we want to avoid major bugs:
+      //  https://github.com/sequelize/sequelize/pull/13689#issuecomment-987412233
       expect(sequelizeEsm[key]).to.eq(sequelizeCjs[key]);
     }
   });
