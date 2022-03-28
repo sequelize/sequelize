57ab7171b98b2f57054889ec17b7797d45eb215e
ephys <zoe@ephys.dev>
2022-03-28 15:40:13 +0200

    test: add test to prevent duplicate associations aliases

--- test/unit/associations/association.test.js
+++ test/unit/associations/association.test.js
@@ -14,8 +14,32 @@ describe(Support.getTestDialectTeaser('belongsTo'), () => {
     const Task = current.define('Task');
 
     User.belongsTo(Task, { as: 'task' });
-    const errorFunction = User.belongsTo.bind(User, Task, { as: 'task' });
-    const errorMessage = 'You have defined two associations with the same name "task" on the model "User". Use another alias using the "as" parameter.';
-    expect(errorFunction).to.throw(AssociationError, errorMessage);
+
+    expect(() => {
+      User.belongsTo(Task, { as: 'task' });
+    }).to.throw(AssociationError, 'You have defined two associations with the same name "task" on the model "User". Use another alias using the "as" parameter.');
+  });
+
+  it('should throw an AssociationError when two associations have the same alias (one inferred)', () => {
+    const User = current.define('User');
+    const Task = current.define('Task');
+
+    const association = User.belongsTo(Task);
+    expect(association.as).to.eq('Task');
+
+    expect(() => {
+      User.belongsTo(Task, { as: 'Task' });
+    }).to.throw(AssociationError, 'You have defined two associations with the same name "Task" on the model "User". Use another alias using the "as" parameter.');
+  });
+
+  it('should throw an AssociationError when two associations have the same alias (both inferred)', () => {
+    const User = current.define('User');
+    const Task = current.define('Task');
+
+    User.belongsTo(Task);
+
+    expect(() => {
+      User.belongsTo(Task);
+    }).to.throw(AssociationError, 'You have defined two associations with the same name "Task" on the model "User". Use another alias using the "as" parameter.');
   });
 });
