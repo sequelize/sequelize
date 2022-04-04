7abd9d3f57fb28c4efadab52b09275a4d9af4b16
Fauzan "Evan Edrok <fncolon@pm.me>
2022-04-05 03:17:22 +0700

    chore(generic): add more falsy values
    falsy/falsey means internal toBoolean returns false for the values
    
    0.0 | -0.0 : any zeroish number return false
    0x0 | -0x0 : represent hex number of 0
    NaN : Not a Number
    ---
    **new falsy values on ES2020**
    0x0n | -0x0n : the bigint of zero and negative zero, alongside 0n and -0n
    ---

--- src/generic/falsy.ts
+++ src/generic/falsy.ts
@@ -1,1 +1,1 @@
-export type Falsy = false | 0 | -0 | 0n | '' | null | undefined;
+export type Falsy = false | 0 | -0 | 0n | -0n | '' | null | undefined | NaN | 0x0n | -0x0n | 0.0 | -0.0 | 0x0 | -0x0;
