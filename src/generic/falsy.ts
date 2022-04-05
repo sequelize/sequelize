0ea6e5fcd5e290b0a36fc33a98992ed5d720b2b5
Fauzan "Evan Edrok" 0x <fncolon@pm.me>
2022-04-06 05:36:22 +0700

    fix: try to fix NaN returning with new Number Constructor

--- src/generic/falsy.ts
+++ src/generic/falsy.ts
@@ -1,1 +1,6 @@
-export type Falsy = false | 0 | -0 | 0n | '' | null | undefined | NaN;
+// fix NaN returning issue https://github.com/Microsoft/TypeScript/issues/28682
+interface NumberConstructor {
+	isNaN(number: unknown): number is NaN;
+}
+
+export type Falsy = false | 0 | -0 | 0n | '' | null | undefined | NumberConstructor.isNaN;
