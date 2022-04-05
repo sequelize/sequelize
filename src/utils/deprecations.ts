bfab15dd24cdb15672fa1f794747aee11e53a2bc
Zoé <zoe@ephys.dev>
2022-04-05 10:12:59 +0200

    feat: use native `Error#cause` in Sequelize errors (#14318)

--- src/utils/deprecations.ts
+++ src/utils/deprecations.ts
@@ -8,3 +8,4 @@ export const noStringOperators = deprecate(noop, 'String based operators are dep
 export const noBoolOperatorAliases = deprecate(noop, 'A boolean value was passed to options.operatorsAliases. This is a no-op with v5 and should be removed.', 'SEQUELIZE0004');
 export const noDoubleNestedGroup = deprecate(noop, 'Passing a double nested nested array to `group` is unsupported and will be removed in v6.', 'SEQUELIZE0005');
 export const unsupportedEngine = deprecate(noop, 'This database engine version is not supported, please update your database server. More information https://github.com/sequelize/sequelize/blob/main/ENGINE.md', 'SEQUELIZE0006');
+export const useErrorCause = deprecate(noop, 'The "parent" and "original" properties in Sequelize errors have been replaced with the native "cause" property. Use that one instead.', 'SEQUELIZE0007');
