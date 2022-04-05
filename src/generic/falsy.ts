918cf51a832dac09ddba11be4de2983e27be9674
Fauzan "Evan Edrok" 0x <fncolon@pm.me>
2022-04-06 05:52:12 +0700

    fix: follow typescript 4.6

--- src/generic/falsy.ts
+++ src/generic/falsy.ts
@@ -2,4 +2,4 @@ interface NumberConstructor {
   isNaN(number: unknown): number is Number.NaN;
 }
 
-export type Falsy = false | 0 | -0 | 0n | '' | null | undefined | NumberConstructor.isNaN;
+export type Falsy = false | 0 | -0 | 0n | '' | null | undefined | NumberConstructor["isNaN"];
