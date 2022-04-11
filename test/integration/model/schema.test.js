bbb0d0228ee586b0372521110c99c157e63a3f37
Zoé <zoe@ephys.dev>
2022-04-11 14:00:02 +0200

    feat: simplify withSchema API (#14359)
    * feat: simplify withSchema API
    
    * feat: add check to ensure people migrate properly
    
    * fix: normalize schema to empty string
    
    * test: fixb roken test

--- test/integration/model/schema.test.js
+++ test/integration/model/schema.test.js
@@ -64,7 +64,7 @@ describe(Support.getTestDialectTeaser('Model'), () => {
 
       describe('Add data via model.create, retrieve via model.findOne', () => {
         it('should be able to sync model without schema option', function () {
-          expect(this.RestaurantOne._schema).to.be.null;
+          expect(this.RestaurantOne._schema).to.eq('');
           expect(this.RestaurantTwo._schema).to.equal(SCHEMA_TWO);
         });
 
