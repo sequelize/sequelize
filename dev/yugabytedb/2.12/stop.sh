6e0638d84a23ec91f1702300d6e825c6aeb81e2c
Priyanshi Gupta <gupta.priyanshi.yb@gmail.com>
2022-04-13 09:06:14 +0000

    feat(dialect): dev environment set up for yugabytedb dialect

--- dev/yugabytedb/2.12/stop.sh
+++ dev/yugabytedb/2.12/stop.sh
@@ -3,7 +3,6 @@ set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
 
 
-docker-compose -p sequelize-yugabyte down --remove-orphans
-docker-compose -p sequelize-yugabyte up -d
+docker-compose -p sequelize-yugabytedb-2.4 down --remove-orphans
 
-echo "Local Yugabyte instance is ready for Sequelize tests."
+echo "Local Yugabyte instance stopped (if it was running)."
