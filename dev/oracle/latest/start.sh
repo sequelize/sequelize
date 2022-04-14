60c2745554938562215f9320a901affe10e26a94
NILABJA BHATTACHARYA <nilabja.bhattacharya@oracle.com>
2022-04-14 14:28:40 +0530

    fix: changes to start.sh

--- dev/oracle/latest/start.sh
+++ dev/oracle/latest/start.sh
@@ -15,25 +15,25 @@ docker-compose -p oraclexedb up -d
 docker cp privileges.sql oraclexedb:/opt/oracle/. 
 
 # Granting all the needed privileges to sequelizetest user
-docker exec -it oraclexedb sqlplus system/password@XEPDB1 @privileges.sql
+docker exec -t oraclexedb sqlplus system/password@XEPDB1 @privileges.sql
 
 # Setting up Oracle instant client for oracledb
-if [ ! -d  ~/Downloads/oracle ] && [ $(uname) == 'Linux' ]
+if [ ! -d  ~/oracle ] && [ $(uname) == 'Linux' ]
 then 
-    mkdir ~/Downloads/oracle && 
+    mkdir ~/oracle && 
     wget https://download.oracle.com/otn_software/linux/instantclient/215000/instantclient-basic-linux.x64-21.5.0.0.0dbru.zip --no-check-certificate && 
-    unzip instantclient-basic-linux.x64-21.5.0.0.0dbru.zip -d ~/Downloads/oracle/ &&
+    unzip instantclient-basic-linux.x64-21.5.0.0.0dbru.zip -d ~/oracle/ &&
     rm instantclient-basic-linux.x64-21.5.0.0.0dbru.zip &&
-    mv ~/Downloads/oracle/instantclient_21_5 ~/Downloads/oracle/instantclient
+    mv ~/oracle/instantclient_21_5 ~/oracle/instantclient
 
     echo "Local Oracle instant client has been setup!"
 elif [ ! -d  ~/Downloads/oracle ] && [ $(uname) == 'Darwin' ]
 then
-    mkdir ~/Downloads/oracle && 
+    mkdir ~/oracle && 
     wget https://download.oracle.com/otn_software/mac/instantclient/198000/instantclient-basic-macos.x64-19.8.0.0.0dbru.zip --no-check-certificate && 
-    unzip instantclient-basic-macos.x64-19.8.0.0.0dbru.zip -d ~/Downloads/oracle/ &&
+    unzip instantclient-basic-macos.x64-19.8.0.0.0dbru.zip -d ~/oracle/ &&
     rm instantclient-basic-macos.x64-19.8.0.0.0dbru.zip &&
-    mv ~/Downloads/oracle/instantclient_19_8 ~/Downloads/oracle/instantclient
+    mv ~/oracle/instantclient_19_8 ~/oracle/instantclient
 
     echo "Local Oracle instant client has been setup!"
 fi
