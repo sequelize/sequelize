65c052d4721c7133b7316db604a3ede91ed5bb25
NILABJA BHATTACHARYA <nilabja.bhattacharya@oracle.com>
2022-04-14 11:56:54 +0530

    feat: github workflow

---
+++ dev/oracle/latest/start.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
+
+# Remove an existing Oracle DB docker image
+docker-compose -p oraclexedb down --remove-orphans
+
+# Bring up new Oracle DB docker image
+docker-compose -p oraclexedb up -d
+
+# Wait until Oracle DB is set up and docker state is healthy
+./wait-until-healthy.sh oraclexedb
+
+# Moving privileges.sql to docker container
+docker cp privileges.sql oraclexedb:/opt/oracle/. 
+
+# Granting all the needed privileges to sequelizetest user
+docker exec -it oraclexedb sqlplus system/password@XEPDB1 @privileges.sql
+
+# Setting up Oracle instant client for oracledb
+if [ ! -d  ~/Downloads/oracle ] && [ $(uname) == 'Linux' ]
+then 
+    mkdir ~/Downloads/oracle && 
+    wget https://download.oracle.com/otn_software/linux/instantclient/215000/instantclient-basic-linux.x64-21.5.0.0.0dbru.zip --no-check-certificate && 
+    unzip instantclient-basic-linux.x64-21.5.0.0.0dbru.zip -d ~/Downloads/oracle/ &&
+    rm instantclient-basic-linux.x64-21.5.0.0.0dbru.zip &&
+    mv ~/Downloads/oracle/instantclient_21_5 ~/Downloads/oracle/instantclient
+
+    echo "Local Oracle instant client has been setup!"
+elif [ ! -d  ~/Downloads/oracle ] && [ $(uname) == 'Darwin' ]
+then
+    mkdir ~/Downloads/oracle && 
+    wget https://download.oracle.com/otn_software/mac/instantclient/198000/instantclient-basic-macos.x64-19.8.0.0.0dbru.zip --no-check-certificate && 
+    unzip instantclient-basic-macos.x64-19.8.0.0.0dbru.zip -d ~/Downloads/oracle/ &&
+    rm instantclient-basic-macos.x64-19.8.0.0.0dbru.zip &&
+    mv ~/Downloads/oracle/instantclient_19_8 ~/Downloads/oracle/instantclient
+
+    echo "Local Oracle instant client has been setup!"
+fi
+
+echo "Local Oracle DB is ready for use!"
