db1a5e90617c675ca3c730bff2562f4f36988836
Andreas Lind <andreaslindpetersen@gmail.com>
2022-05-13 22:33:26 +0200

    Update src/model.d.ts
    Co-authored-by: Zoé <zoe@ephys.dev>

--- src/model.d.ts
+++ src/model.d.ts
@@ -1774,7 +1774,7 @@ export type ModelAttributes<M extends Model = Model, TAttributes = any> = {
 /**
  * Possible types for primary keys
  */
-export type Identifier = number | BigInt | string | Buffer;
+export type Identifier = number | bigint | string | Buffer;
 
 /**
  * Options for model definition.
