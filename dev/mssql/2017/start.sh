d33a0153e2fb9fb0186be9e9b58ad4ac4a82b300
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2022-06-07 10:25:59 +0000

    build(mssql): add scripts mssql 2017

---
+++ dev/mssql/2017/start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
+
+
+docker-compose -p sequelize-mssql-2017 down --remove-orphans
+docker-compose -p sequelize-mssql-2017 up -d
+
+./../../wait-until-healthy.sh sequelize-mssql-2017
+
+docker exec sequelize-mssql-2017 \
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Password12!" -Q "CREATE DATABASE sequelize_test; ALTER DATABASE sequelize_test SET READ_COMMITTED_SNAPSHOT ON;"
+
+DIALECT=mssql node check.js
+
+echo "Local MSSQL-2017 instance is ready for Sequelize tests."
