fa776a8840826cd43533117af28467ebb0b3ac9f
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2022-06-08 07:50:21 +0000

    refactor: rename to oldest and latest

--- dev/mssql/latest/start.sh
+++ dev/mssql/latest/start.sh
@@ -3,14 +3,14 @@ set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
 
 
-docker-compose -p sequelize-mssql-2022 down --remove-orphans
-docker-compose -p sequelize-mssql-2022 up -d
+docker-compose -p sequelize-mssql-latest down --remove-orphans
+docker-compose -p sequelize-mssql-latest up -d
 
-./../../wait-until-healthy.sh sequelize-mssql-2022
+./../../wait-until-healthy.sh sequelize-mssql-latest
 
-docker exec sequelize-mssql-2022 \
+docker exec sequelize-mssql-latest \
   /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Password12!" -Q "CREATE DATABASE sequelize_test; ALTER DATABASE sequelize_test SET READ_COMMITTED_SNAPSHOT ON;"
 
 DIALECT=mssql node check.js
 
-echo "Local MSSQL-2022 instance is ready for Sequelize tests."
+echo "Local MSSQL-latest instance is ready for Sequelize tests."
--- dev/mssql/latest/stop.sh
+++ dev/mssql/latest/stop.sh
@@ -3,6 +3,6 @@ set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
 
 
-docker-compose -p sequelize-mssql-2022 down --remove-orphans
+docker-compose -p sequelize-mssql-latest down --remove-orphans
 
-echo "Local MSSQL-2022 instance stopped (if it was running)."
+echo "Local MSSQL-latest instance stopped (if it was running)."
