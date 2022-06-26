06f4429b5e99d797ffdcc38ae44cfe1bd1e2fa92
Zoé <zoe@ephys.dev>
2022-06-26 20:11:15 +0200

    fix(types): fix `findAll()` being treated as raw (#14668)
    * fix(types): fix `findAll()` being treated as raw
    
    * test: fix test

--- test/types/find-all.ts
+++ test/types/find-all.ts
@@ -4,8 +4,15 @@ import { expectTypeOf } from 'expect-type';
 import { User } from './models/user';
 
 (async () => {
-  const users = await User.findAll({ raw: false });
-  expectTypeOf(users).toEqualTypeOf<User[]>();
+  {
+    const users = await User.findAll();
+    expectTypeOf(users).toEqualTypeOf<User[]>();
+  }
+
+  {
+    const users = await User.findAll({ raw: false });
+    expectTypeOf(users).toEqualTypeOf<User[]>();
+  }
 
   const rawUsers = await User.findAll({ raw: true });
   expectTypeOf(rawUsers).toEqualTypeOf<Array<Attributes<User>>>();
