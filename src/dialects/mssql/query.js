6e9ac7d656eba7e705638c5a4fe582ca399a4d47
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2022-07-22 15:08:31 +0000

    fix(mssql): match aggregate errors

--- src/dialects/mssql/query.js
+++ src/dialects/mssql/query.js
@@ -351,6 +351,18 @@ export class MsSqlQuery extends AbstractQuery {
       });
     }
 
+    for (const error of err.errors) {
+      match = error.message.match(/Could not create constraint or index. See previous errors./);
+      if (match && match.length > 0) {
+        return new sequelizeErrors.ForeignKeyConstraintError({
+          fields: null,
+          index: match[1],
+          cause: error,
+          stack: errStack,
+        });
+      }
+    }
+
     match = err.message.match(/Could not drop constraint. See previous errors./);
     if (match && match.length > 0) {
       let constraint = err.sql.match(/(?:constraint|index) \[(.+?)]/i);
@@ -365,6 +377,24 @@ export class MsSqlQuery extends AbstractQuery {
         cause: err,
         stack: errStack,
       });
+    }
+
+    for (const error of err.errors) {
+      match = error.message.match(/Could not drop constraint. See previous errors./);
+      if (match && match.length > 0) {
+        let constraint = err.sql.match(/(?:constraint|index) \[(.+?)]/i);
+        constraint = constraint ? constraint[1] : undefined;
+        let table = err.sql.match(/table \[(.+?)]/i);
+        table = table ? table[1] : undefined;
+
+        return new sequelizeErrors.UnknownConstraintError({
+          message: match[1],
+          constraint,
+          table,
+          cause: error,
+          stack: errStack,
+        });
+      }
     }
 
     return new sequelizeErrors.DatabaseError(err, { stack: errStack });
