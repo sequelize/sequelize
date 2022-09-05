c7ed0495471358fce8a0e20785117ccf0e25b6f3
lesion <lesion@autistici.org>
2022-09-05 17:00:12 +0200

    fix(mariadb): comment that MariaDB 10.5.2 already results in JSON format

--- src/dialects/mariadb/query.js
+++ src/dialects/mariadb/query.js
@@ -185,6 +185,8 @@ class Query extends AbstractQuery {
       if (modelField.type instanceof DataTypes.JSON) {
         // Value is returned as String, not JSON
         rows = rows.map(row => {
+          // JSON fields for MariaDB server 10.5.2+ already results in JSON format, skip JSON.parse
+          // this is due to this https://jira.mariadb.org/browse/MDEV-17832 and how mysql2 connector interacts with MariaDB and JSON fields
           if (row[modelField.fieldName] && typeof row[modelField.fieldName] === 'string' && !this.connection.info.hasMinVersion(10, 5, 2)) {
             row[modelField.fieldName] = JSON.parse(row[modelField.fieldName]);
           }
