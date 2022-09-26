2e52b9c7900fcbefe4682c17cd97bc75512f6127
Andrii Kostenko <andrey@kostenko.name>
2022-09-26 12:20:26 +0000

    keep the type of incoming identifier when producing autoIncrement values

--- src/dialects/mysql/query.js
+++ src/dialects/mysql/query.js
@@ -106,10 +106,10 @@ export class MySqlQuery extends AbstractQuery {
           && this.model.autoIncrementAttribute === this.model.primaryKeyAttribute
           && this.model.rawAttributes[this.model.primaryKeyAttribute]
         ) {
-          const startId = BigInt(data[this.getInsertIdField()]);
+          const startId = data[this.getInsertIdField()];
           result = [];
-          for (let i = startId; i < startId + BigInt(data.affectedRows); i = i + 1n) {
-            result.push({ [this.model.rawAttributes[this.model.primaryKeyAttribute].field]: i > Number.MAX_SAFE_INTEGER ? i.toString() : Number(i) });
+          for (let i = BigInt(startId); i < BigInt(startId) + BigInt(data.affectedRows); i = i + 1n) {
+            result.push({ [this.model.rawAttributes[this.model.primaryKeyAttribute].field]: typeof startId === 'string' ? i.toString() : Number(i) });
           }
         } else {
           result = data[this.getInsertIdField()];
