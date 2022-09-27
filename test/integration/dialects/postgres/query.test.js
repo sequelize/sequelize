7203b6626ed38c06f91f09f73571fb7df56fe348
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2022-09-27 15:27:11 +0200

    fix(postgres): add custom order direction to subQuery ordering with minified alias (#15056)

--- test/integration/dialects/postgres/query.test.js
+++ test/integration/dialects/postgres/query.test.js
@@ -124,22 +124,35 @@ if (dialect.match(/^postgres/)) {
       await Foo.create({ name: 'record1' });
       await Foo.create({ name: 'record2' });
 
-      const thisWorks = (await Foo.findAll({
+      const baseTest = (await Foo.findAll({
         subQuery: false,
         order: sequelizeMinifyAliases.literal('"Foo".my_name')
       })).map(f => f.name);
-      expect(thisWorks[0]).to.equal('record1');
+      expect(baseTest[0]).to.equal('record1');
 
-      const thisShouldAlsoWork = (await Foo.findAll({
+      const orderByAscSubquery = (await Foo.findAll({
         attributes: {
           include: [
             [sequelizeMinifyAliases.literal('"Foo".my_name'), 'customAttribute']
           ]
         },
         subQuery: true,
-        order: ['customAttribute']
+        order: [['customAttribute']],
+        limit: 1
       })).map(f => f.name);
-      expect(thisShouldAlsoWork[0]).to.equal('record1');
+      expect(orderByAscSubquery[0]).to.equal('record1');
+
+      const orderByDescSubquery = (await Foo.findAll({
+        attributes: {
+          include: [
+            [sequelizeMinifyAliases.literal('"Foo".my_name'), 'customAttribute']
+          ]
+        },
+        subQuery: true,
+        order: [['customAttribute', 'DESC']],
+        limit: 1
+      })).map(f => f.name);
+      expect(orderByDescSubquery[0]).to.equal('record2');
     });
 
     it('returns the minified aliased attributes', async () => {
