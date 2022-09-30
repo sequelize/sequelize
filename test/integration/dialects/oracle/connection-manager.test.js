e7c05b6204f37ea921c165d02db07a9859c50c78
Hasan Jamil <jarvis@Hasans-MacBook-Pro.local>
2022-09-30 16:54:10 +0530

    fix: added newline

--- test/integration/dialects/oracle/connection-manager.test.js
+++ test/integration/dialects/oracle/connection-manager.test.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const config = require('../../../config/config.js').oracle;
-const Support = require('../../../support');
+const Support = require('../../support');
 const dialect = Support.getTestDialect();
 
 if (dialect === 'oracle') {
   describe('[Oracle Specific] Connection Manager', () => {
     it('connect string authentication using connection Descriptor', async () => {
       const connDesc = `(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=${config.host})(PORT=${config.port}))(CONNECT_DATA=(SERVICE_NAME=${config.database})))`;
-      const sequelize = await Support.getSequelizeInstance(config.database, config.username, config.password, { dialectOptions: { connectString: connDesc } });
-      await sequelize.authenticate().should.be.ok;
+      const sequelize = Support.createSequelizeInstance( { dialectOptions: { connectString: connDesc } });
+      sequelize.authenticate().should.be.ok;
     });
   });
 }
