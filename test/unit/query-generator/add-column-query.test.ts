0fced48cb208e33c0e567d256528fbb245e0efac
frostzt <aidenfrostbite@gmail.com>
2022-10-18 11:01:32 +0530

    tests: updated unit tests for query generator

--- test/unit/query-generator/add-column-query.test.ts
+++ test/unit/query-generator/add-column-query.test.ts
@@ -11,25 +11,25 @@ describe('QueryGenerator#addColumnQuery', () => {
     firstName: DataTypes.STRING,
   }, { timestamps: false });
 
-  it('generates a ADD COLUMN query in supported dialects', async () => {
-    const sql = queryGenerator.addColumnQuery(User.tableName, 'age', DataTypes.INTEGER);
+  it('generates a ADD COLUMN query in supported dialects', () => {
+    const sql = queryGenerator.addColumnQuery(User.tableName, 'age', {
+      type: DataTypes.INTEGER,
+    });
 
-    expectsql(sql, {
-      default: `ALTER TABLE "public"."Users" ADD COLUMN "age" INTEGER;`,
+    expectsql(() => sql, {
+      default: `ALTER TABLE [Users] ADD [age] INTEGER;`,
+      mssql: `ALTER TABLE [Users] ADD [age] INTEGER NULL;`,
       postgres: `ALTER TABLE "public"."Users" ADD COLUMN "age" INTEGER;`,
       mysql: 'ALTER TABLE `Users` ADD `age` INTEGER;',
     });
   });
 
-  it('generates a ADD COLUMN IF NOT EXISTS query in supported dialects', async () => {
-    const sql = queryGenerator.addColumnQuery(User.tableName, 'age', DataTypes.INTEGER, {
-      ifNotExists: true,
-    });
-
-    expectsql(sql, {
+  it('generates a ADD COLUMN IF NOT EXISTS query in supported dialects', () => {
+    expectsql(() => queryGenerator.addColumnQuery(User.tableName, 'age', {
+      type: DataTypes.INTEGER,
+    }, { ifNotExists: true }), {
       default: buildInvalidOptionReceivedError('addColumnQuery', dialectName, ['ifNotExists']),
       postgres: `ALTER TABLE "public"."Users" ADD COLUMN IF NOT EXISTS "age" INTEGER;`,
-      mysql: buildInvalidOptionReceivedError('addColumnQuery', dialectName, ['ifNotExists']),
     });
   });
 });
