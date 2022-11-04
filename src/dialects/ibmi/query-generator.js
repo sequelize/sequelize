506c360a3f8f27d3977f4851cdc4afc4673af26a
hancockm <hancockm@live.com>
2022-11-04 10:11:54 -0400

    Update query-generator.js

--- src/dialects/ibmi/query-generator.js
+++ src/dialects/ibmi/query-generator.js
@@ -133,7 +133,7 @@ export class IBMiQueryGenerator extends AbstractQueryGenerator {
 
     let schema;
 
-     if (typeof tableName === 'object') {
+    if (typeof tableName === 'object') {
       schema = tableName.schema || options.schema || undefined;
       tableName = tableName.table;
      }
