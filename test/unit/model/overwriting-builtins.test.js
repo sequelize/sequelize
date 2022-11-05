25a3062974afdf594aff74a316b4a67042fb8ad8
Ricardo Spear <rspear@adventhealth.com>
2022-11-05 14:28:52 -0400

    Revert "delete duplicate js test"
    This reverts commit eb39d771f26dc5670ff15edd0a2286ed8bb661d7.

---
+++ test/unit/model/overwriting-builtins.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const chai = require('chai');
+
+const expect = chai.expect;
+const Support = require('../../support');
+const { DataTypes } = require('@sequelize/core');
+
+describe(Support.getTestDialectTeaser('Model'), () => {
+
+  describe('not breaking built-ins', () => {
+    it('it should not break instance.set by defining a model set attribute', function () {
+      const User = this.sequelize.define('OverWrittenKeys', {
+        set: DataTypes.STRING,
+      });
+
+      const user = User.build({ set: 'A' });
+      expect(user.get('set')).to.equal('A');
+      user.set('set', 'B');
+      expect(user.get('set')).to.equal('B');
+    });
+  });
+});
