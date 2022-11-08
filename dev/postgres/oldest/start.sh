5bf760fff09d5d0bce33d816da6aa2a6e6595804
Luke Hart <50547373+lohart13@users.noreply.github.com>
2022-11-09 09:08:41 +1300

    meta: support docker compose plugin (#15257)
    * feat: support docker compose plugin
    
    * fix: remove unnecessary docker-compose check
    
    * docs: add docker compose to optional requirements
    
    * docs: update CONTRIBUTING.md
    
    Co-authored-by: Rik Smale <13023439+WikiRik@users.noreply.github.com>
    
    * fix: remove extra lines
    
    Co-authored-by: Rik Smale <13023439+WikiRik@users.noreply.github.com>
    
    Co-authored-by: Rik Smale <13023439+WikiRik@users.noreply.github.com>

--- dev/postgres/oldest/start.sh
+++ dev/postgres/oldest/start.sh
@@ -2,9 +2,8 @@
 set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
 
-
-docker-compose -p sequelize-postgres-oldest down --remove-orphans
-docker-compose -p sequelize-postgres-oldest up -d
+docker compose -p sequelize-postgres-oldest down --remove-orphans
+docker compose -p sequelize-postgres-oldest up -d
 
 ./../../wait-until-healthy.sh sequelize-postgres-oldest
 
