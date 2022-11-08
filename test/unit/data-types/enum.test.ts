d99ac415a8b45c7e1ba542c04954e3b489898ec1
fzn0x <fncolon@pm.me>
2022-11-09 02:43:37 +0700

    fix(test): resolve by suggestions

--- test/unit/data-types/enum.test.ts
+++ test/unit/data-types/enum.test.ts
@@ -1,34 +1,25 @@
 import { DataTypes } from '@sequelize/core';
-import { expect } from 'chai';
 
 describe('DataTypes.ENUM', () => {
   describe('setting values for enums', () => {
     it('should not throw if `values` is a readonly array', () => {
-      const Array = ['A', 'B', 'C', 'D', 'E'] as const;
-      const type = DataTypes.ENUM({ values: Array });
-
-      expect(type.options.values).to.be.equal(Array);
+      const values = ['A', 'B', 'C', 'D', 'E'] as const;
+      DataTypes.ENUM({ values });
     });
 
     it('should not throw if `values` is an mutable array', () => {
-      const Array = ['A', 'B', 'C', 'D', 'E'];
-      const type = DataTypes.ENUM({ values: Array });
-
-      expect(type.options.values).to.be.equal(Array);
+      const values = ['A', 'B', 'C', 'D', 'E'];
+      DataTypes.ENUM({ values });
     });
 
     it('should not throw if members is a readonly array', () => {
-      const Array = ['A', 'B', 'C', 'D', 'E'] as const;
-      const type = DataTypes.ENUM(Array);
-
-      expect(type.options.values).to.be.equal(Array);
+      const values = ['A', 'B', 'C', 'D', 'E'] as const;
+      DataTypes.ENUM(values);
     });
 
     it('should not throw if members is an mutable array', () => {
-      const Array = ['A', 'B', 'C', 'D', 'E'];
-      const type = DataTypes.ENUM(Array);
-
-      expect(type.options.values).to.be.equal(Array);
+      const values = ['A', 'B', 'C', 'D', 'E'];
+      DataTypes.ENUM(values);
     });
   });
 });
