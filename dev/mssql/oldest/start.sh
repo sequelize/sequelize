616a83dab5fb96409baf9d79cc5036d73fc1487a
Luke Hart <50547373+lohart13@users.noreply.github.com>
2022-11-09 08:48:48 +1300

    fix: remove extra lines
    Co-authored-by: Rik Smale <13023439+WikiRik@users.noreply.github.com>

--- dev/mssql/oldest/start.sh
+++ dev/mssql/oldest/start.sh
@@ -2,7 +2,6 @@
 set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
 
-
 docker compose -p sequelize-mssql-oldest down --remove-orphans
 docker compose -p sequelize-mssql-oldest up -d
 
--- dev/mysql/latest/start.sh
+++ dev/mysql/latest/start.sh
@@ -2,10 +2,8 @@
 set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
 
-
 docker compose -p sequelize-mysql-latest down --remove-orphans
 docker compose -p sequelize-mysql-latest up -d
-
 
 ./../../wait-until-healthy.sh sequelize-mysql-latest
 
