25d88301eb388be3e1b958ecc1fe44a7741356a1
ephys <zoe@ephys.dev>
2022-11-17 18:14:17 +0100

    fix: revert schema workaround removal

--- src/dialects/abstract/query-interface.js
+++ src/dialects/abstract/query-interface.js
@@ -242,6 +242,8 @@ export class QueryInterface {
       table: tableName,
       context: 'createTable',
       withoutForeignKeyConstraints: options.withoutForeignKeyConstraints,
+      // schema override for multi-tenancy
+      schema: options.schema,
     });
     sql = this.queryGenerator.createTableQuery(tableName, attributes, options);
 
