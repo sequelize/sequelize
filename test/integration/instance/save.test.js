3095d99a67258425c85378b0bef54f1c5e673264
Sourav Singh Rawat <aidenfrostbite@gmail.com>
2022-11-20 20:05:46 +0530

    feat: restrict instance methods if no primary key (#15108)
    BREAKING CHANGE: Some model instance methods cannot be used if the model doesn't have a primary key anymore. See https://github.com/sequelize/sequelize/pull/15108

--- test/integration/instance/save.test.js
+++ test/integration/instance/save.test.js
@@ -129,6 +129,14 @@ describe(Support.getTestDialectTeaser('Instance'), () => {
       });
     });
 
+    it('is disallowed if no primary key is present', async function () {
+      const Foo = this.sequelize.define('Foo', {});
+      await Foo.sync({ force: true });
+
+      const instance = await Foo.build({}, { isNewRecord: false });
+      await expect(instance.save()).to.be.rejectedWith('You attempted to save an instance with no primary key');
+    });
+
     describe('hooks', () => {
       it('should update attributes added in hooks when default fields are used', async function () {
         const User = this.sequelize.define(`User${Support.rand()}`, {
