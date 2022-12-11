5ed1dbbc98d7deb176a08cb4030414e2bc9c295b
Evan Rittenhouse <evanrittenhouse@gmail.com>
2022-12-11 12:23:44 -0600

    Implement reviewer comments, pt. 2

--- test/integration/query-interface/schemas.test.ts
+++ test/integration/query-interface/schemas.test.ts
@@ -7,32 +7,33 @@ const queryInterface = sequelize.queryInterface;
 
 const testSchema = 'testSchema';
 
-if (sequelize.dialect.supports.schemas) {
-  describe('QueryInterface#createSchema', async () => {
+describe('QueryInterface#createSchema', async () => {
+  if (!sequelize.dialect.supports.schemas) {
+    return;
+  }
 
-    it('creates a schema', async () => {
-      const preCreationSchemas = await queryInterface.showAllSchemas();
-      expect(preCreationSchemas).to.not.include(testSchema, 'testSchema existed before tests ran');
+  it('creates a schema', async () => {
+    const preCreationSchemas = await queryInterface.showAllSchemas();
+    expect(preCreationSchemas).to.not.include(testSchema, 'testSchema existed before tests ran');
 
-      await queryInterface.createSchema(testSchema);
-      const postCreationSchemas = await queryInterface.showAllSchemas();
-      expect(postCreationSchemas).to.include(testSchema, 'createSchema did not create testSchema');
-    });
-
-    it(`passes options through to the queryInterface's queryGenerator`, async () => {
-      const options: CreateSchemaQueryOptions = {
-        collate: 'en_US.UTF-8',
-        charset: 'utf8mb4',
-      };
-      const queryGeneratorSpy = spy(queryInterface.queryGenerator, 'createSchemaQuery');
-
-      try {
-        await queryInterface.createSchema(testSchema, options);
-      } catch {
-        // Dialects which don't support collate/charset will throw
-      }
-
-      expect(queryGeneratorSpy.args[0]).to.include(options);
-    });
+    await queryInterface.createSchema(testSchema);
+    const postCreationSchemas = await queryInterface.showAllSchemas();
+    expect(postCreationSchemas).to.include(testSchema, 'createSchema did not create testSchema');
   });
-}
+
+  it(`passes options through to the queryInterface's queryGenerator`, async () => {
+    const options: CreateSchemaQueryOptions = {
+      collate: 'en_US.UTF-8',
+      charset: 'utf8mb4',
+    };
+    const queryGeneratorSpy = spy(queryInterface.queryGenerator, 'createSchemaQuery');
+
+    try {
+      await queryInterface.createSchema(testSchema, options);
+    } catch {
+      // Dialects which don't support collate/charset will throw
+    }
+
+    expect(queryGeneratorSpy.args[0]).to.include(options);
+  });
+});
