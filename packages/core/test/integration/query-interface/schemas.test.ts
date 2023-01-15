84b4f52dfa52fefc83312a1720690aa5358eddba
Evan Rittenhouse <evanrittenhouse@gmail.com>
2023-01-15 12:17:21 -0600

    Implement reviewer comments on tests

--- packages/core/test/integration/query-interface/schemas.test.ts
+++ packages/core/test/integration/query-interface/schemas.test.ts
@@ -10,92 +10,84 @@ const testSchema = 'testSchema';
 const dialectsWithEqualDBsSchemas = ['mysql', 'mariadb'];
 
 describe('QueryInterface#{create,drop,dropAll,showAll}Schema', () => {
-  if (!dialect.supports.schemas) {
-    return;
-  }
-
-  it('creates a schema', async () => {
-    const preCreationSchemas = await queryInterface.showAllSchemas();
-    expect(preCreationSchemas).to.not.include(testSchema, 'testSchema existed before tests ran');
-
-    await queryInterface.createSchema(testSchema);
-    const postCreationSchemas = await queryInterface.showAllSchemas();
-    expect(postCreationSchemas).to.include(testSchema, 'createSchema did not create testSchema');
-  });
-
-  it(`passes options through to the queryInterface's queryGenerator`, async () => {
-    const options: CreateSchemaQueryOptions = {
-      collate: 'en_US.UTF-8',
-      charset: 'utf8mb4',
-    };
-    const queryGeneratorSpy = spy(queryInterface.queryGenerator, 'createSchemaQuery');
-
-    try {
-      await queryInterface.createSchema(testSchema, options);
-    } catch {
-      // Dialects which don't support collate/charset will throw
-    }
-
-    expect(queryGeneratorSpy.args[0]).to.include(options);
-  });
-
-  it('drops a schema', async () => {
-    await queryInterface.createSchema(testSchema);
-    const preDeletionSchemas = await queryInterface.showAllSchemas();
-    expect(preDeletionSchemas).to.include(testSchema, 'createSchema did not create testSchema');
-
-    await queryInterface.dropSchema(testSchema);
-    const postDeletionSchemas = await queryInterface.showAllSchemas();
-    expect(postDeletionSchemas).to.not.include(testSchema, 'dropSchema did not drop testSchema');
-  });
-
-  it('shows all schemas', async () => {
-    await queryInterface.createSchema(testSchema);
-    const allSchemas = await queryInterface.showAllSchemas();
-
-    const expected = dialectsWithEqualDBsSchemas.includes(dialect.name)
-      ? [sequelize.config.database, testSchema]
-      : [testSchema];
-    expect(allSchemas.sort()).to.deep.eq(expected.sort());
-  });
-
-  describe('drops all schemas', () => {
-    it('drops all schemas except default schema', async () => {
-      await queryInterface.dropAllSchemas({
-        skip: [sequelize.config.database],
-      });
-      const postDeleteSchemas = await queryInterface.showAllSchemas();
-
-      const expected = dialectsWithEqualDBsSchemas.includes(dialect.name) ? [sequelize.config.database] : [];
-      expect(postDeleteSchemas).to.deep.eq(expected);
-    });
-
-    it('drops all schemas except test schema', async () => {
-      // required for testing this option with Postgres, MSSQL, DB2
-      if (!['postgres', 'mssql', 'db2'].includes(dialect.name)) {
-        return;
-      }
+  if (dialect.supports.schemas) {
+    it('creates a schema', async () => {
+      const preCreationSchemas = await queryInterface.showAllSchemas();
+      expect(preCreationSchemas).to.not.include(testSchema, 'testSchema existed before tests ran');
 
       await queryInterface.createSchema(testSchema);
-      await queryInterface.dropAllSchemas({
-        skip: [testSchema],
-      });
-      const postDeleteSchemas = await queryInterface.showAllSchemas();
-      expect(postDeleteSchemas).to.deep.eq([testSchema]);
+      const postCreationSchemas = await queryInterface.showAllSchemas();
+      expect(postCreationSchemas).to.include(testSchema, 'createSchema did not create testSchema');
     });
 
-    it('drops all schemas', async () => {
-      await queryInterface.dropAllSchemas();
-      const postDeleteSchemas = await queryInterface.showAllSchemas();
-      expect(postDeleteSchemas).to.be.empty;
+    it(`passes options through to the queryInterface's queryGenerator`, async () => {
+      const options: CreateSchemaQueryOptions = {
+        collate: 'en_US.UTF-8',
+        charset: 'utf8mb4',
+      };
+      const queryGeneratorSpy = spy(queryInterface.queryGenerator, 'createSchemaQuery');
 
-      // Removing this call will break all subsequent MariaDB/MySQL tests since the they wouldn't
-      // have a database to run against. Recreate test schema - can't run this in an `after` block
-      // since `afterEach` hooks run first and require their own database
-      if (dialectsWithEqualDBsSchemas.includes(dialect.name)) {
-        await queryInterface.createSchema(sequelize.config.database);
-        await sequelize.queryRaw(`USE ${sequelize.config.database}`);
+      try {
+        await queryInterface.createSchema(testSchema, options);
+      } catch {
+        // Dialects which don't support collate/charset will throw
       }
+
+      expect(queryGeneratorSpy.args[0]).to.include(options);
     });
-  });
+
+    it('drops a schema', async () => {
+      await queryInterface.createSchema(testSchema);
+      const preDeletionSchemas = await queryInterface.showAllSchemas();
+      expect(preDeletionSchemas).to.include(testSchema, 'createSchema did not create testSchema');
+
+      await queryInterface.dropSchema(testSchema);
+      const postDeletionSchemas = await queryInterface.showAllSchemas();
+      expect(postDeletionSchemas).to.not.include(testSchema, 'dropSchema did not drop testSchema');
+    });
+
+    it('shows all schemas', async () => {
+      await queryInterface.createSchema(testSchema);
+      const allSchemas = await queryInterface.showAllSchemas();
+
+      const expected = dialectsWithEqualDBsSchemas.includes(dialect.name)
+        ? [sequelize.config.database, testSchema]
+        : [testSchema];
+      expect(allSchemas.sort()).to.deep.eq(expected.sort());
+    });
+
+    describe('drops all schemas', () => {
+      it('drops all schemas and skips', async () => {
+        await queryInterface.dropAllSchemas({
+          skip: [sequelize.config.database, testSchema],
+        });
+        const postDeleteSchemas = await queryInterface.showAllSchemas();
+
+        const expected = dialectsWithEqualDBsSchemas.includes(dialect.name)
+          ? [sequelize.config.database, testSchema]
+          : [testSchema];
+        expect(postDeleteSchemas).to.deep.eq(expected);
+      });
+
+      it('drops all schemas', async () => {
+        await queryInterface.dropAllSchemas();
+        const postDeleteSchemas = await queryInterface.showAllSchemas();
+        expect(postDeleteSchemas).to.be.empty;
+      });
+
+      after(async () => {
+        if (dialectsWithEqualDBsSchemas.includes(dialect.name)) {
+          await queryInterface.createSchema(sequelize.config.database);
+          await sequelize.queryRaw(`USE ${sequelize.config.database}`);
+        }
+      });
+    });
+  } else {
+    it('should throw, indicating that the method is not supported', async () => {
+      await expect(queryInterface.createSchema(testSchema)).to.be.rejectedWith(`Schemas are not supported in ${dialect}.`);
+      await expect(queryInterface.dropSchema(testSchema)).to.be.rejectedWith(`Schemas are not supported in ${dialect}.`);
+      await expect(queryInterface.dropAllSchemas()).to.be.rejectedWith(`Schemas are not supported in ${dialect}.`);
+      await expect(queryInterface.showAllSchemas()).to.be.rejectedWith(`Schemas are not supported in ${dialect}.`);
+    });
+  }
 });
