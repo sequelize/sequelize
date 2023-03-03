1e673460cada89489fedd904dcb0870b33834ff1
Mhashh <77043440+Mhashh@users.noreply.github.com>
2023-03-03 20:59:22 +0530

    updated test path,added timer

--- test/integration/smoketest.test.js
+++ test/integration/smoketest.test.js
@@ -1,42 +1,52 @@
 'use strict';
 
 const chai = require('chai'),
+  Sequelize = require('sequelize'),
   expect = chai.expect,
   sinon = require('sinon'),
-  Support = require('../support'),
+  Support = require('./support'),
   DataTypes = require('sequelize/lib/data-types'),
-  current = Support.sequelize;
+  current = Support.sequelize,
+  Op = Sequelize.Op;
 
 describe(Support.getTestDialectTeaser('Model'), () => {
-  describe('Bulk update when model defined has attributes with virtual getters and setters should not throw undefined.', () => {
-    before(async function() {
-      this.User = current.define('users', {
-        id: {
-          type: DataTypes.UUID,
-          primaryKey: true,
-          defaultValue: DataTypes.UUIDV4
+  
+  before(function() {
+    this.clock = sinon.useFakeTimers();
+  });
+
+  after(function() {
+    this.clock.restore();
+  });
+
+  beforeEach(async function() {
+    this.User = this.sequelize.define('users', {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+      },
+      status: {
+        type: DataTypes.STRING,
+        defaultValue: 'active'
+      },
+      roles: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        get() {
+          return this.getDataValue('roles').split(',');
         },
-        status: {
-          type: DataTypes.STRING,
-          defaultValue: 'active'
-        },
-        roles: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          get() {
-            return this.getDataValue('roles').split(',');
-          },
-          set(val) {
-            this.setDataValue('roles', val.join(','));
-          }
+        set(val) {
+          this.setDataValue('roles', val.join(','));
         }
-      });
+      }
+    });
 
-      await this.User.sync({ force: true });
-    });       
+    await this.User.sync({ force: true });
+  });
 
-    
-
+  describe('Bulk update when model defined has attributes with virtual getters and setters should not throw undefined.', () => {
+           
     it('Values are correctly updated without any error being thrown.', async function() {
         
       const u1 = await this.User.create({
@@ -48,7 +58,7 @@ describe(Support.getTestDialectTeaser('Model'), () => {
 
       await this.User.update({ status: 'blocked' }, { where: {
         id: {
-          [current.Sequelize.Op.ne]: null
+          [Op.ne]: null
         } 
       } });
       const a1 = await this.User.findOne({ where: { id: u1.id } });
