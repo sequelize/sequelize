edc714007fbd905c05218f482066b9ac702e8afb
Mhashh <77043440+Mhashh@users.noreply.github.com>
2023-03-05 12:42:01 +0530

    feat:added test for bulkUpdate

--- packages/core/test/integration/model.test.js
+++ packages/core/test/integration/model.test.js
@@ -2787,4 +2787,52 @@ describe(Support.getTestDialectTeaser('Model'), () => {
       expect(users1[1].id.toString()).to.equal('3415718944570971484');
     });
   });
+
+  describe('BulkUpdate', () => {
+           
+    it('should update correctly when model defined has attributes with virtual getters', async function() {
+      
+      const User = this.sequelize.define('users', {
+        id: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          defaultValue: DataTypes.UUIDV4
+        },
+        status: {
+          type: DataTypes.STRING,
+          defaultValue: 'active'
+        },
+        roles: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          get() {
+            return this.getDataValue('roles').split(',');
+          },
+          set(val) {
+            this.setDataValue('roles', val.join(','));
+          }
+        }
+      });
+  
+      await this.User.sync({ force: true });
+
+      const u1 = await this.User.create({
+        roles: ['authenticated user']
+      });
+      const u2 = await this.User.create({
+        roles: ['authenticated user']
+      });
+
+      await User.update({ status: 'blocked' }, { where: {
+        id: {
+          [Op.ne]: null
+        } 
+      } });
+      const a1 = await User.findOne({ where: { id: u1.id } });
+      const a2 = await User.findOne({ where: { id: u2.id } });
+      
+      expect(a1.get('status')).to.eq('blocked');
+      expect(a2.get('status')).to.eq('blocked');
+    });
+  });
 });
