a25940ba73d8aa8f1fff722b2d4a65df2eeef1f1
Vyacheslav Aristov <vv.aristov@gmail.com>
2023-03-05 20:47:40 +0800

    Fixing issues after code review

--- test/integration/model/sync.test.js
+++ test/integration/model/sync.test.js
@@ -266,6 +266,24 @@ describe(Support.getTestDialectTeaser('Model'), () => {
           }
         });
       }
+
+      // TODO add support for db2 and mssql dialects
+      if (!['db2', 'mssql'].includes(dialect)) {
+        it('does not recreate existing enums (#7649)', async () => {
+          sequelize.define('Media', {
+            type: DataTypes.ENUM([
+              'video', 'audio'
+            ])
+          });
+          await sequelize.sync({ alter: true });
+          sequelize.define('Media', {
+            type: DataTypes.ENUM([
+              'image', 'video', 'audio'
+            ])
+          });
+          await sequelize.sync({ alter: true });
+        });
+      }
     });
 
     describe('indexes', () => {
