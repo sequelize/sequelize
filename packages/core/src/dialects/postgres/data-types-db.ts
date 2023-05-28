6201db38fbb8bf9f895bad4aff1d3fd83e6dbc36
Zoé Cox <zoe@ephys.dev>
2023-05-28 16:33:24 +0200

    update typedoc

--- packages/core/src/dialects/postgres/data-types-db.ts
+++ packages/core/src/dialects/postgres/data-types-db.ts
@@ -9,7 +9,7 @@ import type { PostgresDialect } from './index.js';
 
 /**
  * First pass of DB value parsing: Parses based on the Postgres Type ID.
- * If a Sequelize DataType is specified, the value is then passed to {@link AbstractDataType#parseDatabaseValue}.
+ * If a Sequelize DataType is specified, the value is then passed to {@link DataTypes.ABSTRACT#parseDatabaseValue}.
  *
  * @param dialect
  */
--- typedoc.js
+++ typedoc.js
@@ -1,11 +1,10 @@
 module.exports = {
   entryPointStrategy: 'packages',
-  entryPoints: ['packages/core', 'packages/validator-js'],
+  entryPoints: ['packages/*'],
   out: './.typedoc-build',
   readme: 'none',
   plugin: ['typedoc-plugin-missing-exports', 'typedoc-plugin-mdn-links', 'typedoc-plugin-carbon-ads'],
   carbonPlacement: 'sequelizeorg',
   carbonServe: 'CEAI627Y',
-  excludeExternals: true,
-  treatWarningsAsErrors: true,
+  // treatWarningsAsErrors: true,
 };
