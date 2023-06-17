a3213f053bcc4534073e56def64f22392edaf28c
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2023-06-17 15:16:50 +0200

    fix: bump dependencies (#16119)

--- test/unit/sql/add-column.test.js
+++ test/unit/sql/add-column.test.js
@@ -8,7 +8,7 @@ const current   = Support.sequelize;
 const sql       = current.dialect.queryGenerator;
 
 const customSequelize = Support.createSequelizeInstance({
-  schema: 'custom',
+  schema: 'custom'
 });
 const customSql = customSequelize.dialect.queryGenerator;
 
@@ -18,18 +18,18 @@ describe(Support.getTestDialectTeaser('SQL'), () => {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true,
-      },
+        autoIncrement: true
+      }
     }, { timestamps: false });
     if (['mysql', 'mariadb'].includes(current.dialect.name)) {
 
       it('properly generate alter queries', () => {
         return expectsql(sql.addColumnQuery(User.getTableName(), 'level_id', current.normalizeAttribute({
           type: DataTypes.FLOAT,
-          allowNull: false,
+          allowNull: false
         })), {
           mariadb: 'ALTER TABLE `Users` ADD `level_id` FLOAT NOT NULL;',
-          mysql: 'ALTER TABLE `Users` ADD `level_id` FLOAT NOT NULL;',
+          mysql: 'ALTER TABLE `Users` ADD `level_id` FLOAT NOT NULL;'
         });
       });
 
@@ -38,33 +38,33 @@ describe(Support.getTestDialectTeaser('SQL'), () => {
           type: DataTypes.INTEGER,
           references: {
             model: 'level',
-            key: 'id',
+            key: 'id'
           },
           onUpdate: 'cascade',
-          onDelete: 'cascade',
+          onDelete: 'cascade'
         })), {
           mariadb: 'ALTER TABLE `Users` ADD `level_id` INTEGER, ADD CONSTRAINT `Users_level_id_foreign_idx` FOREIGN KEY (`level_id`) REFERENCES `level` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;',
-          mysql: 'ALTER TABLE `Users` ADD `level_id` INTEGER, ADD CONSTRAINT `Users_level_id_foreign_idx` FOREIGN KEY (`level_id`) REFERENCES `level` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;',
+          mysql: 'ALTER TABLE `Users` ADD `level_id` INTEGER, ADD CONSTRAINT `Users_level_id_foreign_idx` FOREIGN KEY (`level_id`) REFERENCES `level` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;'
         });
       });
 
       it('properly generate alter queries with FIRST', () => {
         return expectsql(sql.addColumnQuery(User.getTableName(), 'test_added_col_first', current.normalizeAttribute({
           type: DataTypes.STRING,
-          first: true,
+          first: true
         })), {
           mariadb: 'ALTER TABLE `Users` ADD `test_added_col_first` VARCHAR(255) FIRST;',
-          mysql: 'ALTER TABLE `Users` ADD `test_added_col_first` VARCHAR(255) FIRST;',
+          mysql: 'ALTER TABLE `Users` ADD `test_added_col_first` VARCHAR(255) FIRST;'
         });
       });
 
       it('properly generates alter queries with column level comment', () => {
         return expectsql(sql.addColumnQuery(User.getTableName(), 'column_with_comment', current.normalizeAttribute({
           type: DataTypes.STRING,
-          comment: 'This is a comment',
+          comment: 'This is a comment'
         })), {
           mariadb: 'ALTER TABLE `Users` ADD `column_with_comment` VARCHAR(255) COMMENT \'This is a comment\';',
-          mysql: 'ALTER TABLE `Users` ADD `column_with_comment` VARCHAR(255) COMMENT \'This is a comment\';',
+          mysql: 'ALTER TABLE `Users` ADD `column_with_comment` VARCHAR(255) COMMENT \'This is a comment\';'
         });
       });
     }
@@ -72,7 +72,7 @@ describe(Support.getTestDialectTeaser('SQL'), () => {
     it('defaults the schema to the one set in the Sequelize options', () => {
       return expectsql(customSql.addColumnQuery(User.getTableName(), 'level_id', customSequelize.normalizeAttribute({
         type: DataTypes.FLOAT,
-        allowNull: false,
+        allowNull: false
       })), {
         mariadb: 'ALTER TABLE `Users` ADD `level_id` FLOAT NOT NULL;',
         mysql: 'ALTER TABLE `Users` ADD `level_id` FLOAT NOT NULL;',
