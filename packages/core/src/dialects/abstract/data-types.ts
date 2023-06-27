f248547956b0fde6fad5387e508ed56ccca323ab
bra1n <steffen@baumgart.biz>
2023-06-27 10:02:46 +0200

    attempt to fix tests

--- packages/core/src/dialects/abstract/data-types.ts
+++ packages/core/src/dialects/abstract/data-types.ts
@@ -2369,7 +2369,7 @@ export class ARRAY<T extends AbstractDataType<any>> extends AbstractDataType<Arr
   clone(): this {
     // there is a convention that all DataTypes must accept a single "options" parameter as one of their signatures, but it's impossible to enforce in typing
     // @ts-expect-error -- see ^
-    return this._construct(this.options.type.clone());
+    return this._construct(this.options.type.clone().toDialectDataType(this._getDialect()));
   }
 }
 
