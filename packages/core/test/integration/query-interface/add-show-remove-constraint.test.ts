f1945cfb56f9d1a5bba16e648df431a677b99a1e
lohart13 <lohart13@gmail.com>
2023-09-04 12:44:23 +1200

    meta: merge tests

--- packages/core/test/integration/query-interface/add-show-remove-constraint.test.ts
+++ packages/core/test/integration/query-interface/add-show-remove-constraint.test.ts
@@ -1,6 +1,6 @@
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import { lt } from 'semver';
-import { DataTypes, Op } from '@sequelize/core';
+import { DataTypes, Op, UnknownConstraintError } from '@sequelize/core';
 import { sequelize } from '../support';
 
 const queryInterface = sequelize.queryInterface;
@@ -52,6 +52,39 @@ describe('QueryInterface#{add,show,removeConstraint}', () => {
           allowNull: false,
         },
       });
+    });
+
+    it('should throw an error if constraint type is missing', async () => {
+      await expect(
+        // @ts-expect-error -- intentionally missing type
+        queryInterface.addConstraint('levels', {
+          fields: ['roles'],
+          where: { roles: ['user', 'admin', 'guest', 'moderator'] },
+          name: 'check_user_roles',
+        }),
+      ).to.be.rejectedWith(Error, 'Constraint type must be specified through options.type');
+    });
+
+    it('should throw non existent constraints as UnknownConstraintError', async () => {
+      try {
+        await queryInterface.removeConstraint('levels', 'unknown__constraint__name', { type: 'unique' });
+        expect.fail('Expected to throw an error');
+      } catch (error) {
+        let err = error;
+        if (dialect === 'mssql') {
+          expect(error).to.be.instanceOf(AggregateError);
+          assert(error instanceof AggregateError);
+          err = error.errors.at(-1);
+        } else {
+          expect(err).to.be.instanceOf(UnknownConstraintError);
+          assert(err instanceof UnknownConstraintError);
+          if (dialect !== 'ibmi') {
+            expect(err.table).to.equal('levels');
+          }
+
+          expect(err.constraint).to.equal('unknown__constraint__name');
+        }
+      }
     });
 
     it('should add, show and delete a UNIQUE constraint', async () => {
