99b9bed259aa8911094850ef0613fa6ecd0b8c2b
lohart13 <lohart13@gmail.com>
2023-09-14 08:05:27 +1200

    meta: update drop test schema function

--- packages/core/test/integration/support.ts
+++ packages/core/test/integration/support.ts
@@ -271,22 +271,17 @@ export async function dropTestSchemas(customSequelize: Sequelize = sequelize) {
     return;
   }
 
-  const schemas = await customSequelize.showAllSchemas();
+  const schemas = await customSequelize.showAllSchemas({ skip: [customSequelize.config.database] });
   const schemasPromise = [];
-  for (const schema of schemas) {
-    // @ts-expect-error -- TODO: type return value of "showAllSchemas"
-    const schemaName = schema.name ? schema.name : schema;
-    if (schemaName !== customSequelize.config.database) {
-      const promise = customSequelize.dropSchema(schemaName);
-
-      if (getTestDialect() === 'db2') {
-        // https://github.com/sequelize/sequelize/pull/14453#issuecomment-1155581572
-        // DB2 can sometimes deadlock / timeout when deleting more than one schema at the same time.
-        // eslint-disable-next-line no-await-in-loop
-        await promise;
-      } else {
-        schemasPromise.push(promise);
-      }
+  for (const schemaName of schemas) {
+    const promise = customSequelize.dropSchema(schemaName);
+    if (getTestDialect() === 'db2') {
+      // https://github.com/sequelize/sequelize/pull/14453#issuecomment-1155581572
+      // DB2 can sometimes deadlock / timeout when deleting more than one schema at the same time.
+      // eslint-disable-next-line no-await-in-loop
+      await promise;
+    } else {
+      schemasPromise.push(promise);
     }
   }
 
