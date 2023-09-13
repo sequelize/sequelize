b490bcb5c10db6bc54dde14e326fd2ff76a5b98b
lohart13 <lohart13@gmail.com>
2023-09-14 08:05:47 +1200

    meta: update tests

--- packages/core/test/integration/query-interface/create-drop-list-database.test.ts
+++ packages/core/test/integration/query-interface/create-drop-list-database.test.ts
@@ -4,13 +4,11 @@ import { sequelize } from '../support';
 
 const queryInterface = sequelize.queryInterface;
 const dialect = sequelize.getDialect();
-
-const supportedByDialect = ['postgres', 'mssql', 'snowflake'].includes(dialect);
 const newDbName = 'myDB';
 
 describe('QueryInterface#{create,drop,list}Database', () => {
 
-  if (supportedByDialect) {
+  if (sequelize.dialect.supports.multiDatabases) {
     it('should create, drop, and list databases respectively', async () => {
       const preCreationDatabases: DatabaseDescription[] = await queryInterface.listDatabases();
       expect(preCreationDatabases.some(db => db.name === newDbName)).to.eq(false, 'Database already exists prior to running the test');
@@ -26,6 +24,11 @@ describe('QueryInterface#{create,drop,list}Database', () => {
 
       expect(postDeletionDatabases.some(db => db.name === newDbName)).to.eq(false, 'Database "myDB" still exists, but should have been deleted');
       expect(postDeletionDatabases.length).to.eq(preCreationDatabases.length);
+    });
+
+    it('should list all databases except technical ones', async () => {
+      const databases: DatabaseDescription[] = await queryInterface.listDatabases();
+      expect(databases).to.deep.equal([{ name: 'sequelize_test' }]);
     });
   } else {
     it('should throw, indicating that the method is not supported', async () => {
