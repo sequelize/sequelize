a2370bfc54162bb8b97123bf719de32e14155666
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2024-02-19 18:42:13 +0000

    Fix test

--- packages/core/test/support.ts
+++ packages/core/test/support.ts
@@ -615,7 +615,6 @@ const ignoredDeprecations: readonly string[] = [
   'SEQUELIZE0025',
   'SEQUELIZE0026',
   'SEQUELIZE0027',
-  'DEP0118',
 ];
 let allowedDeprecations: readonly string[] = ignoredDeprecations;
 export function allowDeprecationsInSuite(codes: readonly string[]) {
