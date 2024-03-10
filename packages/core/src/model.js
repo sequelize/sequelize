7b86d43b4b25dc3a6429ac4c8340c23baf530a2d
lohart13 <lohart13@gmail.com>
2024-03-11 12:29:07 +1300

    fix(sqlite): add support for returning values

--- packages/core/src/model.js
+++ packages/core/src/model.js
@@ -2529,7 +2529,7 @@ ${associationOwner._getAssociationDebugList()}`);
                 !instance ||
                 (key === model.primaryKeyAttribute &&
                   instance.get(model.primaryKeyAttribute) &&
-                  ['mysql', 'mariadb', 'sqlite'].includes(dialect))
+                  ['mysql', 'mariadb'].includes(dialect))
               ) {
                 // The query.js for these DBs is blind, it autoincrements the
                 // primarykey value, even if it was set manually. Also, it can
