bccd7b9f54732dc189aff1611dcfd8c8eff92ba2
Zoé Cox <zoe@ephys.dev>
2024-03-18 11:28:05 +0100

    delete changelog files

---
+++ dev/delete-changelog.mjs
@@ -0,0 +1,31 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const changelogPaths = [
+  path.resolve(__dirname, '../CHANGELOG.md'),
+  path.resolve(__dirname, '../packages/core/CHANGELOG.md'),
+  path.resolve(__dirname, '../packages/utils/CHANGELOG.md'),
+  path.resolve(__dirname, '../packages/validator-js/CHANGELOG.md'),
+];
+
+await Promise.all(
+  changelogPaths.map(async changelogPath => {
+    if (await tryAccess(changelogPath)) {
+      await fs.unlink(changelogPath);
+      console.info(`Deleted ${changelogPath}`);
+    }
+  }),
+);
+
+async function tryAccess(path) {
+  try {
+    await fs.access(path);
+
+    return true;
+  } catch {
+    return false;
+  }
+}
