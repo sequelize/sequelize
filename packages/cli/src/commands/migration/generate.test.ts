c124452848f2389ea5d2b52847fb8b6204815a79
Alyx <zoe@ephys.dev>
2024-04-06 19:03:27 +0200

    make tests work on windows

--- packages/cli/src/commands/migration/generate.test.ts
+++ packages/cli/src/commands/migration/generate.test.ts
@@ -2,6 +2,7 @@ import { expect, test } from '@oclif/test';
 import { fileUrlToDirname } from '@sequelize/utils/node';
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import { pathToFileURL } from 'node:url';
 
 const __dirname = fileUrlToDirname(import.meta.url);
 const packageRoot = path.join(__dirname, '..', '..', '..');
@@ -20,7 +21,9 @@ describe('migration:generate', () => {
       const asJson = JSON.parse(ctx.stdout);
 
       expect(Object.keys(asJson)).to.deep.eq(['path']);
-      expect(asJson.path).to.match(/migrations\/[\d\-t]{19}-test-migration/);
+      expect(pathToFileURL(asJson.path).pathname).to.match(
+        /migrations\/[\d\-t]{19}-test-migration/,
+      );
       expect(await fs.readdir(asJson.path)).to.have.members(['up.sql', 'down.sql']);
     });
 
@@ -31,7 +34,9 @@ describe('migration:generate', () => {
       const asJson = JSON.parse(ctx.stdout);
 
       expect(Object.keys(asJson)).to.deep.eq(['path']);
-      expect(asJson.path).to.match(/migrations\/[\d\-t]{19}-test-migration\.ts/);
+      expect(pathToFileURL(asJson.path).pathname).to.match(
+        /migrations\/[\d\-t]{19}-test-migration\.ts/,
+      );
       await fs.access(asJson.path);
     });
 
@@ -42,7 +47,9 @@ describe('migration:generate', () => {
       const asJson = JSON.parse(ctx.stdout);
 
       expect(Object.keys(asJson)).to.deep.eq(['path']);
-      expect(asJson.path).to.match(/migrations\/[\d\-t]{19}-test-migration\.cjs/);
+      expect(pathToFileURL(asJson.path).pathname).to.match(
+        /migrations\/[\d\-t]{19}-test-migration\.cjs/,
+      );
       await fs.access(asJson.path);
     });
 
@@ -53,7 +60,9 @@ describe('migration:generate', () => {
       const asJson = JSON.parse(ctx.stdout);
 
       expect(Object.keys(asJson)).to.deep.eq(['path']);
-      expect(asJson.path).to.match(/migrations\/[\d\-t]{19}-test-migration\.mjs/);
+      expect(pathToFileURL(asJson.path).pathname).to.match(
+        /migrations\/[\d\-t]{19}-test-migration\.mjs/,
+      );
       await fs.access(asJson.path);
     });
 
@@ -64,6 +73,6 @@ describe('migration:generate', () => {
       const asJson = JSON.parse(ctx.stdout);
 
       expect(Object.keys(asJson)).to.deep.eq(['path']);
-      expect(asJson.path).to.match(/migrations\/[\d\-t]{19}-unnamed/);
+      expect(pathToFileURL(asJson.path).pathname).to.match(/migrations\/[\d\-t]{19}-unnamed/);
     });
 });
