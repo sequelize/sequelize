55a79401213fd85ab4265ee9fcc3aa2d61d0640a
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2024-09-29 16:43:16 +0000

    build: update docker-compose to docker compose

--- dev/mariadb/10.3/start.sh
+++ dev/mariadb/10.3/start.sh
@@ -3,8 +3,8 @@ set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
 
 
-docker-compose -p sequelize-mariadb-103 down --remove-orphans
-docker-compose -p sequelize-mariadb-103 up -d
+docker compose -p sequelize-mariadb-103 down --remove-orphans
+docker compose -p sequelize-mariadb-103 up -d
 
 ./../../wait-until-healthy.sh sequelize-mariadb-103
 
