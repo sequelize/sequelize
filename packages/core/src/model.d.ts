5135e8305789d438f636e366c453db04f6560db5
Leandro Costa <contato@leandrocosta.pro.br>
2024-10-08 06:33:13 -0300

    fix(core): adjust Model.sum|min|max return type to include null (#17527)
    Co-authored-by: ephys <zoe@ephys.dev>

--- packages/core/src/model.d.ts
+++ packages/core/src/model.d.ts
@@ -2579,7 +2579,7 @@ export abstract class Model<
     this: ModelStatic<M>,
     field: keyof Attributes<M>,
     options?: AggregateOptions<T, Attributes<M>>,
-  ): Promise<T>;
+  ): Promise<T | null>;
 
   /**
    * Finds the minimum value of field
@@ -2588,7 +2588,7 @@ export abstract class Model<
     this: ModelStatic<M>,
     field: keyof Attributes<M>,
     options?: AggregateOptions<T, Attributes<M>>,
-  ): Promise<T>;
+  ): Promise<T | null>;
 
   /**
    * Retrieves the sum of field
@@ -2597,7 +2597,7 @@ export abstract class Model<
     this: ModelStatic<M>,
     field: keyof Attributes<M>,
     options?: AggregateOptions<T, Attributes<M>>,
-  ): Promise<number>;
+  ): Promise<number | null>;
 
   /**
    * Builds a new model instance.
