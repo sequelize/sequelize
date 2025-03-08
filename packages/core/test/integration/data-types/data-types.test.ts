58f4151501502951b3a524a66d7755033284ca32
Alyx <zoe@ephys.dev>
2025-03-08 14:50:48 +0100

    fix type issues

--- packages/core/test/integration/data-types/data-types.test.ts
+++ packages/core/test/integration/data-types/data-types.test.ts
@@ -1545,7 +1545,7 @@ describe('DataTypes', () => {
   describe('BLOB', () => {
     const vars = beforeAll2(async () => {
       class User extends Model<InferAttributes<User>> {
-        declare attr: ArrayBuffer | string | Blob;
+        declare attr: ArrayBuffer | string | Buffer | Uint8Array;
       }
 
       User.init(
@@ -1589,6 +1589,7 @@ describe('DataTypes', () => {
       it('rejects Blobs & non-Uint8Array ArrayBufferViews', async () => {
         await expect(
           vars.User.create({
+            // @ts-expect-error -- intentionally testing invalid input
             attr: new Blob(['abcd']),
           }),
         ).to.be.rejectedWith(
@@ -1598,6 +1599,7 @@ describe('DataTypes', () => {
 
         await expect(
           vars.User.create({
+            // @ts-expect-error -- intentionally testing invalid input
             attr: new Uint16Array([49, 50, 51, 52]),
           }),
         ).to.be.rejectedWith(
