eb2bc8f016ae175fb5ddd452747b9f632bf17f69
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2025-03-12 12:38:03 +0000

    ci: [wip] start new attempt for ubuntu-22.04

--- dev/mssql/oldest/start.sh
+++ dev/mssql/oldest/start.sh
@@ -5,7 +5,9 @@ cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744
 docker compose -p sequelize-mssql-oldest down --remove-orphans
 docker compose -p sequelize-mssql-oldest up -d
 
-./../../wait-until-healthy.sh sequelize-mssql-oldest
+sleep 10
+
+docker logs sequelize-mssql-oldest
 
 docker exec sequelize-mssql-oldest \
   /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Password12!" -Q "CREATE DATABASE sequelize_test; ALTER DATABASE sequelize_test SET READ_COMMITTED_SNAPSHOT ON;"
