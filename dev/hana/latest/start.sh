97428c66578278e7874bd4e8a1504da3937ecebe
Dazhuang Cui <dazhuang.cui@sap.com>
2025-03-29 03:09:37 +0800

    feat(hana): HANA dialect
    1. before start docker, add /data/hxe/ and /data/hxe/hxepassword.json and set permission
      see https://hub.docker.com/r/saplabs/hanaexpress
    2. use 240s timeout in hana own wait-until-healthy.sh
      usually it takes ~200s to get started and healthy

--- dev/hana/latest/start.sh
+++ dev/hana/latest/start.sh
@@ -2,9 +2,15 @@
 set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" # https://stackoverflow.com/a/17744637
 
+sudo mkdir -p /data/hxe
+sudo chown 12000:79 /data/hxe
+echo '{ "master_password" : "HXEHana1" }' | sudo tee /data/hxe/hxepassword.json
+sudo chmod 600 /data/hxe/hxepassword.json
+sudo chown 12000:79 /data/hxe/hxepassword.json
+
 docker compose -p sequelize-hana-latest down --remove-orphans
 docker compose -p sequelize-hana-latest up -d
 
-./../../wait-until-healthy.sh sequelize-hana-latest
+./../wait-until-healthy.sh sequelize-hana-latest
 
 DIALECT=hana ../../../node_modules/.bin/ts-node ../../check-connection.ts
