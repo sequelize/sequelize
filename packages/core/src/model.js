4e68c7689deff2058b2490cca779ece901e9fc25
salmanlaskar <salman.laskar@napses.com>
2025-10-06 14:29:18 +0530

    lint fix

--- packages/core/src/model.js
+++ packages/core/src/model.js
@@ -219,10 +219,10 @@ export class Model extends ModelTypeScript {
       const defaults =
         modelDefinition.defaultValues.size > 0
           ? mapValues(getObjectFromMap(modelDefinition.defaultValues), getDefaultValue => {
-              const value = getDefaultValue();
+            const value = getDefaultValue();
 
-              return value && value instanceof BaseSqlExpression ? value : cloneDeepLodash(value);
-            })
+            return value && value instanceof BaseSqlExpression ? value : cloneDeepLodash(value);
+          })
           : Object.create(null);
 
       // set id to null if not passed as value, a newly created dao has no id
@@ -1303,7 +1303,7 @@ ${associationOwner._getAssociationDebugList()}`);
   }
 
   static _createModelVariant(optionOverrides) {
-    const model = class extends this {};
+    const model = class extends this { };
     model._initialModel = this;
     Object.defineProperty(model, 'name', { value: this.name });
 
@@ -1913,7 +1913,7 @@ ${associationOwner._getAssociationDebugList()}`);
     if (!options || !options.where || arguments.length > 1) {
       throw new Error(
         'Missing where attribute in the options parameter passed to findOrBuild. ' +
-          'Please note that the API has changed, and is now options only (an object with where, defaults keys, transaction etc.)',
+        'Please note that the API has changed, and is now options only (an object with where, defaults keys, transaction etc.)',
       );
     }
 
@@ -1955,7 +1955,7 @@ ${associationOwner._getAssociationDebugList()}`);
     if (!options || !options.where || arguments.length > 1) {
       throw new Error(
         'Missing where attribute in the options parameter passed to findOrCreate. ' +
-          'Please note that the API has changed, and is now options only (an object with where, defaults keys, transaction etc.)',
+        'Please note that the API has changed, and is now options only (an object with where, defaults keys, transaction etc.)',
       );
     }
 
@@ -2631,7 +2631,7 @@ ${associationOwner._getAssociationDebugList()}`);
                         attributeName === include.association.foreignKey ||
                         attributeName === include.association.otherKey ||
                         typeof associationInstance[include.association.through.model.name][
-                          attributeName
+                        attributeName
                         ] === 'undefined'
                       ) {
                         continue;
@@ -3411,8 +3411,8 @@ Instead of specifying a Model, either:
     assert(options && options.where, 'Missing where attribute in the options parameter');
     assert(
       isPlainObject(options.where) ||
-        Array.isArray(options.where) ||
-        options.where instanceof BaseSqlExpression,
+      Array.isArray(options.where) ||
+      options.where instanceof BaseSqlExpression,
       'Expected plain object, array or sequelize method in the options.where parameter',
     );
   }
