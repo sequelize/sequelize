b639a7c3723757ec537a39c39d599dc2e773c2f9
Andreas Lind <andreas.lind@workday.com>
2025-10-27 13:05:36 +0100

    Refine the support for bind assertions so we get to expect undefined for some dialects without falling back to expecting the bind object

--- packages/core/test/support.ts
+++ packages/core/test/support.ts
@@ -457,8 +457,15 @@ export function expectsql(
   }
 
   if ('bind' in assertions && !(expectation instanceof Error)) {
-    const bind =
-      assertions.bind[sequelize.dialect.name] || assertions.bind.default || assertions.bind;
+    let bind;
+    if (sequelize.dialect.name in assertions.bind) {
+      bind = assertions.bind[sequelize.dialect.name];
+    } else if ('default' in assertions.bind) {
+      bind = assertions.bind.default;
+    } else {
+      bind = assertions.bind;
+    }
+
     // @ts-expect-error -- too difficult to type, but this is safe
     expect(query.bind).to.deep.equal(bind);
   }
