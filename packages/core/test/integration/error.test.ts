b3a4b77b6ef824ef06c84d75d3d3e75e2d66e8c4
nav-sdi <ipates@navcanada.ca>
2026-02-20 16:13:27 -0500

    Apply suggestions from code review
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- packages/core/test/integration/error.test.ts
+++ packages/core/test/integration/error.test.ts
@@ -677,9 +677,9 @@ describe(getTestDialectTeaser('Sequelize Errors'), () => {
 
           case 'sqlite3':
             // https://github.com/sequelize/sequelize/pull/18117
-            // Using regex match() instead of equal() to pass UT for both sqlite3 and @vscode/sqlite3
-            expect(error.cause.message).match(
-              /SQLITE_CONSTRAINT.*: UNIQUE constraint failed: Users\.username/,
+            // Using regex match() instead of equal() to pass UT for both sqlite3 and `@vscode/sqlite3`
+            expect(error.cause.message).to.match(
+              /SQLITE_CONSTRAINT.*: UNIQUE constraint failed: Users\.username/
             );
             break;
 
