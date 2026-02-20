f19aa87395db809f88ec2dfac152f1f2504ed6a3
stefan ipate <stefan.ipate@navcanada.ca>
2026-02-20 14:16:28 -0500

    merge main to resolve conflicts, and address code review comments

--- packages/sqlite3/src/connection-manager.ts
+++ packages/sqlite3/src/connection-manager.ts
@@ -2,9 +2,9 @@ import type { AbstractConnection, ConnectionOptions } from '@sequelize/core';
 import { AbstractConnectionManager, ConnectionError } from '@sequelize/core';
 import { logger } from '@sequelize/core/_non-semver-use-at-your-own-risk_/utils/logger.js';
 import { checkFileExists } from '@sequelize/utils/node';
-import * as Sqlite3 from '@vscode/sqlite3';
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import * as Sqlite3 from 'sqlite3';
 import type { SqliteDialect } from './dialect.js';
 
 const debug = logger.debugContext('connection:sqlite3');
@@ -79,7 +79,7 @@ export {
   OPEN_READWRITE,
   OPEN_SHAREDCACHE,
   OPEN_URI,
-} from '@vscode/sqlite3';
+} from 'sqlite3';
 
 export class SqliteConnectionManager extends AbstractConnectionManager<
   SqliteDialect,
