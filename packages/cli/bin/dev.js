050348c4bcc543cdcd570338494f983c636c28e5
Rik Smale <git@riksmale.info>
2026-04-14 08:39:25 +0200

    Revert "chore: replace ts-node with tsx and native strip-types"
    This reverts commit e53877d2ae286c0a307324fc30082d9f9fe9d097.

--- packages/cli/bin/dev.js
+++ packages/cli/bin/dev.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S node --experimental-strip-types --no-warnings=ExperimentalWarning
+#!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
 
 import { execute } from '@oclif/core';
 
