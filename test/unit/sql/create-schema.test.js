51457591b7700e9e4000c0fb8cc2d40929d625de
Jason Williams <jaswilli@users.noreply.github.com>
2026-07-05 19:32:20 -0600

    Cleanup tests

--- test/unit/sql/create-schema.test.js
+++ test/unit/sql/create-schema.test.js
@@ -16,25 +16,9 @@ describe(Support.getTestDialectTeaser('SQL'), () => {
     });
 
     describe('createSchema', () => {
-      before(function () {
-        this.version = current.options.databaseVersion;
-      });
-
-      after(function () {
-        current.options.databaseVersion = this.version;
-      });
-
-      it('9.2.0 or above', () => {
-        current.options.databaseVersion = '9.2.0';
+      it('uses IF NOT EXISTS', () => {
         expectsql(sql.createSchema('foo'), {
           postgres: 'CREATE SCHEMA IF NOT EXISTS foo;'
-        });
-      });
-
-      it('below 9.2.0', () => {
-        current.options.databaseVersion = '9.0.0';
-        expectsql(sql.createSchema('foo'), {
-          postgres: 'CREATE SCHEMA foo;'
         });
       });
     });
